369a605ae36aa5991cc9ca5907e8cecce12272a2
nicholasrice <nicholasrice@users.noreply.github.com>
2021-10-04 13:17:33 -0700

    adds design-system-provider component

---
+++ src/Microsoft.Fast.Components.FluentUI/LocalizationDirection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Fast.Components.FluentUI
+{
+    public enum LocalizationDirection
+
+    {
+        ltr,
+        rtl
+    }
+
+    internal static class LocalizationDirectionExtensions
+    {
+        private static Dictionary<LocalizationDirection, string> _directionValues =
+            Enum.GetValues<LocalizationDirection>().ToDictionary(id => id, id => Enum.GetName(id).ToLowerInvariant());
+
+        public static string ToAttributeValue(this LocalizationDirection? value) => value == null ? null : _directionValues[value.Value];
+    }
+}
