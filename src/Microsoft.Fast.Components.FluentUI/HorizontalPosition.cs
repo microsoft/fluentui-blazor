6f3224a5d7945ecace5e6d4cfd6d8dc7c5f56e5a
Vincent Baaij <vnbaaij@outlook.com>
2021-10-05 19:11:13 +0200

    Add <FluentAnchoredRegion>

---
+++ src/Microsoft.Fast.Components.FluentUI/HorizontalPosition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Fast.Components.FluentUI
+{
+    public enum HorizontalPosition
+    {
+        Unset,
+        Start,
+        End,
+        Left,
+        Right
+    }
+
+    internal static class HorizontalDefaultPositionExtensions
+    {
+        private static Dictionary<HorizontalPosition, string> _positionValues =
+            Enum.GetValues<HorizontalPosition>().ToDictionary(id => id, id => Enum.GetName(id).ToLowerInvariant());
+
+        public static string ToAttributeValue(this HorizontalPosition? value) => value == null ? null : _positionValues[value.Value];
+    }
+}
---
+++ src/Microsoft.Fast.Components.FluentUI/VerticalPosition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Fast.Components.FluentUI
+{
+    public enum VerticalPosition
+    {
+        Unset,
+        Top,
+        Bottom
+    }
+
+    internal static class VerticalDefaultPositionExtensions
+    {
+        private static Dictionary<VerticalPosition, string> _positionValues =
+            Enum.GetValues<VerticalPosition>().ToDictionary(id => id, id => Enum.GetName(id).ToLowerInvariant());
+
+        public static string ToAttributeValue(this VerticalPosition? value) => value == null ? null : _positionValues[value.Value];
+    }
+}
