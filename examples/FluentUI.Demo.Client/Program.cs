1b4f640cc91c2aafe10541acfa1b23a5c997ddc6
Vincent Baaij <vnbaaij@outlook.com>
2021-10-08 17:44:53 +0200

    Add Client project

---
+++ examples/FluentUI.Demo.Client/Program.cs
@@ -0,0 +1,11 @@
+using FluentUI.Demo.Shared;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
