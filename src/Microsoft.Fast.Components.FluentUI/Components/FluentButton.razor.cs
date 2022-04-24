3f21ca20fbc8ac4ba4afe7c44ffceda015093f9b
Denis Voituron <denis@voituron.net>
2022-04-24 16:01:47 +0200

    Add Button icon properties

--- src/Microsoft.Fast.Components.FluentUI/Components/FluentButton.razor.cs
+++ src/Microsoft.Fast.Components.FluentUI/Components/FluentButton.razor.cs
@@ -13,6 +13,18 @@ public partial class FluentButton
     public bool? Autofocus { get; set; }
 
     [Parameter]
+    public string? IconStart { get; set; }
+
+    [Parameter]
+    public string? IconEnd { get; set; }
+
+    [Parameter]
+    public IconSize IconSize { get; set; } = FluentUI.IconSize.Size20;
+
+    [Parameter]
+    public bool IconAccentColor { get; set; } = false;
+
+    [Parameter]
     public RenderFragment? ChildContent { get; set; }
 
     [Parameter(CaptureUnmatchedValues = true)]
