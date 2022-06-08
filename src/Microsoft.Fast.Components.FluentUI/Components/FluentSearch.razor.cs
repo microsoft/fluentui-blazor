bcebc49c2b2a1210505d4387b11ab918a7ab02d4
William Wagner <44823142+williamw2@users.noreply.github.com>
2022-06-08 10:42:11 -0700

    Add Search Implementation

---
+++ src/Microsoft.Fast.Components.FluentUI/Components/FluentSearch.razor.cs
@@ -0,0 +1,80 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Components;
+
+namespace Microsoft.Fast.Components.FluentUI;
+
+public partial class FluentSearch : FluentInputBase<string?>
+{
+    /// <summary>
+    /// Gets or sets if the text area is disabled
+    /// </summary>
+    [Parameter]
+    public bool? Disabled { get; set; }
+
+    /// <summary>
+    /// Gets or sets if the text area is readonly
+    /// </summary>
+    [Parameter]
+    public bool? Readonly { get; set; }
+
+    /// <summary>
+    /// Gets or sets if the text area is required
+    /// </summary>
+    [Parameter]
+    public bool? Required { get; set; }
+
+    /// <summary>
+    /// Gets or sets if the text area is auto focussed
+    /// </summary>
+    [Parameter]
+    public bool? Autofocus { get; set; }
+
+    /// <summary>
+    /// Gets or sets the size of the text field
+    /// </summary>
+    [Parameter]
+    public int? Size { get; set; }
+
+    /// <summary>
+    /// Gets or sets the visual appearance. See <see cref="FluentUI.Appearance"/>
+    /// </summary>
+    [Parameter]
+    public Appearance? Appearance { get; set; }
+
+    /// <summary>
+    /// Gets or sets the text filed type. See <see cref="FluentUI.TextFieldType"/>
+    /// </summary>
+    [Parameter]
+    public TextFieldType? TextFieldType { get; set; }
+
+    /// <summary>
+    /// Gets or sets the placholder text
+    /// </summary>
+    [Parameter]
+    public string? Placeholder { get; set; }
+
+    /// <summary>
+    /// Gets or sets the minimum length
+    /// </summary>
+    [Parameter]
+    public int? MinLength { get; set; }
+
+    /// <summary>
+    /// Gets or sets the maximum length
+    /// </summary>
+    [Parameter]
+    public int? MaxLength { get; set; }
+
+    /// <summary>
+    /// Gets or sets the if spellcheck should be used
+    /// </summary>
+    [Parameter]
+    public bool? Spellcheck { get; set; }
+
+    protected override bool TryParseValueFromString(string? value, out string? result, [NotNullWhen(false)] out string? validationErrorMessage)
+    {
+        result = value;
+        validationErrorMessage = null;
+        return true;
+    }
+}
