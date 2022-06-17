2914e4cdf3d53d952ce959e23407a427605c6d23
Vincent Baaij <vnbaaij@outlook.com>
2022-06-17 16:28:23 +0200

    Rename fluentcheckedchange->checkedchange

--- src/Microsoft.Fast.Components.FluentUI/Events/EventHandlers.cs
+++ src/Microsoft.Fast.Components.FluentUI/Events/EventHandlers.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components;
 namespace Microsoft.Fast.Components.FluentUI;
 
 
-[EventHandler("onfluentcheckedchange", typeof(CheckboxChangeEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+[EventHandler("oncheckedchange", typeof(CheckboxChangeEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
 [EventHandler("ontabchange", typeof(TabChangeEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
 public static class EventHandlers
 {
