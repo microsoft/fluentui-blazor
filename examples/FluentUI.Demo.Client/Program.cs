a2d00e9a4fe86a096bb7aadf74d35f4bd8834481
Vincent Baaij <vnbaaij@outlook.com>
2022-09-06 14:26:38 +0200

    Add BlazorWasmPreRendering to Client

--- examples/FluentUI.Demo.Client/Program.cs
+++ examples/FluentUI.Demo.Client/Program.cs
@@ -7,7 +7,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddFluentUIComponents();
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
+
+
 
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+    services.AddFluentUIComponents();
+}
