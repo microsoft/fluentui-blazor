6fba80e66c9bad8eae06468ba4a1d264938cb405
Vincent Baaij <vnbaaij@outlook.com>
2022-09-22 21:44:20 +0200

    Add DataSource 'service' to WASM site

--- examples/FluentUI.Demo.Client/Program.cs
+++ examples/FluentUI.Demo.Client/Program.cs
@@ -1,4 +1,5 @@
 using FluentUI.Demo.Shared;
+using FluentUI.Demo.Shared.SampleData;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Fast.Components.FluentUI;
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents();
+builder.Services.AddScoped<DataSource>();
 
 await builder.Build().RunAsync();
