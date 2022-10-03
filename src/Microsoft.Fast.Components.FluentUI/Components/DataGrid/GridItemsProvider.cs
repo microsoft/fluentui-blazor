5885707497f1df561b216909bb0e089df7bdc01e
Vincent Baaij <vnbaaij@outlook.com>
2022-10-03 15:28:50 +0200

    Initial fluentui-reboot.css Dialog styling example DataGrid work

--- src/Microsoft.Fast.Components.FluentUI/Components/DataGrid/GridItemsProvider.cs
+++ src/Microsoft.Fast.Components.FluentUI/Components/DataGrid/GridItemsProvider.cs
@@ -8,6 +8,6 @@ namespace Microsoft.Fast.Components.FluentUI;
 /// </summary>
 /// <typeparam name="TGridItem">The type of data represented by each row in the grid.</typeparam>
 /// <param name="request">Parameters describing the data being requested.</param>
-/// <returns>A <see cref="ValueTask{GridItemsProviderResult{TResult}}" /> that gives the data to be displayed.</returns>
+/// <returns>A <see cref="T:ValueTask{GridItemsProviderResult{TResult}}" /> that gives the data to be displayed.</returns>
 public delegate ValueTask<GridItemsProviderResult<TGridItem>> GridItemsProvider<TGridItem>(
     GridItemsProviderRequest<TGridItem> request);
