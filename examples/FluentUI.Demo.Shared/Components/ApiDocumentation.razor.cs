01e2f6e1464e48c985365514faf084302cc69c7c
Vincent Baaij <vnbaaij@outlook.com>
2022-10-23 10:21:04 +0200

    ChildContent should show up in ApiDocumentation as it is no longer part of FluentComponentBase

--- examples/FluentUI.Demo.Shared/Components/ApiDocumentation.razor.cs
+++ examples/FluentUI.Demo.Shared/Components/ApiDocumentation.razor.cs
@@ -39,7 +39,7 @@ public partial class ApiDocumentation
 
     private IEnumerable<MemberDescription> GetMembers(MemberTypes type)
     {
-        string[] MEMBERS_TO_EXCLUDE = new[] { "AdditionalAttributes", "BackReference", "ChildContent", "Equals", "GetHashCode", "GetType", "SetParametersAsync", "ToString", "Dispose", "Class", "Style" };
+        string[] MEMBERS_TO_EXCLUDE = new[] { "AdditionalAttributes", "BackReference", "Class", "Style", "Equals", "GetHashCode", "GetType", "SetParametersAsync", "ToString", "Dispose" };
 
         if (_allMembers == null)
         {
