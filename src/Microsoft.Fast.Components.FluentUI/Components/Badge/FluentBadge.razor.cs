3330be812cccf24b7f40d934a690cb92a957437b
Vincent Baaij <vnbaaij@outlook.com>
2022-11-13 23:27:39 +0100

    Fine-tune examples

--- src/Microsoft.Fast.Components.FluentUI/Components/Badge/FluentBadge.razor.cs
+++ src/Microsoft.Fast.Components.FluentUI/Components/Badge/FluentBadge.razor.cs
@@ -23,13 +23,26 @@ public partial class FluentBadge : FluentComponentBase
 
     /// <summary>
     /// Gets or sets the visual appearance. See <seealso cref="FluentUI.Appearance"/>
+    /// Possible values are Accent (default), Neutral or Lightweight
     /// </summary>
     [Parameter]
-    public Appearance? Appearance { get; set; } = null;
+    public Appearance? Appearance { get; set; } = FluentUI.Appearance.Accent;
 
     /// <summary>
     /// Gets or sets the content to be rendered inside the component.
     /// </summary>
     [Parameter]
     public RenderFragment? ChildContent { get; set; }
-}
+
+    protected override Task OnParametersSetAsync()
+    {
+        if (Appearance != FluentUI.Appearance.Accent && 
+            Appearance != FluentUI.Appearance.Lightweight && 
+            Appearance != FluentUI.Appearance.Neutral)
+        {
+            throw new ArgumentException("FluentBadge Appearance needs to be one of Accent, Lightweight or Neutral.");
+        }
+
+        return base.OnParametersSetAsync();
+    }
+    }
