4c0c0d46b108bf3db700bd0359ab2bb724e1742f
Vincent Baaij <vnbaaij@outlook.com>
2022-11-13 12:15:52 +0100

    adjust style and headings

--- examples/FluentUI.Demo.Shared/Components/TableOfContents.razor.js
+++ examples/FluentUI.Demo.Shared/Components/TableOfContents.razor.js
@@ -1,12 +1,18 @@
 ﻿export function queryDomForTocEntries() {
     const article = document.getElementById('article');
-    const headings = article.querySelectorAll("h2, h3");
+    const headings = article.querySelectorAll("h2, h3, h4");
 
     let tocArray = new Array()
     let chapter = null;
     let subchapter = null;
 
     for (let element of headings) {
+        if (!element.id) {
+            let anchorText = element.innerText;
+            let elementId = anchorText.replaceAll(" ", "-").toLowerCase();
+            element.id = elementId;
+        }
+                
         let anchor = {
             "level": element.nodeName,
             "text": element.innerText,
@@ -20,7 +26,12 @@
                 chapter.anchors.push(subchapter);
 
             }
-        } else {
+        } else if ("H4" === element.nodeName) {
+            if (subchapter) {
+                subchapter.anchors.push(anchor);
+            }
+        }
+        else {
             chapter = anchor;
             tocArray.push(chapter);
 
