ba5da0e4f2b0b29a0fdd74271e135f67ef2b36c9
Andras Csanyi <andras.csanyi@pm.me>
2022-11-20 13:39:13 +0100

    adding Tests project adding bUnit tests against FluentAccordionItem

---
+++ src/Microsoft.Fast.Components.FluentUI.Tests/FluentAccordion/FluentAccordionItemRenderShould.cs
@@ -0,0 +1,123 @@
+using Bunit;
+using Xunit;
+
+namespace Microsoft.Fast.Components.FluentUI.Tests.FluentAccordion
+{
+    public class FluentAccordionItemRenderShould : RenderTestBase
+    {
+        [Fact]
+        public void RenderProperly_WithoutAnyProvided_Content()
+        {
+            // Arrange
+            IRenderedComponent<FluentAccordionItem> cut = TestContext.RenderComponent<FluentAccordionItem>();
+
+            // Assert
+            cut.MarkupMatches("<fluent-accordion-item id:ignore>" +
+                              "<span slot=\"heading\"></span>" +
+                              "</fluent-accordion-item>");
+        }
+
+        [Fact]
+        public void RenderProperly_WithProvided_Content()
+        {
+            // Arrange
+            IRenderedComponent<FluentAccordionItem> cut = TestContext.RenderComponent<FluentAccordionItem>(
+                parameters => parameters.AddChildContent("child content"));
+
+            // Assert
+            cut.MarkupMatches("<fluent-accordion-item id:ignore>" +
+                              "<span slot=\"heading\"></span>" +
+                              "child content" +
+                              "</fluent-accordion-item>");
+        }
+
+        [Fact]
+        public void RenderProperly_WithCustomHeaderValue()
+        {
+            // Arrange
+            IRenderedComponent<FluentAccordionItem> cut = TestContext.RenderComponent<FluentAccordionItem>(
+                parameters => parameters.Add(p => p.Heading, "custom heading value"));
+
+            // Assert
+            cut.MarkupMatches("<fluent-accordion-item id:ignore>" +
+                              "<span slot=\"heading\">custom heading value</span>" +
+                              "</fluent-accordion-item>");
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void RenderProperly_WithProvidedExpanded_Parameter(bool expanded)
+        {
+            // Arrange
+            IRenderedComponent<FluentAccordionItem> cut = TestContext.RenderComponent<FluentAccordionItem>(
+                parameters => parameters.Add(p => p.Expanded, expanded));
+
+            // Assert
+            if (expanded)
+            {
+                cut.MarkupMatches("<fluent-accordion-item id:ignore expanded=\"\">" +
+                                  "<span slot=\"heading\"></span>" +
+                                  "</fluent-accordion-item>");
+            }
+            else
+            {
+                cut.MarkupMatches("<fluent-accordion-item id:ignore>" +
+                                  "<span slot=\"heading\"></span>" +
+                                  "</fluent-accordion-item>");
+            }
+        }
+
+        [Fact]
+        public void RenderProperly_WithAdditionalAttribute()
+        {
+            // Arrange
+            IRenderedComponent<FluentAccordionItem> cut = TestContext.RenderComponent<FluentAccordionItem>(
+                parameters => parameters.AddUnmatched("unknown", "unknowns-value"));
+
+            // Assert
+            cut.MarkupMatches("<fluent-accordion-item id:ignore unknown=\"unknowns-value\">" +
+                              "<span slot=\"heading\"></span>" +
+                              "</fluent-accordion-item>");
+        }
+
+        [Fact]
+        public void RenderProperly_WithMultipleAdditionalAttributes()
+        {
+            // Arrange
+            IRenderedComponent<FluentAccordionItem> cut = TestContext.RenderComponent<FluentAccordionItem>(
+                parameters => parameters
+                    .AddUnmatched("unknown1", "unknown1s-value")
+                    .AddUnmatched("unknown2", "unknown2s-value"));
+
+            // Assert
+            cut.MarkupMatches("<fluent-accordion-item id:ignore " +
+                              "unknown1=\"unknown1s-value\"" +
+                              "unknown2=\"unknown2s-value\">" +
+                              "<span slot=\"heading\"></span>" +
+                              "</fluent-accordion-item>");
+        }
+
+        [Fact]
+        public void RenderProperly_WhenAllParamsAdded_AndAdditionalAttributes_AndContent()
+        {
+            // Arrange
+            IRenderedComponent<FluentAccordionItem> cut = TestContext.RenderComponent<FluentAccordionItem>(
+                parameters => parameters
+                    .Add(p => p.Expanded, true)
+                    .Add(p => p.Heading, "custom heading value")
+                    .AddUnmatched("unknown1", "unknown1s-value")
+                    .AddUnmatched("unknown2", "unknown2s-value")
+                    .AddChildContent("child content"));
+
+            // Assert
+            cut.MarkupMatches("<fluent-accordion-item id:ignore " +
+                              "expanded=\"\"" +
+                              "unknown1=\"unknown1s-value\"" +
+                              "unknown2=\"unknown2s-value\">" +
+                              "<span slot=\"heading\">custom heading value</span>" +
+                              "child content" +
+                              "</fluent-accordion-item>");
+        }
+    }
+}
---
+++ src/Microsoft.Fast.Components.FluentUI.Tests/RenderTestBase.cs
@@ -0,0 +1,9 @@
+using Bunit;
+
+namespace Microsoft.Fast.Components.FluentUI.Tests
+{
+    public class RenderTestBase
+    {
+        protected readonly TestContext TestContext = new TestContext();
+    }
+}
