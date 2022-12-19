15d608ec34f3826e4e658ac21b33d8cda5debca9
احسان ولی <ehsanvgfl@gmail.com>
2022-12-19 13:36:51 +0330

    Generate column header From DisplayAttribute

---
+++ src/Microsoft.Fast.Components.FluentUI/Components/DataGrid/Infrastructure/ColumnTypes/IBindColumn.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Microsoft.Fast.Components.FluentUI.DataGrid.Infrastructure;
+
+/// <summary>
+/// a column that can bind to a property of model
+/// </summary>
+public interface IBindColumn
+{
+   PropertyInfo? PropertyInfo { get; }
+}
+/// <summary>
+/// a column that can bind to a property of model
+/// </summary>
+/// <typeparam name="TItem">model item type</typeparam>
+/// <typeparam name="TValue">type of property</typeparam>
+internal interface IBindColumn<TItem, TValue> : IBindColumn
+{
+
+    public Expression<Func<TItem, TValue>> Property { get; set; }
+
+}
---
+++ src/Microsoft.Fast.Components.FluentUI/Components/DataGrid/Infrastructure/DisplayAttributeExtesions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.ComponentModel.DataAnnotations;
+
+namespace Microsoft.Fast.Components.FluentUI.DataGrid.Infrastructure;
+internal static class DisplayAttributeExtesions
+{
+
+    public static string? GetDisplayAttributeString(this Type itemType, string propertyName)
+    {
+        PropertyInfo? propertyInfo = itemType.GetProperty(propertyName);
+        //if (PropertyInfo == null && typeof(ICustomTypeProvider).IsAssignableFrom(itemType))
+        //    PropertyInfo = ((ICustomTypeProvider)Item).GetCustomType().GetProperty(PropertyName);
+        if (propertyInfo == null)
+            return null;
+
+        var DisplayAtt = propertyInfo.GetCustomAttributes(typeof(DisplayAttribute), true).FirstOrDefault();
+        if (DisplayAtt != null)
+            return ((DisplayAttribute)DisplayAtt).GetName();
+        else
+        {
+            var metadata = itemType.GetCustomAttribute(typeof(MetadataTypeAttribute)) as MetadataTypeAttribute;
+            if (metadata != null)
+                return metadata.MetadataClassType.GetDisplayAttributeString(propertyName);
+        }
+        return null;
+    }
+
+}
