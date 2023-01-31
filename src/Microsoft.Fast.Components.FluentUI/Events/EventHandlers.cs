01aea7757b98923dd6586ba3ef77dabf3ad4ba94
Vincent Baaij <vnbaaij@outlook.com>
2023-01-31 21:37:10 +0100

    Changes needed to fix #304 and #298

--- src/Microsoft.Fast.Components.FluentUI/Events/EventHandlers.cs
+++ src/Microsoft.Fast.Components.FluentUI/Events/EventHandlers.cs
@@ -4,6 +4,7 @@ namespace Microsoft.Fast.Components.FluentUI;
 
 
 [EventHandler("oncheckedchange", typeof(CheckboxChangeEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+[EventHandler("oncustomclick", typeof(ChangeEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
 [EventHandler("ontabchange", typeof(TabChangeEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
 [EventHandler("onselectedchange", typeof(TreeChangeEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
 [EventHandler("onexpandedchange", typeof(TreeChangeEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
