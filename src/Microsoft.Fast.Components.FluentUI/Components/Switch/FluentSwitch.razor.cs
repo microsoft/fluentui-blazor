7c7bd3e207cc577f058aa854b998f23529f52a07
Vincent Baaij <vnbaaij@outlook.com>
2023-02-01 13:00:22 +0100

    DemoSection: AdditionalFiles implementation FluentInputBase: require @bind-Value/ValueExpression FluentInputBase: make Class + status(modified/valid/invalid) work FluentRadio: make generic, fix cyclical render, make being in FluentRadioGroup required FluentRadioGroup: make generic, add FluentRadioContext FluentSwitch: fix cyclical render, add Checked parameter (not bound!) FluentNumberField: fix cyclical render

--- src/Microsoft.Fast.Components.FluentUI/Components/Switch/FluentSwitch.razor.cs
+++ src/Microsoft.Fast.Components.FluentUI/Components/Switch/FluentSwitch.razor.cs
@@ -11,7 +11,18 @@ public partial class FluentSwitch : FluentInputBase<bool>
     [Parameter]
     public RenderFragment? ChildContent { get; set; }
 
-    public bool? Checked { get; set; }
+    [Parameter]
+    public bool Checked
+    {
+        get
+        {
+            return Value;
+        }
+        set
+        {
+            Value = value;
+        }
+    }
 
     protected override bool TryParseValueFromString(string? value, out bool result, [NotNullWhen(false)] out string? validationErrorMessage) => throw new NotSupportedException($"This component does not parse string inputs. Bind to the '{nameof(CurrentValue)}' property, not '{nameof(CurrentValueAsString)}'.");
 }
