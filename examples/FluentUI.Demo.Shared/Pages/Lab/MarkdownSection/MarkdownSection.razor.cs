1ad2a45df52751dc20fc336337722bee1015344c
Vincent Baaij <vnbaaij@outlook.com>
2023-02-23 00:15:36 +0100

    Add new lab component: MarkdownSection Add more information section to demosite Add What's new page

---
+++ examples/FluentUI.Demo.Shared/Pages/Lab/MarkdownSection/MarkdownSection.razor.cs
@@ -0,0 +1,72 @@
+using Markdig;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Fast.Components.FluentUI;
+using Microsoft.Fast.Components.FluentUI.Infrastructure;
+
+// Remember to replace the namespace below with your own project's namespace..
+namespace FluentUI.Demo.Shared;
+
+public partial class MarkdownSection : FluentComponentBase
+{
+    private string? _content;
+
+    [Inject]
+    private IStaticAssetService StaticAssetService { get; set; } = default!;
+
+
+    /// <summary>
+    /// Gets or sets the Markdown content 
+    /// </summary>
+    [Parameter]
+    public string? Content { get; set; }
+
+    /// <summary>
+    /// Gets or sets asset to read the Markdown from
+    /// </summary>
+    [Parameter]
+    public string? FromAsset { get; set; }
+
+    public string? InternalContent
+    {
+        get => _content;
+        set
+        {
+            _content = value;
+            HtmlContent = ConvertToMarkupString(_content);
+            StateHasChanged();
+        }
+    }
+
+    public MarkupString HtmlContent { get; private set; }
+
+
+    protected override void OnInitialized()
+    {
+        if (Content is null && string.IsNullOrEmpty(FromAsset))
+            throw new ArgumentException("You need to provide either Content or FromAsset parameter");
+
+        InternalContent = Content;
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+
+        if (firstRender && !string.IsNullOrEmpty(FromAsset))
+            InternalContent = await StaticAssetService.GetAsync(FromAsset);
+
+    }
+
+    private MarkupString ConvertToMarkupString(string? value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            // Convert markdown string to HTML
+            string? html = Markdown.ToHtml(value, new MarkdownPipelineBuilder().UseAdvancedExtensions().Build());
+
+            // Return sanitized HTML as a MarkupString that Blazor can render
+            return new MarkupString(html);
+        }
+
+        return new MarkupString();
+    }
+}
