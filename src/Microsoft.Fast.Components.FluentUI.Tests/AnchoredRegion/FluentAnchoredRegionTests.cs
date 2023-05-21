7f1e74f920883a6b94d5946163de42194e0b6ea4
Denis Voituron <dvoituron@outlook.com>
2023-05-21 13:58:56 +0200

    Rename FluentAnchoredRegionRenderShould to FluentAnchoredRegionTests

--- src/Microsoft.Fast.Components.FluentUI.Tests/AnchoredRegion/FluentAnchoredRegionTests.cs
+++ src/Microsoft.Fast.Components.FluentUI.Tests/AnchoredRegion/FluentAnchoredRegionTests.cs
@@ -3,7 +3,7 @@ using Xunit;
 
 namespace Microsoft.Fast.Components.FluentUI.Tests.AnchoredRegion
 {
-    public class FluentAnchoredRegionRenderShould : TestBase
+    public class FluentAnchoredRegionTests : TestBase
     {
         [Fact]
         public void FluentAnchoredRegion_AttributeDefaultValues()
