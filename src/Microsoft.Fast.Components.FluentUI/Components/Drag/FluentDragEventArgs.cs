c1452c5d91ebb88588234ace79980ff294644e6c
Denis Voituron <dvoituron@outlook.com>
2023-05-26 16:48:35 +0200

    Add Drag and Drop components

---
+++ src/Microsoft.Fast.Components.FluentUI/Components/Drag/FluentDragEventArgs.cs
@@ -0,0 +1,27 @@
+﻿namespace Microsoft.Fast.Components.FluentUI;
+
+/// <summary />
+public class FluentDragEventArgs<TItem> : EventArgs
+{
+    /// <summary />
+    public FluentDragEventArgs()
+    {
+    }
+
+    /// <summary />
+    internal FluentDragEventArgs(FluentDropZone<TItem> source, FluentDropZone<TItem> target)
+    {
+        Source = source;
+        Target = target;
+    }
+
+    /// <summary>
+    /// Gets the zone where the drag started.
+    /// </summary>
+    public FluentDropZone<TItem> Source { get; } = null!;
+
+    /// <summary>
+    /// Gets the zone where the drag ended.
+    /// </summary>
+    public FluentDropZone<TItem> Target { get; } = null!;
+}
