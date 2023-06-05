888521cf7d8cda25ad49f1e67c24591f09ec53a2
Vincent Baaij <vnbaaij@outlook.com>
2023-06-05 16:15:26 +0200

    Replace sizes/variants with enums

--- src/Icons/FluentUI.Icons/Assets/Filled10.cs
+++ src/Icons/FluentUI.Icons/Assets/Filled10.cs
@@ -4,30 +4,21 @@
 //     the code is regenerated.
 // </auto-generated>
 
+#pragma warning disable 1591
 namespace Microsoft.Fast.Components.FluentUI;
 
-/// <summary />
 public static partial class Icons
 {
-    /// <summary />
     public static partial class Filled
     {
-        /// <summary />
         [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
         public static partial class Size10
         {
-            /// <summary />
-            public static Icon PresenceAvailable { get; } = new Icon("PresenceAvailable", "Filled", 10, "<path d=\"M5 10A5 5 0 1 0 5 0a5 5 0 0 0 0 10Zm2.1-5.9L4.85 6.35a.5.5 0 0 1-.7 0l-1-1a.5.5 0 0 1 .7-.7l.65.64 1.9-1.9a.5.5 0 0 1 .7.71Z\"/>");
-
-            /// <summary />
-            public static Icon PresenceAway { get; } = new Icon("PresenceAway", "Filled", 10, "<path d=\"M5 10A5 5 0 1 0 5 0a5 5 0 0 0 0 10Zm0-7v1.8l1.35 1.35a.5.5 0 1 1-.7.7l-1.5-1.5A.5.5 0 0 1 4 5V3a.5.5 0 0 1 1 0Z\"/>");
-
-            /// <summary />
-            public static Icon PresenceBusy { get; } = new Icon("PresenceBusy", "Filled", 10, "<path d=\"M10 5A5 5 0 1 1 0 5a5 5 0 0 1 10 0Z\"/>");
-
-            /// <summary />
-            public static Icon PresenceDnd { get; } = new Icon("PresenceDnd", "Filled", 10, "<path d=\"M5 10A5 5 0 1 0 5 0a5 5 0 0 0 0 10ZM3.5 4.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1 0-1Z\"/>");
-
+            public static Icon PresenceAvailablePresenceavailable { get; } = new Icon("PresenceAvailablePresenceavailable", IconVariant.Filled, IconSize.Size10, "<path d=\"M5 10A5 5 0 1 0 5 0a5 5 0 0 0 0 10Zm2.1-5.9L4.85 6.35a.5.5 0 0 1-.7 0l-1-1a.5.5 0 0 1 .7-.7l.65.64 1.9-1.9a.5.5 0 0 1 .7.71Z\"/>");
+            public static Icon PresenceAwayPresenceaway { get; } = new Icon("PresenceAwayPresenceaway", IconVariant.Filled, IconSize.Size10, "<path d=\"M5 10A5 5 0 1 0 5 0a5 5 0 0 0 0 10Zm0-7v1.8l1.35 1.35a.5.5 0 1 1-.7.7l-1.5-1.5A.5.5 0 0 1 4 5V3a.5.5 0 0 1 1 0Z\"/>");
+            public static Icon PresenceBusyPresencebusy { get; } = new Icon("PresenceBusyPresencebusy", IconVariant.Filled, IconSize.Size10, "<path d=\"M10 5A5 5 0 1 1 0 5a5 5 0 0 1 10 0Z\"/>");
+            public static Icon PresenceDndPresencednd { get; } = new Icon("PresenceDndPresencednd", IconVariant.Filled, IconSize.Size10, "<path d=\"M5 10A5 5 0 1 0 5 0a5 5 0 0 0 0 10ZM3.5 4.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1 0-1Z\"/>");
         }
     }
 }
+#pragma enable disable 1591
