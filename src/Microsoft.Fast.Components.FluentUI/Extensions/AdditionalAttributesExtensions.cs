a2b8217c053410da8d3db42df7c9251e2fedecc9
Andrei Saperski <30940385+andreisaperski@users.noreply.github.com>
2023-06-08 21:09:43 +0300

    Fix #370 - FluentIcon performance improvements (#439)
    * Fix #370
    
     - reduced number of StaticAssetService.GetAsync calls in FluentIcon
     - suppression of re-render if parameters haven't changed (when icon is in RenderFragment)
     - dedicated dictionary instead of array is used for icon availability check
    
    * additional attributes equality check refactored to an extension method

---
+++ src/Microsoft.Fast.Components.FluentUI/Extensions/AdditionalAttributesExtensions.cs
@@ -0,0 +1,56 @@
+﻿namespace Microsoft.Fast.Components.FluentUI;
+
+public static class AdditionalAttributesExtensions
+{
+    /// <summary> Determines whether two sets of attributes are equal when rendered. </summary>
+    /// <param name="x">The compared set</param>
+    /// <param name="y">The set to compare with</param>
+    /// <remarks></remarks>
+    /// <returns><c>true</c> if both sets render the same attributes; otherwise, <c>false</c>.</returns>
+    public static bool RenderedAttributesEqual(
+        this IReadOnlyDictionary<string, object>? x,
+        IReadOnlyDictionary<string, object>? y)
+    {
+        if ((x?.Count ?? 0) == 0 && (y?.Count ?? 0) == 0)
+        {
+            return true;
+        }
+
+        if (x is { Count: > 0 } &&
+            !x.AllRenderedAttributesInAndEqual(y))
+        {
+            return false;
+        }
+
+        if (y is { Count: > 0 } &&
+            !y.AllRenderedAttributesInAndEqual(x))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static bool AllRenderedAttributesInAndEqual(
+        this IReadOnlyDictionary<string, object> x,
+        IReadOnlyDictionary<string, object>? y)
+    {
+        foreach (var xKvp in x)
+        {
+            if (xKvp.Value is null)
+            {
+                continue;
+            }
+
+            if (y is null) return false;
+
+            if (!y.TryGetValue(xKvp.Key, out var yValue) ||
+                !xKvp.Value.Equals(yValue))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
