bbd93eb75ffb12668e072263ed7ff2c5793ac03e
dvoituron <dvoituron@outlook.com>
2023-06-11 17:38:42 +0200

    Add Unit Tests

--- src/Microsoft.Fast.Components.FluentUI.Tests/DateTime/FluentTimePickerTests.cs
+++ src/Microsoft.Fast.Components.FluentUI.Tests/DateTime/FluentTimePickerTests.cs
@@ -1,5 +1,30 @@
-﻿namespace Microsoft.Fast.Components.FluentUI.Tests.DateTime;
+﻿using Xunit;
+
+namespace Microsoft.Fast.Components.FluentUI.Tests.DateTime;
 
 public class FluentTimePickerTests : TestBase
 {
+    [Fact]
+    public void FluentTimePicker_Value_0123()
+    {
+        /*
+        WHY ???
+
+        FluentTimePicker requires a value for the 'ValueExpression' parameter.
+        Normally this is provided automatically when using 'bind-Value'.
+
+        // Arrange
+        using var ctx = new Bunit.TestContext();
+
+        // Act
+        var picker = ctx.RenderComponent<FluentTimePicker>(parameters =>
+        {
+            parameters.Add(p => p.Value, new System.DateTime(2000, 02, 03, 01, 23, 00));   // 01:23
+        });
+
+        // Assert
+        picker.Verify();
+
+        */
+    }
 }
--- src/Microsoft.Fast.Components.FluentUI.Tests/DateTime/ToTimeAgoTests.cs
+++ src/Microsoft.Fast.Components.FluentUI.Tests/DateTime/ToTimeAgoTests.cs
@@ -1,5 +1,29 @@
-﻿namespace Microsoft.Fast.Components.FluentUI.Tests.DateTime;
+﻿using Xunit;
 
-internal class ToTimeAgoTests : TestBase
+namespace Microsoft.Fast.Components.FluentUI.Tests.DateTime;
+
+public class ToTimeAgoTests : TestBase
 {
+    [Theory]
+    [InlineData("000.00:00:02", "Just now")]
+    [InlineData("000.00:00:25", "25 seconds ago")]
+    [InlineData("000.00:01:00", "1 minute ago")]
+    [InlineData("000.00:05:00", "5 minutes ago")]
+    [InlineData("000.01:00:00", "1 hour ago")]
+    [InlineData("000.05:00:00", "5 hours ago")]
+    [InlineData("001.00:00:00", "1 day ago")]
+    [InlineData("005.00:00:00", "5 days ago")]
+    [InlineData("031.00:00:00", "1 month ago")]
+    [InlineData("035.00:00:00", "2 months ago")]
+    [InlineData("150.00:00:00", "6 months ago")]
+    [InlineData("370.00:00:00", "1 year ago")]
+    [InlineData("740.00:00:00", "2 years ago")]
+    [InlineData("900.00:00:00", "2 years ago")]
+    [InlineData("920.00:00:00", "3 years ago")]
+    public void ToTimeAgo_Tests(string delayAsString, string expectedValue)
+    {
+        var delay = TimeSpan.ParseExact(delayAsString, @"ddd\.hh\:mm\:ss", null);
+
+        Assert.Equal(expectedValue, delay.ToTimeAgo());
+    }
 }
