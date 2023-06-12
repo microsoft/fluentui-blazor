c8a5000d702ba3662d149da37633f551061eaf77
dvoituron <dvoituron@outlook.com>
2023-06-12 09:40:42 +0200

    Add TimePicker Test

--- src/Microsoft.Fast.Components.FluentUI.Tests/DateTime/FluentTimePickerTests.cs
+++ src/Microsoft.Fast.Components.FluentUI.Tests/DateTime/FluentTimePickerTests.cs
@@ -7,24 +7,19 @@ public class FluentTimePickerTests : TestBase
     [Fact]
     public void FluentTimePicker_Value_0123()
     {
-        /*
-        WHY ???
-
-        FluentTimePicker requires a value for the 'ValueExpression' parameter.
-        Normally this is provided automatically when using 'bind-Value'.
-
         // Arrange
         using var ctx = new Bunit.TestContext();
+        System.DateTime? value = new System.DateTime(2000, 02, 03, 01, 23, 00); // 01:23
 
         // Act
         var picker = ctx.RenderComponent<FluentTimePicker>(parameters =>
         {
-            parameters.Add(p => p.Value, new System.DateTime(2000, 02, 03, 01, 23, 00));   // 01:23
+            parameters.Add(p => p.Value, value);
+            parameters.Add(p => p.ValueChanged, (System.DateTime? newValue) => value = newValue);
+            parameters.Add(p => p.ValueExpression, () => value);
         });
 
         // Assert
         picker.Verify();
-
-        */
     }
 }
