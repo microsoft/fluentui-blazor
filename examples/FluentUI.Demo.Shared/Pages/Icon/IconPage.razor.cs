6968b089f85a6257a5170fa8cc84ec4033df88ea
dvoituron <dvoituron@outlook.com>
2023-06-30 14:27:13 +0200

    Replace FluentIcon with FluentSystemIcon component

--- examples/FluentUI.Demo.Shared/Pages/Icon/IconPage.razor.cs
+++ examples/FluentUI.Demo.Shared/Pages/Icon/IconPage.razor.cs
@@ -105,7 +105,7 @@ public partial class IconPage
     {
         Logger.LogInformation($"You clicked on {icon.Name}");
 
-        string Text = $$"""<FluentIcon Name="@FluentIcons.{{icon.Name}}" Size="@IconSize.{{icon.Size}}" Variant="@IconVariant.{{icon.Variant}}" Color="@Color.{{Form.Color}}"/>""";
+        string Text = $$"""<FluentSystemIcon Name="@FluentIcons.{{icon.Name}}" Size="@IconSize.{{icon.Size}}" Variant="@IconVariant.{{icon.Variant}}" Color="@Color.{{Form.Color}}"/>""";
 
         if (_jsModule is not null)
         {
