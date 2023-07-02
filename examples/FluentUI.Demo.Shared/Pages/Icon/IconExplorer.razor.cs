f58463cdc2744c198fa34f0da922dcbc65fbbe5c
Denis Voituron <dvoituron@outlook.com>
2023-07-02 13:52:50 +0200

    Add first icon sample

---
+++ examples/FluentUI.Demo.Shared/Pages/Icon/IconExplorer.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Fast.Components.FluentUI;
+
+namespace FluentUI.Demo.Shared.Pages.Icon;
+
+public partial class IconExplorer
+{
+    private bool SearchInProgress = true;
+    private string SearchTerm = string.Empty;
+    private IEnumerable<IconInfo> IconsFound = Array.Empty<IconInfo>();
+
+    private async Task HandleSearchField(string searchTerm)
+    {
+        SearchInProgress = true;
+        await Task.Delay(1);
+
+        IconsFound = Icons.AllIcons
+                          .Where(i => i.Name.Contains(searchTerm, StringComparison.InvariantCultureIgnoreCase)
+                                   && i.Variant == IconVariant.Regular
+                                   && i.Size == IconSize.Size24)
+                          .Take(100)
+                          .ToArray();
+
+        SearchInProgress = false;
+        await Task.Delay(1);
+    }
+}
