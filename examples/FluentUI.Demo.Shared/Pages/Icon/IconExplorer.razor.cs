f99e5f9e9e1363dbfe939a2b1ff3981b89702581
dvoituron <dvoituron@outlook.com>
2023-07-02 15:32:07 +0200

    Add Icon sample page

--- examples/FluentUI.Demo.Shared/Pages/Icon/IconExplorer.razor.cs
+++ examples/FluentUI.Demo.Shared/Pages/Icon/IconExplorer.razor.cs
@@ -1,26 +1,37 @@
-﻿using Microsoft.Fast.Components.FluentUI;
+﻿using System.Linq;
+using Microsoft.Fast.Components.FluentUI;
 
 namespace FluentUI.Demo.Shared.Pages.Icon;
 
 public partial class IconExplorer
 {
-    private bool SearchInProgress = true;
-    private string SearchTerm = string.Empty;
+    private const int MAX_ICONS = 200;
+    private bool SearchInProgress = false;
+    private SearchCriteria Criteria = new ();
     private IEnumerable<IconInfo> IconsFound = Array.Empty<IconInfo>();
+    private int IconsCount = 0;
 
-    private async Task HandleSearchField(string searchTerm)
+    private async Task HandleSearchField()
     {
         SearchInProgress = true;
         await Task.Delay(1);
 
-        IconsFound = Icons.AllIcons
-                          .Where(i => i.Name.Contains(searchTerm, StringComparison.InvariantCultureIgnoreCase)
-                                   && i.Variant == IconVariant.Regular
-                                   && i.Size == IconSize.Size24)
-                          .Take(100)
-                          .ToArray();
+        var icons = Icons.AllIcons
+                         .Where(i => i.Name.Contains(Criteria.SearchTerm, StringComparison.InvariantCultureIgnoreCase)
+                                  && i.Variant == Criteria.Variant
+                                  && i.Size == Criteria.Size);
+
+        IconsCount = icons.Count();
+        IconsFound = icons.Take(MAX_ICONS).ToArray();
 
         SearchInProgress = false;
         await Task.Delay(1);
+    }
+
+    private class SearchCriteria
+    {
+        public string SearchTerm { get; set; } = string.Empty;
+        public IconVariant Variant { get; set; } = IconVariant.Regular;
+        public IconSize Size { get; set; } = IconSize.Size24;
     }
 }
