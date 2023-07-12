6d041f890269c5b8c75768d97b8380ea97886f8b
Denis Voituron <dvoituron@outlook.com>
2023-07-12 15:10:00 +0200

    New Solution Structure (#521)
    Move to new folders

---
+++ examples/Demo/Shared/wwwroot/sources/ReadMe.txt
@@ -0,0 +1,1 @@
+This folder will contain TXT files containing sample source code.
--- watch-server.bat
+++ watch-server.bat
@@ -1,2 +1,2 @@
 set DOTNET_WATCH_RESTART_ON_RUDE_EDIT=true
-dotnet watch --project examples\FluentUI.Demo.Server\FluentUI.Demo.Server.csproj
+dotnet watch --project examples\Demo\Server\FluentUI.Demo.Server.csproj
