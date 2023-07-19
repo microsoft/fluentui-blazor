aa9a75bcacfd722c62ac4493c579d133340153d2
Peter Morris <mrpmorris@gmail.com>
2023-07-19 17:04:40 +0100

    Remove MouseWithTargetIdEventArgs (#569)
    * Allow click on group of collapsed nav menu to trigger its action
    
    * Renamed TargetIdMouseEventArgs to MouseWithTargetIdEventArgs
    
    * Remove unneeded MouseWithTargetIdEventArgs
    
    * Remove MouseWithTargetIdEventArgs

--- src/Core/Components/NavMenu/FluentNavMenuGroup.razor.cs
+++ src/Core/Components/NavMenu/FluentNavMenuGroup.razor.cs
@@ -106,17 +106,6 @@ public partial class FluentNavMenuGroup : FluentNavMenuItemBase, INavMenuItemsOw
 
     private Task ToggleCollapsedAsync() => SetExpandedAsync(!Expanded);
 
-    private async Task HandleClickAsync(MouseWithTargetIdEventArgs args)
-    {
-        if (args.TargetId != Id || args.Button != 0)
-        {
-            return;
-        }
-
-        var navMenuActionArgs = new NavMenuActionArgs(this);
-        await ExecuteAsync(navMenuActionArgs);
-    }
-
     private async Task SetExpandedAsync(bool value)
     {
         if (value != Expanded)
