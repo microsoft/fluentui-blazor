5076f445a7a2d251d38305c22a64ced92e4cd873
Vincent Baaij <vnbaaij@outlook.com>
2023-07-20 12:46:22 +0200

    Use root namespace in all tests Start FluentBodyContentTests

--- tests/Core/Breadcrumb/FluentBreadcrumbTests.cs
+++ tests/Core/Breadcrumb/FluentBreadcrumbTests.cs
@@ -1,78 +1,77 @@
 using Bunit;
 using Xunit;
 
-namespace Microsoft.Fast.Components.FluentUI.Tests.Breadcrumb
+namespace Microsoft.Fast.Components.FluentUI.Tests.Breadcrumb;
+
+public class FluentBreadcrumbTests : TestBase
 {
-    public class FluentBreadcrumbTests : TestBase
+    [Fact]
+    public void FluentBreadcrumb_DefaultAttributeValues()
     {
-        [Fact]
-        public void FluentBreadcrumb_DefaultAttributeValues()
+        // Arrange && Act
+        var cut = TestContext.RenderComponent<FluentBreadcrumb>(parameters =>
         {
-            // Arrange && Act
-            var cut = TestContext.RenderComponent<FluentBreadcrumb>(parameters =>
-            {
-                parameters.AddChildContent<FluentBreadcrumbItem>(p => p.AddChildContent("child content"));
-            });
+            parameters.AddChildContent<FluentBreadcrumbItem>(p => p.AddChildContent("child content"));
+        });
 
-            // Assert
-            cut.Verify();
-        }
+        // Assert
+        cut.Verify();
+    }
 
-        [Fact]
-        public void FluentBreadcrumb_AdditionalCssClass()
+    [Fact]
+    public void FluentBreadcrumb_AdditionalCssClass()
+    {
+        // Arrange && Act
+        var cut = TestContext.RenderComponent<FluentBreadcrumb>(parameters =>
         {
-            // Arrange && Act
-            var cut = TestContext.RenderComponent<FluentBreadcrumb>(parameters =>
-            {
-                parameters.Add(p => p.Class, "css-class");
-                parameters.AddChildContent<FluentBreadcrumbItem>(p => p.AddChildContent("child content"));
-            });
+            parameters.Add(p => p.Class, "css-class");
+            parameters.AddChildContent<FluentBreadcrumbItem>(p => p.AddChildContent("child content"));
+        });
 
-            // Assert
-            cut.Verify();
-        }
+        // Assert
+        cut.Verify();
+    }
 
-        [Fact]
-        public void FluentBreadcrumb_AdditionalStyle()
+    [Fact]
+    public void FluentBreadcrumb_AdditionalStyle()
+    {
+        // Arrange && Act
+        var cut = TestContext.RenderComponent<FluentBreadcrumb>(parameters =>
         {
-            // Arrange && Act
-            var cut = TestContext.RenderComponent<FluentBreadcrumb>(parameters =>
-            {
-                parameters.Add(p => p.Style, "background-color: green");
-                parameters.AddChildContent<FluentBreadcrumbItem>(p => p.AddChildContent("child content"));
-            });
+            parameters.Add(p => p.Style, "background-color: green");
+            parameters.AddChildContent<FluentBreadcrumbItem>(p => p.AddChildContent("child content"));
+        });
 
-            // Assert
-            cut.Verify();
-        }
+        // Assert
+        cut.Verify();
+    }
 
-        [Fact]
-        public void FluentBreadcrumb_AdditionalAttribute()
+    [Fact]
+    public void FluentBreadcrumb_AdditionalAttribute()
+    {
+        // Arrange && Act
+        var cut = TestContext.RenderComponent<FluentBreadcrumb>(parameters =>
         {
-            // Arrange && Act
-            var cut = TestContext.RenderComponent<FluentBreadcrumb>(parameters =>
-            {
-                parameters.AddUnmatched("additional-attribute-name", "additional-attribute-value");
-                parameters.AddChildContent<FluentBreadcrumbItem>(p => p.AddChildContent("child content"));
-            });
+            parameters.AddUnmatched("additional-attribute-name", "additional-attribute-value");
+            parameters.AddChildContent<FluentBreadcrumbItem>(p => p.AddChildContent("child content"));
+        });
 
-            // Assert
-            cut.Verify();
-        }
+        // Assert
+        cut.Verify();
+    }
 
-        [Fact]
-        public void FluentBreadcrumb_AdditionalAttributes()
+    [Fact]
+    public void FluentBreadcrumb_AdditionalAttributes()
+    {
+        // Arrange && Act
+        var cut = TestContext.RenderComponent<FluentBreadcrumb>(parameters =>
         {
-            // Arrange && Act
-            var cut = TestContext.RenderComponent<FluentBreadcrumb>(parameters =>
-            {
-                parameters.AddUnmatched("additional-attribute1-name", "additional-attribute1-value");
-                parameters.AddUnmatched("additional-attribute2-name", "additional-attribute2-value");
-                parameters.AddChildContent<FluentBreadcrumbItem>(p => p.AddChildContent("child content"));
-            });
+            parameters.AddUnmatched("additional-attribute1-name", "additional-attribute1-value");
+            parameters.AddUnmatched("additional-attribute2-name", "additional-attribute2-value");
+            parameters.AddChildContent<FluentBreadcrumbItem>(p => p.AddChildContent("child content"));
+        });
 
-            // Assert
-            cut.Verify();
-        }
+        // Assert
+        cut.Verify();
     }
 }
