81c37adc762808c08b6798cd87ebeaf6e0887c67
Vincent Baaij <vnbaaij@outlook.com>
2023-07-21 12:43:45 +0200

    Fix merge and resolve comments

--- src/Core/Components/Forms/FluentValidationSummary.razor.cs
+++ src/Core/Components/Forms/FluentValidationSummary.razor.cs
@@ -5,6 +5,16 @@ namespace Microsoft.Fast.Components.FluentUI;
 
 public partial class FluentValidationSummary
 {
+    private IEnumerable<string>? _validationMessages;
+
     [CascadingParameter]
     public EditContext FluentEditContext { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+
+        _validationMessages = Model is null
+            ? FluentEditContext.GetValidationMessages()
+            : FluentEditContext.GetValidationMessages(new FieldIdentifier(Model, string.Empty));
+    }
 }
