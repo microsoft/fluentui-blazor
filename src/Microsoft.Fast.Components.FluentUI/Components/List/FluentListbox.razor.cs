2cda8bd7b7b01bd3836f90896525c7cad49cb334
Yue Cui <wangavatar95@gmail.com>
2023-08-10 10:48:53 +0800

    fix: Remove the outer div and the max-height, max-width parameters.

--- src/Microsoft.Fast.Components.FluentUI/Components/List/FluentListbox.razor.cs
+++ src/Microsoft.Fast.Components.FluentUI/Components/List/FluentListbox.razor.cs
@@ -6,19 +6,6 @@ namespace Microsoft.Fast.Components.FluentUI;
 public partial class FluentListbox<TOption> : ListComponentBase<TOption>
 {
     /// <summary>
-    /// Width style
-    /// </summary>
-
-    [Parameter]
-    public string Width { get; set; } = "250px";
-
-    /// <summary>
-    /// Height style
-    /// </summary>
-    [Parameter]
-    public string Height { get; set; } = "350px";
-
-    /// <summary>
     /// The maximum number of options that should be visible in the listbox scroll area.
     /// </summary>
     [Parameter]
