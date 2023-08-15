08a0db71a1935c679717595404d77f734cc51976
Vincent Baaij <vnbaaij@outlook.com>
2023-08-15 16:24:13 +0200

    Fix #596

--- src/Core/Components/DataGrid/Infrastructure/InternalGridContext.cs
+++ src/Core/Components/DataGrid/Infrastructure/InternalGridContext.cs
@@ -4,23 +4,26 @@ namespace Microsoft.Fast.Components.FluentUI.DataGrid.Infrastructure;
 
 // The grid cascades this so that descendant columns can talk back to it. It's an internal type
 // so that it doesn't show up by mistake in unrelated components.
-internal sealed class InternalGridContext<TGridItem>
+internal sealed class InternalGridContext<TGridItem>(FluentDataGrid<TGridItem> grid)
 {
-    private int index=0;
+    private int _index = 0;
     public Dictionary<string, FluentDataGridRow<TGridItem>> Rows { get; set; } = new();
 
-    public FluentDataGrid<TGridItem> Grid { get; }
+    public FluentDataGrid<TGridItem> Grid { get; } = grid;
     public EventCallbackSubscribable<object?> ColumnsFirstCollected { get; } = new();
 
-    public InternalGridContext(FluentDataGrid<TGridItem> grid)
+    internal void ResetRowIndexes(int start)
     {
-        Grid = grid;
+        _index = start;
     }
 
     internal void Register(FluentDataGridRow<TGridItem> row)
     {
         Rows.Add(row.RowId, row);
-        row.RowIndex = index++;
+        if (!Grid.Virtualize)
+        {
+            row.RowIndex = _index++;
+        }
     }
 
     internal void Unregister(FluentDataGridRow<TGridItem> row)
