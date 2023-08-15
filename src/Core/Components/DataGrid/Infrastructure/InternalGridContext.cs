55d0510b612982c85e90853e544b7a262eaea3a7
Vincent Baaij <vnbaaij@outlook.com>
2023-08-15 16:34:25 +0200

    Revert primary constructor to regular one as it is still in preview

--- src/Core/Components/DataGrid/Infrastructure/InternalGridContext.cs
+++ src/Core/Components/DataGrid/Infrastructure/InternalGridContext.cs
@@ -4,13 +4,18 @@ namespace Microsoft.Fast.Components.FluentUI.DataGrid.Infrastructure;
 
 // The grid cascades this so that descendant columns can talk back to it. It's an internal type
 // so that it doesn't show up by mistake in unrelated components.
-internal sealed class InternalGridContext<TGridItem>(FluentDataGrid<TGridItem> grid)
+internal sealed class InternalGridContext<TGridItem>
 {
     private int _index = 0;
     public Dictionary<string, FluentDataGridRow<TGridItem>> Rows { get; set; } = new();
 
-    public FluentDataGrid<TGridItem> Grid { get; } = grid;
+    public FluentDataGrid<TGridItem> Grid { get; }
     public EventCallbackSubscribable<object?> ColumnsFirstCollected { get; } = new();
+
+    public InternalGridContext(FluentDataGrid<TGridItem> grid)
+    {
+        Grid = grid;
+    }
 
     internal void ResetRowIndexes(int start)
     {
