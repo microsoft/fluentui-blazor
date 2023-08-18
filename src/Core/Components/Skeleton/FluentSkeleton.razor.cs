8dd41fec84d38558fed8e43063a2633e5ab318f0
Vincent Baaij <vnbaaij@outlook.com>
2023-08-18 09:38:59 +0200

    Skeleton: Add Width, Height, Visible

--- src/Core/Components/Skeleton/FluentSkeleton.razor.cs
+++ src/Core/Components/Skeleton/FluentSkeleton.razor.cs
@@ -1,9 +1,19 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.Fast.Components.FluentUI.Utilities;
 
 namespace Microsoft.Fast.Components.FluentUI;
 
 public partial class FluentSkeleton : FluentComponentBase
 {
+    protected string? ClassValue => new CssBuilder(Class)
+        .Build();
+
+    protected string? StyleValue => new StyleBuilder().AddStyle(Style)
+        .AddStyle("width", Width, () => !string.IsNullOrWhiteSpace(Width))
+        .AddStyle("height", Height, () => !string.IsNullOrWhiteSpace(Height))
+        .Build();
+
+
     /// <summary>
     /// Indicates the Skeleton should have a filled style.
     /// </summary>
@@ -27,6 +37,24 @@ public partial class FluentSkeleton : FluentComponentBase
     /// </summary>
     [Parameter]
     public bool? Shimmer { get; set; }
+
+    /// <summary>
+    /// Gets or sets the width of the skeleton
+    /// </summary>
+    [Parameter]
+    public string Width { get; set; } = "50px";
+
+    /// <summary>
+    /// Gets or sets the height of the skeleton
+    /// </summary>
+    [Parameter]
+    public string Height { get; set; } = "50px";
+
+    /// <summary>
+    /// Gets or sets whether the skeleton is visible
+    /// </summary>
+    [Parameter]
+    public bool Visible { get; set; } = true;
 
     /// <summary>
     /// Gets or sets the content to be rendered inside the component.
