c0121c74d91d0a213283ca180117060f30a87be9
dvoituron <dvoituron@outlook.com>
2023-09-08 14:13:07 +0200

    Add face picture samples

--- examples/Demo/Shared/SampleData/DataSourceExtensions.cs
+++ examples/Demo/Shared/SampleData/DataSourceExtensions.cs
@@ -4,7 +4,12 @@ public static class DataSourceExtensions
 {
     public static IQueryable<Person> WithVeryLongName(this IQueryable<Person> values)
     {
-        var longName = new Person(PersonId: 91, FirstName: "Jean", LastName: "With a very long name to validate components", CountryCode: "fr", BirthDate: new DateOnly(1984, 4, 27));
+        var longName = new Person(PersonId: 91,
+                                  FirstName: "Jean",
+                                  LastName: "With a very long name to validate components",
+                                  CountryCode: "fr",
+                                  BirthDate: new DateOnly(1984, 4, 27),
+                                  Picture: DataSource.ImageFaces[0]);
         return values.Concat(new[] { longName });
     }
 }
