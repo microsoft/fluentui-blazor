1c6822b0c0a1019a8ca6a4883b5601ee3ebe84df
Vincent Baaij <vnbaaij@outlook.com>
2023-09-11 10:49:56 +0200

    FluentMessageBarTest wip

---
+++ tests/Core/MessageBar/FluentMessageBarTests.cs
@@ -0,0 +1,50 @@
+﻿using Bunit;
+using Xunit;
+
+namespace Microsoft.Fast.Components.FluentUI.Tests.MessageBar;
+
+public class FluentMessageBarTests : TestBase
+{
+    private const string FluentAnchorRazorJs = "./_content/Microsoft.Fast.Components.FluentUI/Components/Anchor/FluentAnchor.razor.js";
+
+    public FluentMessageBarTests()
+    {
+        TestContext.JSInterop.SetupModule(FluentAnchorRazorJs);
+    }
+    
+    [Fact]
+    public void FluentMessageBar_Default()
+    {
+        // Arrange
+        var cut = TestContext.RenderComponent<FluentMessageBar>(parameters =>
+        {
+            parameters.Add(p => p.Title, "This is a message");
+        });
+
+        // Assert
+        cut.Verify();
+    }
+
+    [Fact]
+    public void FluentMessageBar_Body_Link()
+    {
+        // Arrange
+        var content = new Message();
+        var cut = TestContext.RenderComponent<FluentMessageBar>(parameters =>
+        {
+            content.Body = "This is a message body";
+            content.Link = new MessageAction
+            {
+                Href = "https://fast.design",
+                Text = "Learn more"
+            };
+
+            parameters.Add(p => p.Title, "This is a message");
+            parameters.Add(p => p.Content, content);
+        
+        });
+
+        // Assert
+        cut.Verify();
+    }
+}
