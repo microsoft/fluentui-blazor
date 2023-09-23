ba4bf129dace0510dcd5e4ec81b1f972c0f8b6a2
Denis Voituron <dvoituron@outlook.com>
2023-09-23 19:52:06 +0200

    Add InlineStyleBuilder and fix the FluentSelect.Height property (#774)

---
+++ tests/Core/Utilities/InlineStyleBuilderTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Fast.Components.FluentUI.Utilities;
+using Xunit;
+
+namespace Microsoft.Fast.Components.FluentUI.Tests.Utilities;
+
+public class InlineStyleBuilderTests : TestBase
+{
+    [Fact]
+    public void InlineStyleBuilder_Default()
+    {
+        // Assert
+        var styleBuilder = new InlineStyleBuilder();
+
+        // Act
+        styleBuilder.AddStyle("div", "color", "red");
+        styleBuilder.AddStyle("div", "background-color", "blue");
+        styleBuilder.AddStyle("span", "color", "yellow");
+
+        // Assert - Values are sorted
+        Assert.Equal(@"<style> div { color: red; background-color: blue; } span { color: yellow; } </style>", styleBuilder.Build(newLineSeparator: false));
+    }
+
+    [Fact]
+    public void InlineStyleBuilder_SingleStyle()
+    {
+        // Assert
+        var styleBuilder = new InlineStyleBuilder();
+
+        // Act
+        styleBuilder.AddStyle("div", "color", "red");
+
+        // Assert - Values are sorted
+        Assert.Equal(@"<style> div { color: red; } </style>", styleBuilder.Build(newLineSeparator: false));
+    }
+
+    [Fact]
+    public void InlineStyleBuilder_NoStyle()
+    {
+        // Assert
+        var styleBuilder = new InlineStyleBuilder();
+
+        // Act
+        styleBuilder.AddStyle("div", "color", string.Empty);
+
+        // Assert - Values are sorted
+        Assert.Null(styleBuilder.Build(newLineSeparator: false));
+    }
+}
