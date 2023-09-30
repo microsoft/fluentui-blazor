13fd1d0639253a8e0449337c507e176cb891873f
Aathif_Mahir <aathifmahir@gmail.com>
2023-09-30 19:57:40 +0530

    Initial Theme Service Contracts

---
+++ src/Core/DesignTokens/Services/IThemeService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Microsoft.Fast.Components.FluentUI.DesignTokens;
+
+public interface IThemeService
+{
+    bool IsMobile { get; }
+    StandardLuminance SelectedTheme { get; }
+    OfficeColor SelectedAccentColor { get; }
+    LocalizationDirection SelectedDirection { get; }
+    ElementReference ElementRef { get; set; }
+    Task InitializeAsync(bool isPersistent = true);
+    Task SetThemeAsync(StandardLuminance standardLuminance);
+    Task SetThemeAsync(bool isDarkMode);
+    Task SetAccentColorAsync(OfficeColor officeColor);
+    Task SetAccentColorAsync(string color);
+    Task SetDirectionAsync(bool isRTL);
+}
---
+++ src/Core/DesignTokens/Services/IThemeStorageService.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.Fast.Components.FluentUI.DesignTokens;
+
+internal interface IThemeStorageService : IAsyncDisposable
+{
+    ValueTask<bool> IsStorageEnabledAndSupported();
+    ValueTask<bool> IsMobile();
+    ValueTask<string?> GetThemeAsync();
+    ValueTask SetThemeAsync(StandardLuminance theme);
+    ValueTask<string?> GetAccentColorAsync();
+    ValueTask SetAccentColorAsync(string color);
+    ValueTask<bool?> GetDirectionAsync();
+    ValueTask SetDirectionLocallyAsync(bool isRTL);
+    ValueTask SetDirectionAsync(bool isRTL);
+}
