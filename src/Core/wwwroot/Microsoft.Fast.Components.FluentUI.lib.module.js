325ea504cf55ee4eca6cb7b543dfb1ec143fa158
Vincent Baaij <vnbaaij@outlook.com>
2023-10-13 16:37:55 +0200

    Use latest SDKs Make demos better when opening in light/dark mode

--- src/Core/wwwroot/Microsoft.Fast.Components.FluentUI.lib.module.js
+++ src/Core/wwwroot/Microsoft.Fast.Components.FluentUI.lib.module.js
@@ -157,6 +157,9 @@ export function afterStarted(blazor) {
 }
 
 export function beforeStart(options, extensions) {
+    var defaultBaseLayerLuminance = window.matchMedia("(prefers-color-scheme: dark)").matches ? 0.23 : 1.0;
+    window['DefaultBaseLayerLuminance'] = defaultBaseLayerLuminance;
+
     var wcScript = document.createElement('script');
     wcScript.type = 'module';
     wcScript.src = './_content/Microsoft.Fast.Components.FluentUI/js/web-components-v2.5.16-custom.min.js';
