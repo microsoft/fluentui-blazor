7f0a9c6eed0f9e00d662e70f798d74c4edb92637
Denis Voituron <dvoituron@outlook.com>
2023-10-21 17:01:59 +0200

    Fix aria-orientation (#877)

---
+++ src/Core/Components/Divider/FluentDivider.razor.js
@@ -0,0 +1,12 @@
+﻿export function setDividerAriaOrientation() {
+
+    const elements = document.querySelectorAll("fluent-divider[role='presentation']");
+
+    if (!!elements) {
+        elements.forEach((element, i, array) => {
+            if (!!element && element.hasAttribute("aria-orientation")) {
+                element.removeAttribute("aria-orientation");
+            }
+        });
+    }
+}
