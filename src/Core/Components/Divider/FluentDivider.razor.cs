5a2f84fdf23d8a639e7fa18272abfb8aa0418b33
Vincent Baaij <vnbaaij@outlook.com>
2023-10-29 22:47:42 +0100

    Templates WIP Fix FluentDivider

--- src/Core/Components/Divider/FluentDivider.razor.cs
+++ src/Core/Components/Divider/FluentDivider.razor.cs
@@ -1,18 +1,18 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components.Utilities;
 using Microsoft.JSInterop;
 
 namespace Microsoft.FluentUI.AspNetCore.Components;
 
-public partial class FluentDivider : FluentComponentBase
+public partial class FluentDivider : FluentComponentBase, IAsyncDisposable
 {
     private const string JAVASCRIPT_FILE = "./_content/Microsoft.FluentUI.AspNetCore.Components/Components/Divider/FluentDivider.razor.js";
 
+    private IJSObjectReference _jsModule = default!;
+
     [Inject]
-    private IJSRuntime JSRuntime { get; set; } = default!;
-
-    /// <summary />
-    private IJSObjectReference? Module { get; set; }
-
+    protected IJSRuntime JSRuntime { get; set; } = default!;
+    
     /// <summary>
     /// The role of the element.
     /// </summary>
@@ -31,12 +31,30 @@ public partial class FluentDivider : FluentComponentBase
     [Parameter]
     public RenderFragment? ChildContent { get; set; }
 
-    protected async override Task OnInitializedAsync()
+    protected async override Task OnAfterRenderAsync(bool firstRender)
     {
-        Module ??= await JSRuntime.InvokeAsync<IJSObjectReference>("import", JAVASCRIPT_FILE);
-        await Module.InvokeVoidAsync("setDividerAriaOrientation");
-     
-        await base.OnInitializedAsync();
+        if (firstRender)
+        {
+            _jsModule ??= await JSRuntime.InvokeAsync<IJSObjectReference>("import", JAVASCRIPT_FILE);
+            await _jsModule.InvokeVoidAsync("setDividerAriaOrientation");
+        }   
+    }
+
+    /// <inheritdoc />
+    public async ValueTask DisposeAsync()
+    {
+        try
+        {
+            if (_jsModule is not null)
+            {
+                await _jsModule.DisposeAsync();
+            }
+        }
+        catch (JSDisconnectedException)
+        {
+            // The JSRuntime side may routinely be gone already if the reason we're disposing is that
+            // the client disconnected. This is not an error.
+        }
     }
 }
 
