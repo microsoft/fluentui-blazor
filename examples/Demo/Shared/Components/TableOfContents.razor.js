6bf40cd6844ccf677984cf32d01c79914d1127de
Vincent Baaij <vnbaaij@outlook.com>
2023-10-29 15:19:23 +0100

    Fix 'Back to top' and header styling

--- examples/Demo/Shared/Components/TableOfContents.razor.js
+++ examples/Demo/Shared/Components/TableOfContents.razor.js
@@ -44,13 +44,18 @@
 let backToTopButton = document.getElementById("backtotop");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function ()
+let bodycontent = document.getElementById('body-content');
+if (!bodycontent) {
+    bodycontent = document.body;
+}
+
+bodycontent.onscroll = function ()
 { 
     scrollFunction()
 };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 20 ||document.getElementById('body-content').scrollTop > 20 || document.documentElement.scrollTop > 20) {
         backToTopButton.style.display = "flex";
     } else {
         backToTopButton.style.display = "none";
@@ -61,6 +66,7 @@ function scrollFunction() {
 export function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+    document.getElementById('body-content').scrollTop = 0;
 }
 
 // Very simple check to see if mobile or tablet is being used 
