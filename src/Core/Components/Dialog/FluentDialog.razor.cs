8945c9e67290b0d5a69b24b56512b4b788ac3473
Vincent Baaij <vnbaaij@outlook.com>
2023-11-05 14:27:50 +0100

    Dialog: Add autoheight class if Height set to 'auto'

--- src/Core/Components/Dialog/FluentDialog.razor.cs
+++ src/Core/Components/Dialog/FluentDialog.razor.cs
@@ -24,6 +24,7 @@ public partial class FluentDialog : FluentComponentBase
         .AddClass("right", () => _parameters.DialogType == DialogType.Panel && _parameters.Alignment == HorizontalAlignment.Right)
         .AddClass("left", () => _parameters.DialogType == DialogType.Panel && _parameters.Alignment == HorizontalAlignment.Left)
         .AddClass("prevent-scroll", () => Instance is null ? (PreventScroll && !Hidden): _parameters.PreventScroll)
+        .AddClass("autoheight", () => !string.IsNullOrWhiteSpace(_parameters.Height) && _parameters.Height == "auto")
         .Build();
 
     /// <summary />
