a88691f62534d4555e84d0013c53850a5b9faeac
dvoituron <dvoituron@outlook.com>
2023-11-09 22:27:50 +0100

    Update the design

--- examples/Demo/AssetExplorer/Components/Pages/IconExplorer.razor.cs
+++ examples/Demo/AssetExplorer/Components/Pages/IconExplorer.razor.cs
@@ -1,4 +1,5 @@
-﻿using FluentUI.Demo.AssetExplorer.Models;
+﻿using System.Reflection;
+using FluentUI.Demo.AssetExplorer.Models;
 using Microsoft.AspNetCore.Components;
 using Microsoft.FluentUI.AspNetCore.Components;
 using Microsoft.JSInterop;
@@ -7,16 +8,22 @@ namespace FluentUI.Demo.AssetExplorer.Components.Pages;
 
 public partial class IconExplorer
 {
+    private const int ITEMS_PER_PAGE = 24;
+    private const string JAVASCRIPT_FILE = "./_content/FluentUI.Demo.AssetExplorer/Components/Pages/IconExplorer.razor.js";
+
     private bool SearchInProgress = false;
 
     private readonly IconSearchCriteria Criteria = new();
-    private IconInfo[] IconsFound = Array.Empty<IconInfo>();
-    
-    private IJSObjectReference? _jsModule;
-    private PaginationState PaginationState = new PaginationState { ItemsPerPage = 25 };
+    private IconInfo[] IconsFound = Array.Empty<IconInfo>();    
+    private PaginationState PaginationState = new PaginationState { ItemsPerPage = ITEMS_PER_PAGE };
 
     [Inject]
     private IJSRuntime JSRuntime { get; set; } = default!;
+
+    private IJSObjectReference? JSModule { get; set; }
+
+    [Inject]
+    public IToastService ToastService { get; set; } = default!;
 
     private IEnumerable<IconInfo> IconsForCurrentPage
     {
@@ -27,11 +34,7 @@ public partial class IconExplorer
         }
     }
 
-    /// <summary>
-    /// Start a new search.
-    /// </summary>
-    /// <returns></returns>
-    private async Task HandleSearchAsync()
+    private async Task StartNewSearchAsync()
     {
         Console.WriteLine($"HandleSearchAsync {Criteria.Size}");
 
@@ -53,14 +56,16 @@ public partial class IconExplorer
         StateHasChanged();
     }
 
-    //public async void HandleClick(IconInfo icon)
-    //{
-    //    string Text = $$"""<FluentIcon Value="@(new Icons.{{icon.Variant}}.{{icon.Size}}.{{icon.Name}}())" Color="@Color.{{IconColor}}"/>""";
+    public async void CopyToClipboardAsync(IconInfo icon)
+    {
+        string value = $"Value=\"@(new Icons.{icon.Variant}.{icon.Size}.{icon.Name}())\"";
+        string color = Criteria.Color == Color.Accent ? string.Empty : $" Color=\"@Color.{Criteria.Color}\"";
+        
+        string code = $"<FluentIcon {value}{color} />";
 
-    //    if (_jsModule is not null)
-    //    {
-    //        await _jsModule.InvokeVoidAsync("copyText", Text);
-    //    }
-    //}
+        //JSModule ??= await JSRuntime.InvokeAsync<IJSObjectReference>("import", JAVASCRIPT_FILE);
+        //var error = await JSModule.InvokeAsync<string>("copyToClipboard", code);
 
+        ToastService.ShowSuccess($"FluentIcon `{icon.Name}` component declaration copied to clipboard.");
+    }
 }
