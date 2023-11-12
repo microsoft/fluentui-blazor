1dea85a4f786513520be39f592017f10882ae9f7
dvoituron <dvoituron@outlook.com>
2023-11-12 16:30:02 +0100

    Add a PreviewCard component

---
+++ examples/Demo/AssetExplorer/Components/Controls/PreviewCard.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace FluentUI.Demo.AssetExplorer.Components.Controls;
+
+public partial class PreviewCard
+{
+    private const string JAVASCRIPT_FILE = "./Components/Controls/PreviewCard.razor.js";
+
+    private IJSObjectReference? JSModule { get; set; }
+
+    [Inject]
+    private IJSRuntime JSRuntime { get; set; } = default!;
+
+
+    [Inject]
+    public IToastService ToastService { get; set; } = default!;
+
+    [Parameter]
+    public Color? IconColor { get; set; }
+
+    [Parameter]
+    public IconInfo? Icon { get; set; }
+
+    [Parameter]
+    public EmojiInfo? Emoji { get; set; }
+
+    public async void CopyToClipboardAsync()
+    {
+        if (Icon != null)
+        {
+            string value = $"Value=\"@(new Icons.{Icon.Variant}.{Icon.Size}.{Icon.Name}())\"";
+            string color = IconColor == Color.Accent ? string.Empty : $" Color=\"@Color.{IconColor}\"";
+
+            string code = $"<FluentIcon {value}{color} />";
+
+            JSModule ??= await JSRuntime.InvokeAsync<IJSObjectReference>("import", JAVASCRIPT_FILE);
+            var error = await JSModule.InvokeAsync<string>("copyToClipboard", code);
+
+            ToastService.ShowSuccess($"FluentIcon `{Icon.Name}` component declaration copied to clipboard.");
+        }
+    }
+}
--- examples/Demo/AssetExplorer/Components/Pages/IconExplorer.razor.cs
+++ examples/Demo/AssetExplorer/Components/Pages/IconExplorer.razor.cs
@@ -2,28 +2,18 @@
 using FluentUI.Demo.AssetExplorer.Models;
 using Microsoft.AspNetCore.Components;
 using Microsoft.FluentUI.AspNetCore.Components;
-using Microsoft.JSInterop;
 
 namespace FluentUI.Demo.AssetExplorer.Components.Pages; 
 
 public partial class IconExplorer
 {
     private const int ITEMS_PER_PAGE = 4 * 12;
-    private const string JAVASCRIPT_FILE = "./_content/FluentUI.Demo.AssetExplorer/Components/Pages/IconExplorer.razor.js";
-
+    
     private bool SearchInProgress = false;
 
     private readonly IconSearchCriteria Criteria = new();
     private IconInfo[] IconsFound = Array.Empty<IconInfo>();    
     private PaginationState PaginationState = new PaginationState { ItemsPerPage = ITEMS_PER_PAGE };
-
-    private IJSObjectReference? JSModule { get; set; }
-
-    [Inject]
-    private IJSRuntime JSRuntime { get; set; } = default!;
-
-    [Inject]
-    public IToastService ToastService { get; set; } = default!;
 
     [Parameter]
     public string Width { get; set; } = "95%";
@@ -58,18 +48,5 @@ public partial class IconExplorer
     private void HandleCurrentPageIndexChanged()
     {
         StateHasChanged();
-    }
-
-    public async void CopyToClipboardAsync(IconInfo icon)
-    {
-        string value = $"Value=\"@(new Icons.{icon.Variant}.{icon.Size}.{icon.Name}())\"";
-        string color = Criteria.Color == Color.Accent ? string.Empty : $" Color=\"@Color.{Criteria.Color}\"";
-        
-        string code = $"<FluentIcon {value}{color} />";
-
-        //JSModule ??= await JSRuntime.InvokeAsync<IJSObjectReference>("import", JAVASCRIPT_FILE);
-        //var error = await JSModule.InvokeAsync<string>("copyToClipboard", code);
-
-        ToastService.ShowSuccess($"FluentIcon `{icon.Name}` component declaration copied to clipboard.");
     }
 }
