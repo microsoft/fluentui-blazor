053a8129a249bc9cc1b0c9b572f0d0f15b27b77f
Vincent Baaij <vnbaaij@outlook.com>
2023-11-13 17:20:48 +0100

    Clean up redundant js functions

--- examples/Demo/Shared/Shared/DemoMainLayout.razor.js
+++ examples/Demo/Shared/Shared/DemoMainLayout.razor.js
@@ -2,25 +2,6 @@
     return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent);
 }
 
-export function switchHighlightStyle(dark) {
-    if (dark) {
-        document
-            .querySelector(`link[title="dark"]`)
-            .removeAttribute("disabled");
-        document
-            .querySelector(`link[title="light"]`)
-            .setAttribute("disabled", "disabled");
-    }
-    else {
-        document
-            .querySelector(`link[title="light"]`)
-            .removeAttribute("disabled");
-        document
-            .querySelector(`link[title="dark"]`)
-            .setAttribute("disabled", "disabled");
-    }
-}
-
 export function isDarkMode() {
     let matched = window.matchMedia("(prefers-color-scheme: dark)").matches ;
 
@@ -29,9 +10,3 @@ export function isDarkMode() {
     else
         return false;
     }
-
-export function switchDirection(dir) {
-    document.dir = dir;
-    const container = document.getElementById('container');
-    container.style.direction = dir;
-}
