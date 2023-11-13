57dca700632d00a8abb1c99a84c79977645d9bd2
LuohuaRain <wangavatar95@gmail.com>
2023-11-13 17:48:14 +0800

    Added new tests for FluentAccordionItem Heading and HeadingContent Parameters

--- tests/Core/Accordion/FluentAccordionItemTests.cs
+++ tests/Core/Accordion/FluentAccordionItemTests.cs
@@ -41,6 +41,24 @@ public class FluentAccordionItemTests : TestBase
         cut.Verify();
     }
 
+    [Fact]
+    public void FluentAccordionItem_WithHeadingContentAndHeading_IsProvidedBoth()
+    {
+        // Arrange & Act
+        var cut = TestContext.RenderComponent<FluentAccordionItem>(parameters =>
+        {
+            parameters.Add(p => p.HeadingContent, context =>
+            {
+                context.AddContent(0, "custom heading content");
+            });
+
+            parameters.Add(p => p.Heading, "Heading Value First");
+        });
+
+        // Assert
+        cut.Verify();
+    }
+
     [Theory]
     [InlineData(true)]
     [InlineData(false)]
@@ -120,6 +138,35 @@ public class FluentAccordionItemTests : TestBase
         var cut = TestContext.RenderComponent<FluentAccordionItem>(parameters =>
         {
             parameters.Add(p => p.Style, "background-color: grey");
+        });
+
+        // Assert
+        cut.Verify();
+    }
+
+    [Fact]
+    public void FluentAccordionItem_WithHeadingContent_IsNull()
+    {
+        // Arrange & Act
+        var cut = TestContext.RenderComponent<FluentAccordionItem>(parameters =>
+        {
+            parameters.Add(p => p.HeadingContent, context => { });
+        });
+
+        // Assert
+        cut.Verify();
+    }
+
+    [Fact]
+    public void FluentAccordionItem_WithHeadingContent_IsProvided()
+    {
+        // Arrange & Act
+        var cut = TestContext.RenderComponent<FluentAccordionItem>(parameters =>
+        {
+            parameters.Add(p => p.HeadingContent, context =>
+            {
+                context.AddContent(0, "custom heading content");
+            });
         });
 
         // Assert
