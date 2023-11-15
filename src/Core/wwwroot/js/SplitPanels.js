3a91649346e4246c81c8bce084240751a12b01be
Vincent Baaij <vnbaaij@outlook.com>
2023-11-15 21:31:11 +0100

    Splitter enhancements (from Aspire feedback). Fix #957 and fix #958 (#960)
    * - Add Panel1MinSize and Panel2MinSize parameters
    - Add BarSize parameter
    - Make resizing always use proportional values
    
    * Fix and add splitter tests

--- src/Core/wwwroot/js/SplitPanels.js
+++ src/Core/wwwroot/js/SplitPanels.js
@@ -9,7 +9,7 @@ styleSheet.replaceSync(`
     :host([resizing]){ user-select: none; }
     :host([resizing][direction=row]){ cursor: col-resize; }
     :host([direction=row]) { grid-template-columns: var(--first-size, 1fr) max-content var(--second-size, 1fr); }
-    :host([direction=row]) #median { inline-size: 0.5rem; grid-column: 2 / 3; }
+    :host([direction=row]) #median { grid-column: 2 / 3; }
     :host([direction=row]) #median:hover { cursor: col-resize; }
     :host([direction=row]) #median span[part="handle"] { height: 16px; margin: 2px 0;}
     :host([direction=row]) #slot1 { grid-column: 1 / 2; grid-row: 1 / 1; }
@@ -17,7 +17,7 @@ styleSheet.replaceSync(`
 
     :host([resizing][direction=col]){ cursor: row-resize; }
     :host([direction=column]) { grid-template-rows: var(--first-size, 1fr) max-content var(--second-size, 1fr); }
-    :host([direction=column]) #median { block-size: 0.5rem; grid-row: 2 / 3; }
+    :host([direction=column]) #median { grid-row: 2 / 3; }
     :host([direction=column]) #median:hover { cursor: row-resize; }
     :host([direction=column]) #median span[part="handle"] { width: 16px; margin: 0 2px;}
     :host([direction=column]) #slot1 { grid-row: 1 / 2; grid-column: 1 / 1; }
@@ -46,12 +46,16 @@ const template = `
 `;
 
 class SplitPanels extends HTMLElement {
-    static observedAttributes = ["direction", "collapsed"];
+    static observedAttributes = ["direction", "collapsed", "barsize", "slot1minsize", "slot2minsize"];
     #direction = "row";
     #isResizing = false;
     #collapsed = false;
+    #barsize;
     #slot1size;
     #slot2size;
+    #slot1minsize;
+    #slot2minsize;
+    #totalsize;
 
     constructor() {
         super();
@@ -68,6 +72,11 @@ class SplitPanels extends HTMLElement {
         const shadow = this.attachShadow({ mode: "open" });
         shadow.adoptedStyleSheets.push(styleSheet);
         shadow.innerHTML = template;
+
+        if (this.direction === "row")
+            shadow.querySelector('#median').style.inlineSize = this.barsize +'px';
+        else
+            shadow.querySelector('#median').style.blockSize = this.barsize + 'px';
     }
     connectedCallback() {
         this.render();
@@ -87,6 +96,8 @@ class SplitPanels extends HTMLElement {
         const clientRect = this.getBoundingClientRect();
         this.left = clientRect.x;
         this.top = clientRect.y;
+        this.#totalsize = this.direction === "row" ? clientRect.width : clientRect.height;
+
         this.addEventListener("pointermove", this.resizeDrag);
         this.addEventListener("pointerup", this.pointerup);
     }
@@ -99,17 +110,51 @@ class SplitPanels extends HTMLElement {
     resizeDrag(e) {
         if (this.direction === "row") {
             const newMedianLeft = e.clientX - this.left;
-            const median = this.dom.median.getBoundingClientRect().width;
+            const median = this.barsize;
+
             this.#slot1size = Math.floor(newMedianLeft - (median / 2));
-            this.#slot2size = this.clientWidth - this.#slot1size - median;
-            this.style.gridTemplateColumns = `${this.#slot1size}px ${median}px 1fr`;
+            this.#slot2size = Math.floor(this.clientWidth - this.#slot1size - (median / 2));
+
+            let min1size = this.ensurevalue(this.slot1minsize);
+            if (this.#slot1size < min1size) {
+                this.#slot1size = Math.floor(min1size);
+                this.#slot2size = Math.floor(this.clientWidth - this.#slot1size - (median / 2));
+            }
+            let min2size = this.ensurevalue(this.slot2minsize);
+            if (this.#slot2size < min2size) {
+                this.#slot2size = Math.floor(min2size);
+                this.#slot1size = Math.floor(this.clientWidth - this.#slot2size - (median / 2));
+            }
+
+            const totalSize = this.#slot1size + this.#slot2size - median;
+            let slot1fraction = (this.#slot1size / totalSize).toFixed(2);
+            let slot2fraction = (this.#slot2size / totalSize).toFixed(2);
+
+            this.style.gridTemplateColumns = `${slot1fraction}fr ${median}px ${slot2fraction}fr`;
         }
         if (this.direction === "column") {
             const newMedianTop = e.clientY - this.top;
-            const median = this.dom.median.getBoundingClientRect().height;
+            const median = this.barsize;
+
             this.#slot1size = Math.floor(newMedianTop - (median / 2));
-            this.#slot2size = this.clientHeight - this.#slot1size - median;
-            this.style.gridTemplateRows = `${this.#slot1size}px ${median}px 1fr`;
+            this.#slot2size = Math.floor(this.clientHeight - this.#slot1size - (median / 2));
+
+            let min1size = this.ensurevalue(this.slot1minsize);
+            if (this.#slot1size < min1size) {
+                this.#slot1size = Math.floor(min1size);
+                this.#slot2size = Math.floor(this.clientHeight - this.#slot1size - (median / 2));
+            }
+            let min2size = this.ensurevalue(this.slot2minsize);
+            if (this.#slot2size < min2size) {
+                this.#slot2size = Math.floor(min2size);
+                this.#slot1size = Math.floor(this.clientHeight - this.#slot2size - (median / 2));
+            }
+
+            const totalSize = this.#slot1size + this.#slot2size - median;
+            let slot1fraction = (this.#slot1size / totalSize).toFixed(2);
+            let slot2fraction = (this.#slot2size / totalSize).toFixed(2);
+
+            this.style.gridTemplateRows = `${slot1fraction}fr ${median}px ${slot2fraction}fr`;
         }
     }
     attributeChangedCallback(name, oldValue, newValue) {
@@ -117,6 +162,24 @@ class SplitPanels extends HTMLElement {
             this[name] = newValue;
         }
     }
+    ensurevalue(value) {
+    if (!value)
+        return null;
+
+    value = value.trim().toLowerCase();
+
+    if (value.endsWith("%"))
+        return this.#totalsize * parseFloat(value) / 100;
+
+    if (value.endsWith("px"))
+        return parseFloat(value);
+
+    if (value.endsWith("fr"))
+        return this.#totalsize * parseFloat(value)
+
+    return 0;
+    }
+
     set isResizing(value) {
         this.#isResizing = value;
         if (value) {
@@ -153,6 +216,26 @@ class SplitPanels extends HTMLElement {
     }
     get collapsed() {
         return this.#collapsed;
+    }
+
+    set slot1minsize(value) {
+        this.#slot1minsize = value ?? 0;
+    }
+    get slot1minsize() {
+        return this.#slot1minsize;
+    }
+
+    set slot2minsize(value) {
+        this.#slot2minsize = value ?? 0;
+    }
+    get slot2minsize() {
+        return this.#slot2minsize;
+    }
+    set barsize(value) {
+        this.#barsize = value;
+    }
+    get barsize() {
+        return this.#barsize;
     }
 }
 
