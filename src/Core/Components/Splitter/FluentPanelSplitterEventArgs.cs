665207a15de44b8f02c6503cee62fd8b2207d81f
dvoituron <dvoituron@outlook.com>
2023-11-17 17:28:06 +0100

    Add FluentyPanelSplitter component

---
+++ src/Core/Components/Splitter/FluentPanelSplitterEventArgs.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// --------------------------------------------------------------
+
+namespace Microsoft.FluentUI.AspNetCore.Components;
+
+/// <summary>
+/// Supplies information about a <see cref="FluentPanelSplitter.OnExpand" /> or <see cref="FluentPanelSplitter.OnCollapse" /> event that is being raised.
+/// </summary>
+public class FluentPanelSplitterEventArgs
+{
+    /// <summary>
+    /// Gets the index of the pane.
+    /// </summary>
+    public int PaneIndex { get; set; } = 0;
+
+    /// <summary>
+    /// Gets the pane which the event applies to.
+    /// </summary>
+    /// <value>The pane.</value>
+    public FluentPanelSplitterPane Pane { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets a value which will cancel the event.
+    /// </summary>
+    /// <value><c>true</c> to cancel the event; otherwise, <c>false</c>.</value>
+    public bool Cancel { get; set; } = false;
+}
---
+++ src/Core/Components/Splitter/FluentPanelSplitterResizeEventArgs.cs
@@ -0,0 +1,16 @@
+﻿// --------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// --------------------------------------------------------------
+
+namespace Microsoft.FluentUI.AspNetCore.Components;
+
+/// <summary>
+/// Supplies information about a <see cref="FluentPanelSplitter.OnResize" /> event that is being raised.
+/// </summary>
+public class FluentPanelSplitterResizeEventArgs : FluentPanelSplitterEventArgs
+{
+    /// <summary>
+    /// The new size of the pane.
+    /// </summary>
+    public double NewSize { get; set; }
+}
