b49ea917730c3f3a121841eda52bbd2e3fd94abc
dvoituron <dvoituron@outlook.com>
2023-12-01 10:02:09 +0100

    Add HiddenWhen

---
+++ src/Core/Enums/GridItemHidden.cs
@@ -0,0 +1,105 @@
+﻿using System.ComponentModel;
+
+namespace Microsoft.FluentUI.AspNetCore.Components;
+
+/// <summary>
+/// Hide the <see cref="FluentGridItem"/> when a size condition is met.
+/// </summary>
+[Flags]
+public enum GridItemHidden
+{
+    /// <summary>
+    /// Never
+    /// </summary>
+    None = 0,
+
+    /// <summary>
+    /// Browser size less  that 599px
+    /// </summary>
+    Xs = 1,
+
+    /// <summary>
+    /// Browser size between 600px and 959px
+    /// </summary>
+    Sm = 2,
+
+    /// <summary>
+    /// Browser size between 960px and 1279px
+    /// </summary>
+    Md = 4,
+
+    /// <summary>
+    /// Browser size between 1280px and 1919px
+    /// </summary>
+    Lg = 8,
+
+    /// <summary>
+    /// Browser size between 1920px and 2559px
+    /// </summary>
+    Xl = 16,
+
+    /// <summary>
+    /// Browser size greater  than 2560px
+    /// </summary>
+    Xxl = 32,
+
+    /// <summary>
+    /// Browser size less  that 599px
+    /// </summary>
+    XsAndDown = Xs,
+
+    /// <summary>
+    /// Browser size less  that 959px
+    /// </summary>
+    SmAndDown = Xs | Sm,
+
+    /// <summary>
+    /// Browser size less  that 1279px
+    /// </summary>
+    MdAndDown = Xs | Sm | Md,
+
+    /// <summary>
+    /// Browser size less  that 1919px
+    /// </summary>
+    LgAndDown = Xs | Sm | Md | Lg,
+
+    /// <summary>
+    /// Browser size less that 2559px
+    /// </summary>
+    XlAndDown = Xs | Sm | Md | Lg | Xl,
+
+    /// <summary>
+    /// All sizes
+    /// </summary>
+    XxlAndDown = Xs | Sm | Md | Lg | Xxl,
+
+    /// <summary>
+    /// All sizes
+    /// </summary>
+    XsAndUp = Xs | Sm | Md | Lg | Xl | Xxl,
+
+    /// <summary>
+    /// Browser size greater that 600px
+    /// </summary>
+    SmAndUp = Sm | Md | Lg | Xl | Xxl,
+
+    /// <summary>
+    /// Browser size greater that 960px
+    /// </summary>
+    MdAndUp = Md | Lg | Xl | Xxl,
+
+    /// <summary>
+    /// Browser size greater that 1280px
+    /// </summary>
+    LgAndUp = Lg | Xl | Xxl,
+
+    /// <summary>
+    /// Browser size greater that 1920px
+    /// </summary>
+    XlAndUp = Xl | Xxl,
+
+    /// <summary>
+    /// Browser size greater that 2560px
+    /// </summary>
+    XxlAndUp = Xxl,
+}
