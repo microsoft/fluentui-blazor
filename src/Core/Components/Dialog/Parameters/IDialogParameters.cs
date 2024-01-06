b5e384cb8874183e8aa16e041fcbf6d8daa42893
Austin Jackson <themacphage@gmail.com>
2024-01-06 03:52:49 -0600

    Implemented OnDialogClosing callback (#1242)
    * Implemented OnDialogClosing callback
    
    * Implemented OnDialogOpened callback + Added example code
    
    * Fixed @ondialogdismiss event handling issue
    
    ---------
    
    Co-authored-by: Vincent Baaij <vnbaaij@outlook.com>

--- src/Core/Components/Dialog/Parameters/IDialogParameters.cs
+++ src/Core/Components/Dialog/Parameters/IDialogParameters.cs
@@ -1,32 +1,34 @@
 ﻿using Microsoft.AspNetCore.Components;
 
-namespace Microsoft.Fast.Components.FluentUI
+namespace Microsoft.FluentUI.AspNetCore.Components;
+
+public interface IDialogParameters
 {
-    public interface IDialogParameters
-    {
-        string Id { get; set; }
-        HorizontalAlignment Alignment { get; set; }
-        string? Title { get; set; }
-        bool? Modal { get; set; }
-        bool? TrapFocus { get; set; }
-        bool ShowTitle { get; set; }
-        bool ShowDismiss { get; set; }
-        string? DismissTitle { get; set; }
-        string? PrimaryAction { get; set; }
-        bool PrimaryActionEnabled { get; set; }
-        string? SecondaryAction { get; set; }
-        bool SecondaryActionEnabled { get; set; }
-        string? Width { get; set; }
-        string? Height { get; set; }
-        string DialogBodyStyle { get; set; }
-        string? AriaDescribedby { get; set; }
-        string? AriaLabel { get; set; }
-        string? AriaLabelledby { get; set; }
-        DialogType DialogType { get; set; }
-        bool PreventScroll { get; set; }
-        bool PreventDismissOnOverlayClick { get; set; }
-        EventCallback<DialogResult> OnDialogResult { get; set; }
-    }
+    string Id { get; set; }
+    HorizontalAlignment Alignment { get; set; }
+    string? Title { get; set; }
+    bool? Modal { get; set; }
+    bool? TrapFocus { get; set; }
+    bool ShowTitle { get; set; }
+    bool ShowDismiss { get; set; }
+    string? DismissTitle { get; set; }
+    string? PrimaryAction { get; set; }
+    bool PrimaryActionEnabled { get; set; }
+    string? SecondaryAction { get; set; }
+    bool SecondaryActionEnabled { get; set; }
+    string? Width { get; set; }
+    string? Height { get; set; }
+    string DialogBodyStyle { get; set; }
+    string? AriaDescribedby { get; set; }
+    string? AriaLabel { get; set; }
+    string? AriaLabelledby { get; set; }
+    DialogType DialogType { get; set; }
+    bool PreventScroll { get; set; }
+    bool PreventDismissOnOverlayClick { get; set; }
+    EventCallback<DialogResult> OnDialogResult { get; set; }
+    EventCallback<DialogInstance> OnDialogClosing { get; set; }
+    EventCallback<DialogInstance> OnDialogOpened { get; set; }
+}
 
     public interface IDialogParameters<TContent> : IDialogParameters
          where TContent : class
