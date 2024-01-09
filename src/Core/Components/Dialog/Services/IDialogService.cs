9cfcb512e8a1e344c6eb74095a9e9ddab09251f4
Yue Cui <37979368+LuohuaRain@users.noreply.github.com>
2024-01-09 22:20:01 +0800

    Optimize Dialog CloseAsync Method  (#1270)
    * fix a typo
    
    * - Dialog performance improved.
    - Fix some typos
    
    ---------
    
    Co-authored-by: Vincent Baaij <vnbaaij@outlook.com>

--- src/Core/Components/Dialog/Services/IDialogService.cs
+++ src/Core/Components/Dialog/Services/IDialogService.cs
@@ -9,14 +9,14 @@ public partial interface IDialogService
     Task CloseAsync(DialogReference dialog, DialogResult result);
 
     /// <summary>
-    /// A event that will be invoked when showing a dialog with a custom component
+    /// An event that will be invoked when showing a dialog with a custom component
     /// </summary>
     public event Action<IDialogReference, Type?, DialogParameters, object>? OnShow;
 
     public event Func<IDialogReference, Type?, DialogParameters, object, Task<IDialogReference>>? OnShowAsync;
 
     public event Action<string, DialogParameters>? OnUpdate;
-    
+
     public event Func<string, DialogParameters, Task<IDialogReference?>>? OnUpdateAsync;
 
     public event Action<IDialogReference, DialogResult>? OnDialogCloseRequested;
