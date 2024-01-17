16d2388da7c1d303eec9c5514506d975de97e02b
Vincent Baaij <vnbaaij@outlook.com>
2024-01-17 10:00:45 +0100

    Fix #1337 by setting GlobalState.Luminance in FluentDesingTheme OnAfterRenderAsync

--- src/Core/Components/DesignSystemProvider/FluentDesignTheme.razor.cs
+++ src/Core/Components/DesignSystemProvider/FluentDesignTheme.razor.cs
@@ -114,6 +114,7 @@ public partial class FluentDesignTheme : ComponentBase
     {
         Id = Identifier.NewId();
     }
+
     /// <summary>
     /// Method raised by the JavaScript code when the "mode" changes.
     /// </summary>
@@ -195,7 +196,7 @@ public partial class FluentDesignTheme : ComponentBase
             var realLuminance = await Module.InvokeAsync<string>("GetGlobalLuminance");
             realLuminance = string.IsNullOrWhiteSpace(realLuminance) ? "1.0" : realLuminance;
             var isDark = double.Parse(realLuminance, CultureInfo.InvariantCulture) < 0.5;
-            //GlobalDesign.SetLuminance(isDark ? StandardLuminance.DarkMode : StandardLuminance.LightMode);
+            GlobalDesign.SetLuminance(isDark ? StandardLuminance.DarkMode : StandardLuminance.LightMode);
 
             if (OnLoaded.HasDelegate)
             {
