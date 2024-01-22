018c42e6358a7f4cb82449c6b5d12a41c8ef95b5
Vincent Baaij <vnbaaij@outlook.com>
2024-01-23 00:10:33 +0100

    Fix compiler warnings/messages

--- src/Core/Components/List/FluentOption.razor.cs
+++ src/Core/Components/List/FluentOption.razor.cs
@@ -6,7 +6,7 @@ public partial class FluentOption<TOption> : FluentComponentBase, IDisposable wh
 {
 
     [CascadingParameter(Name = "ListContext")]
-    internal InternalListContext<TOption> _internalListContext { get; set; } = default!;
+    internal InternalListContext<TOption> InternalListContext { get; set; } = default!;
 
     /// <summary>
     /// Gets or sets a value indicating whether the element is disabled.
@@ -24,14 +24,14 @@ public partial class FluentOption<TOption> : FluentComponentBase, IDisposable wh
     /// Gets or sets a value indicating whether the element is selected.
     /// </summary>
     [Parameter]
-    public bool Selected { get; set; }
+    public bool? Selected { get; set; }
 
 
     /// <summary>
     /// Called whenever the selection changed.
     /// </summary>
     [Parameter]
-    public EventCallback<bool> SelectedChanged { get; set; }
+    public EventCallback<bool?> SelectedChanged { get; set; }
 
     /// <summary>
     /// Gets or sets the content to be rendered inside the component.
@@ -47,19 +47,19 @@ public partial class FluentOption<TOption> : FluentComponentBase, IDisposable wh
 
     protected override Task OnInitializedAsync()
     {
-        _internalListContext.Register(this);
+        InternalListContext.Register(this);
 
         return base.OnInitializedAsync();
     }
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
-        if (firstRender && Selected &&
-            _internalListContext != null &&
-            _internalListContext.ValueChanged.HasDelegate &&
-            _internalListContext.ListComponent.Multiple)
+        if (firstRender && Selected == true &&
+            InternalListContext != null &&
+            InternalListContext.ValueChanged.HasDelegate &&
+            InternalListContext.ListComponent.Multiple)
         {
-            await _internalListContext.ValueChanged.InvokeAsync(Value);
+            await InternalListContext.ValueChanged.InvokeAsync(Value);
         }
     }
 
@@ -83,15 +83,15 @@ public partial class FluentOption<TOption> : FluentComponentBase, IDisposable wh
         }
         else
         {
-            if (_internalListContext != null && _internalListContext.ListComponent.Items is null)
+            if (InternalListContext != null && InternalListContext.ListComponent.Items is null)
             {
-                if (_internalListContext.ValueChanged.HasDelegate)
+                if (InternalListContext.ValueChanged.HasDelegate)
                 {
-                    await _internalListContext.ValueChanged.InvokeAsync(Value);
+                    await InternalListContext.ValueChanged.InvokeAsync(Value);
                 }
-                if (_internalListContext.SelectedOptionChanged.HasDelegate)
+                if (InternalListContext.SelectedOptionChanged.HasDelegate)
                 {
-                    await _internalListContext.SelectedOptionChanged.InvokeAsync();
+                    await InternalListContext.SelectedOptionChanged.InvokeAsync();
                 }
             }
         }
@@ -102,5 +102,5 @@ public partial class FluentOption<TOption> : FluentComponentBase, IDisposable wh
         Id = Identifier.NewId();
     }
 
-    public void Dispose() => _internalListContext.Unregister(this);
+    public void Dispose() => InternalListContext.Unregister(this);
 }
