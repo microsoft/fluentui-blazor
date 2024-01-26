0c2837f64d8f0bd943a0e5669c8e62a56c8f788b
Denis Voituron <dvoituron@outlook.com>
2024-01-26 09:18:29 +0100

    Add Popover Threshold attributes (#1384)
    Co-authored-by: Vincent Baaij <vnbaaij@outlook.com>

--- src/Core/Components/Popover/FluentPopover.razor.cs
+++ src/Core/Components/Popover/FluentPopover.razor.cs
@@ -13,44 +13,56 @@ public partial class FluentPopover : FluentComponentBase
         .Build();
 
     /// <summary>
-    /// Gets or sets the id of the component the popover is positioned relative to
+    /// Gets or sets the id of the component the popover is positioned relative to.
     /// </summary>
     [Parameter]
     public string AnchorId { get; set; } = string.Empty;
 
     /// <summary>
-    /// The default horizontal position of the region relative to the anchor element
+    /// Gets or sets the default horizontal position of the region relative to the anchor element.
     /// Default is unset. See <seealso cref="HorizontalPosition"/>
     /// </summary>
     [Parameter]
     public HorizontalPosition? HorizontalPosition { get; set; } = FluentUI.HorizontalPosition.Unset;
 
     /// <summary>
-    /// Gets or sets popover opened state
+    /// How short the space allocated to the default position has to be before the tallest area is selected for layout.
+    /// </summary>
+    [Parameter]
+    public int VerticalThreshold { get; set; } = 0;
+
+    /// <summary>
+    /// How narrow the space allocated to the default position has to be before the widest area is selected for layout.
+    /// </summary>
+    [Parameter]
+    public int HorizontalThreshold { get; set; } = 0;
+
+    /// <summary>
+    /// Gets or sets popover opened state.
     /// </summary>
     [Parameter]
     public bool Open { get; set; }
 
     /// <summary>
-    /// Callback for when open state changes
+    /// Callback for when open state changes.
     /// </summary>
     [Parameter]
     public EventCallback<bool> OpenChanged { get; set; }
 
     /// <summary>
-    /// Contents of the header part of the popover
+    /// Gets or sets the content of the header part of the popover.
     /// </summary>
     [Parameter]
     public RenderFragment? Header { get; set; }
 
     /// <summary>
-    /// Contents of the body part of the popover
+    /// Gets or sets the content of the body part of the popover.
     /// </summary>
     [Parameter]
     public RenderFragment? Body { get; set; }
 
     /// <summary>
-    /// Contents of the footer part of the popover
+    /// Gets or sets the content of the footer part of the popover.
     /// </summary>
     [Parameter]
     public RenderFragment? Footer { get; set; }
