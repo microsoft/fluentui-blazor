7d8c9f2b25178e62e3f90957271bbc9a7facba1c
Vincent Baaij <vnbaaij@outlook.com>
2024-01-30 11:44:15 +0100

    Styling fixes

--- src/Core/Components/NavMenu/FluentNavGroup.razor.cs
+++ src/Core/Components/NavMenu/FluentNavGroup.razor.cs
@@ -1,3 +1,7 @@
+// ------------------------------------------------------------------------
+// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------
+
 using Microsoft.AspNetCore.Components;
 using Microsoft.FluentUI.AspNetCore.Components.Utilities;
 
