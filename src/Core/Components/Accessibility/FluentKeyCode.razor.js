5bb957f25c07d9c366dfc4235b1c75aca0989c7c
Denis Voituron <dvoituron@outlook.com>
2024-01-30 16:05:17 +0100

    Add stopPropagation and TargetId (#1408)

--- src/Core/Components/Accessibility/FluentKeyCode.razor.js
+++ src/Core/Components/Accessibility/FluentKeyCode.razor.js
@@ -1,10 +1,16 @@
-﻿export function RegisterKeyCode(id, onlyCodes, excludeCodes, dotNetHelper) {
+export function RegisterKeyCode(id, onlyCodes, excludeCodes, stopPropagation, dotNetHelper) {
     const element = document.getElementById(id);
     if (!!element) {
         element.addEventListener('keydown', function (e) {
             const keyCode = e.which || e.keyCode || e.charCode;
 
+            if (stopPropagation) {
+                e.stopPropagation();
+            }
+
             if (!!dotNetHelper && !!dotNetHelper.invokeMethodAsync) {
+
+                const targetId = e.currentTarget?.id ?? "";
 
                 // Exclude
                 if (excludeCodes.length > 0 && excludeCodes.includes(keyCode)) {
@@ -13,7 +19,7 @@
 
                 // All or Include only
                 if (onlyCodes.length == 0 || (onlyCodes.length > 0 && onlyCodes.includes(keyCode))) {
-                    dotNetHelper.invokeMethodAsync("OnKeyDownRaisedAsync", keyCode, e.key, e.ctrlKey, e.shiftKey, e.altKey, e.metaKey, e.location);
+                    dotNetHelper.invokeMethodAsync("OnKeyDownRaisedAsync", keyCode, e.key, e.ctrlKey, e.shiftKey, e.altKey, e.metaKey, e.location, targetId);
                     return;
                 }
             }
