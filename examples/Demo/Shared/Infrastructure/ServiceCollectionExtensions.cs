8d811ff4b3234dc6f124d19a3cd5f873e6f5be08
Luke Parker [SSW] <10430890+Hona@users.noreply.github.com>
2024-02-28 02:54:55 +1100

    feat: Add global search bar (#1583)
    * feat: Add global search bar
    
    * fix: warning
    
    * refactor: Code review comments
    
    * refactor: Move injected services to code behind
    
    * refactor: formatting
    
    ---------
    
    Co-authored-by: Vincent Baaij <vnbaaij@outlook.com>

--- examples/Demo/Shared/Infrastructure/ServiceCollectionExtensions.cs
+++ examples/Demo/Shared/Infrastructure/ServiceCollectionExtensions.cs
@@ -14,6 +14,7 @@ public static class ServiceCollectionExtensions
     {
         services.AddScoped<CacheStorageAccessor>();
         services.AddHttpClient<IStaticAssetService, HttpBasedStaticAssetService>();
+        services.AddSingleton<DemoNavProvider>();
 
         return services;
     }
