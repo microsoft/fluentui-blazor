f4edd2af49a7c3124096286abb905922cfda5dc7
Vincent Baaij <vnbaaij@outlook.com>
2024-02-28 13:44:58 +0100

    MenuButton changes and additions (#1602)
    * Fix #1593 and #1594
    
    * Add documentation

--- src/Core/Components/MenuButton/FluentMenuButton.razor.cs
+++ src/Core/Components/MenuButton/FluentMenuButton.razor.cs
@@ -1,34 +1,70 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.FluentUI.AspNetCore.Components.Utilities;
 
 namespace Microsoft.Fast.Components.FluentUI;
 
 public partial class FluentMenuButton : FluentComponentBase
 {
     private bool _visible;
+    private Color _iconColor = Color.Fill;
 
+    protected string? MenuStyleValue => new StyleBuilder(MenuStyle)
+        .AddStyle("position", "relative")
+        .Build();
+
+    /// <summary>
+    /// Gets or sets a reference to the button.
+    /// </summary>
     [Parameter]
     public FluentButton? Button { get; set; }
 
+    /// <summary>
+    /// Gets or sets the button appearance.
+    /// </summary>
+    [Parameter]
+    public Appearance ButtonAppearance { get; set; } = Appearance.Accent;
+
+    /// <summary>
+    /// Gets or sets a reference to the menu.
+    /// </summary>
     [Parameter]
     public FluentMenu? Menu { get; set; }
 
+    /// <summary>
+    /// Gets or sets the texts shown on th button.
+    /// </summary>
     [Parameter]
     public string? Text { get; set; }
 
+    /// <summary>
+    /// Gets or sets the button style.
+    /// </summary>
     [Parameter]
     public string? ButtonStyle { get; set; }
 
+    /// <summary>
+    /// Gets or sets the menu style.
+    /// </summary>
     [Parameter]
     public string? MenuStyle { get; set; }
 
-
+    /// <summary>
+    /// Gets or sets the items to show in the menu.
+    /// </summary>
     [Parameter]
     public Dictionary<string, string> Items { get; set; } = new();
 
+    /// <summary>
+    /// The callback to invoke when a menu item is chosen.
+    /// </summary>
     [Parameter]
-
     public EventCallback<MenuChangeEventArgs> OnMenuChanged { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        _iconColor = ButtonAppearance == Appearance.Accent ? Color.Fill : Color.FillInverse;
+    }
 
     private void ToggleMenu()
     {
