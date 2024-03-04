42c61c9fb94072c26a1b1209f1fca89cf4fef00f
Vincent Baaij <vnbaaij@outlook.com>
2024-03-04 18:29:36 +0100

    Fix native autocomplete for combobox (#1629)

--- src/Core/Components/List/FluentCombobox.razor.cs
+++ src/Core/Components/List/FluentCombobox.razor.cs
@@ -1,38 +1,48 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.Fast.Components.FluentUI.Utilities;
+using Microsoft.JSInterop;
 
 namespace Microsoft.Fast.Components.FluentUI;
 
 [CascadingTypeParameter(nameof(TOption))]
 public partial class FluentCombobox<TOption> : ListComponentBase<TOption> where TOption : notnull
 {
+    private const string JAVASCRIPT_FILE = "./_content/Microsoft.Fast.Components.FluentUI/Components/List/FluentCombobox.razor.js";
+
+    /// <summary />
+    [Inject]
+    private IJSRuntime JSRuntime { get; set; } = default!;
+
+    /// <summary />
+    private IJSObjectReference? Module { get; set; }
+
     /// <summary>
-    /// Gets or sets if the element is auto completes. See <seealso cref="FluentUI.ComboboxAutocomplete"/>
+    /// Gets or sets a value indicating whether the element auto completes. See <seealso cref="AspNetCore.Components.ComboboxAutocomplete"/>
     /// </summary>
     [Parameter]
     public ComboboxAutocomplete? Autocomplete { get; set; }
 
     /// <summary>
-    /// The open attribute.
+    /// Gets or sets the open attribute.
     /// </summary>
     [Parameter]
     public bool? Open { get; set; }
 
     /// <summary>
-    /// Sets the placeholder value of the element, generally used to provide a hint to the user.
+    /// Gets or sets the placeholder value of the element, generally used to provide a hint to the user.
     /// </summary>
     [Parameter]
     public string? Placeholder { get; set; }
 
     /// <summary>
-    /// The placement for the listbox when the combobox is open.
-    /// See <seealso cref="FluentUI.SelectPosition"/>
+    /// Gets or sets the placement for the listbox when the combobox is open.
+    /// See <seealso cref="AspNetCore.Components.SelectPosition"/>
     /// </summary>
     [Parameter]
     public SelectPosition? Position { get; set; } = SelectPosition.Below;
 
     /// <summary>
-    /// Gets or sets the visual appearance. See <seealso cref="FluentUI.Appearance"/>
+    /// Gets or sets the visual appearance. See <seealso cref="AspNetCore.Components.Appearance"/>
     /// </summary>
     [Parameter]
     public Appearance? Appearance { get; set; }
@@ -41,9 +51,66 @@ public partial class FluentCombobox<TOption> : ListComponentBase<TOption> where 
         .AddStyle("min-width", Width, when: !string.IsNullOrEmpty(Width))
         .Build();
 
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            if (!string.IsNullOrEmpty(Id))
+            {
+                Module ??= await JSRuntime.InvokeAsync<IJSObjectReference>("import", JAVASCRIPT_FILE);
+                await Module.InvokeVoidAsync("setControlAttribute", Id, "autocomplete", "off");
+            }
+        }
+    }
+
     public override async Task SetParametersAsync(ParameterView parameters)
     {
         parameters.SetParameterProperties(this);
+
+        var isSetSelectedOption = false;
+        TOption? newSelectedOption = default;
+
+        foreach (var parameter in parameters)
+        {
+            switch (parameter.Name)
+            {
+                case nameof(SelectedOption):
+                    isSetSelectedOption = true;
+                    newSelectedOption = (TOption?)parameter.Value;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (isSetSelectedOption && !Equals(_currentSelectedOption, newSelectedOption))
+        {
+            if (Items != null)
+            {
+                if (Items.Contains(newSelectedOption))
+                {
+                    _currentSelectedOption = newSelectedOption;
+                }
+                else
+                {
+                    // If the selected option is not in the list of items, reset the selected option
+                    _currentSelectedOption = SelectedOption = default;
+                    await SelectedOptionChanged.InvokeAsync(SelectedOption);
+                }
+            }
+            else
+            {
+                // If Items is null, we don't know if the selected option is in the list of items, so we just set it
+                _currentSelectedOption = newSelectedOption;
+            }
+
+            // Sync Value from selected option.
+            // If it is null, we set it to the default value so the attribute is not deleted & the webcomponents don't throw an exception
+            Value = GetOptionValue(_currentSelectedOption) ?? string.Empty;
+            await ValueChanged.InvokeAsync(Value);
+        }
 
         await base.SetParametersAsync(ParameterView.Empty);
     }
@@ -52,7 +119,7 @@ public partial class FluentCombobox<TOption> : ListComponentBase<TOption> where 
     {
         if (e.Value is not null && Items is not null)
         {
-            string? value = e.Value.ToString();
+            var value = e.Value.ToString();
             TOption? item = Items.FirstOrDefault(i => GetOptionText(i) == value);
 
             if (item is null)
@@ -60,23 +127,34 @@ public partial class FluentCombobox<TOption> : ListComponentBase<TOption> where 
                 SelectedOption = default;
 
                 if (SelectedOptionChanged.HasDelegate)
+                {
                     await SelectedOptionChanged.InvokeAsync(SelectedOption);
+                }
 
                 if (ValueChanged.HasDelegate)
+                {
+                    Value = value;
                     await ValueChanged.InvokeAsync(value);
+                }
 
                 StateHasChanged();
             }
             else
+            {
                 await OnSelectedItemChangedHandlerAsync(item);
+            }
         }
     }
 
     protected override string? GetOptionValue(TOption? item)
     {
         if (item != null)
+        {
             return OptionText.Invoke(item) ?? OptionValue.Invoke(item) ?? item.ToString();
+        }
         else
+        {
             return null;
+        }
     }
 }
