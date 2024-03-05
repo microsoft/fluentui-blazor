9a560a468c36cc648aa7e9ab65ee6c0324a1e305
Vincent Baaij <vnbaaij@outlook.com>
2024-03-05 11:07:47 +0100

    Remove unnecessary using statement

--- src/Core/Components/DataGrid/Columns/TemplateColumn.cs
+++ src/Core/Components/DataGrid/Columns/TemplateColumn.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
-using Microsoft.FluentUI.AspNetCore.Components.DataGrid.Infrastructure;
 
 namespace Microsoft.FluentUI.AspNetCore.Components;
 
