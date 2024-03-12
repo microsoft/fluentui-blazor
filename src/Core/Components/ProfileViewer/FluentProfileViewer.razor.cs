05aecd15df61198feaacac19c3d5bcf7857b580b
dvoituron <dvoituron@outlook.com>
2024-03-12 17:46:59 +0100

    Add extra properties

--- src/Core/Components/ProfileViewer/FluentProfileViewer.razor.cs
+++ src/Core/Components/ProfileViewer/FluentProfileViewer.razor.cs
@@ -16,5 +16,56 @@ public partial class FluentProfileViewer : FluentComponentBase
     private bool PopoverVisible { get; set; } = false;
 
     [Parameter]
+    public RenderFragment? HeaderTemplate { get; set; }
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public RenderFragment? FooterTemplate { get; set; }
+
+    /// <summary>
+    /// Gets or sets the status to show. See <see cref="PresenceStatus"/> for options.
+    /// </summary>
+    [Parameter]
+    public PresenceStatus? Status { get; set; }
+
+    /// <summary>
+    /// Gets or sets the title to show on hover. If not provided, the status will be used.
+    /// </summary>
+    [Parameter]
+    public string? StatusTitle { get; set; }
+
+    [Parameter]
+    public string? Initials { get; set; }
+
+    [Parameter]
+    public string? FullName { get; set; }
+
+    [Parameter]
+    public string? CompanyName { get; set; }
+
+    [Parameter]
+    public string? EMail { get; set; }
+
+    [Parameter]
     public string? Image { get; set; }
+
+    [Parameter]
+    public string? ImageSize { get; set; } = "64px";
+
+    [Parameter]
+    public string? ButtonSize { get; set; } = "32px";
+
+    [Parameter]
+    public string? SignOutLabel { get; set; } = "Sign out";
+
+    [Parameter]
+    public EventCallback OnSignOut { get; set; }
+
+    [Parameter]
+    public string? ViewAccountLabel { get; set; } = "View Account";
+
+    [Parameter]
+    public EventCallback OnViewAccount { get; set; }
 }
