2c4c2f7aafb6d655a3da31e98903ccc6857c8708
Vincent Baaij <vnbaaij@outlook.com>
2024-03-20 14:41:54 +0100

    Try to include script as ts

--- src/Core.Assets/src/index.ts
+++ src/Core.Assets/src/index.ts
@@ -2,6 +2,7 @@ export * from '@fluentui/web-components/dist/web-components'
 import { SplitPanels } from './SplitPanels'
 import { DesignTheme } from './DesignTheme'
 import { FluentPageScript, onEnhancedLoad } from './FluentPageScript'
+import { TouchEmulator } from './TouchEmulator' 
 
 interface Blazor {
   registerCustomEventType: (
@@ -305,6 +306,8 @@ export function afterStarted(blazor: Blazor, mode: string) {
 export function beforeStart(options: any) {
   customElements.define("fluent-design-theme", DesignTheme);
   customElements.define("split-panels", SplitPanels);
+
+  new TouchEmulator();
 
   beforeStartCalled = true;
 }
