e97b49ae6cd8b07590a6c6df994b7af3855930eb
dvoituron <dvoituron@outlook.com>
2024-03-25 14:39:55 +0100

    Update the script to be standalone

--- .azure-devops/common/update-assembly-version.ps1
+++ .azure-devops/common/update-assembly-version.ps1
@@ -1,15 +1,50 @@
-# **/*.csproj
-# !**/src/Templates/content/**/*.csproj
+<#
+.DESCRIPTION
+    Update all .csproj files, adding these properties, excluding the files from $excludePatterns
+    - `AssemblyVersion = $assemblyVersion`,
+    - `FileVersion = $assemblyVersion`,
+    - `InformationalVersion = $assemblyVersion`,
+    - `Version = $packageVersion`,
+
+.PARAMETER sourcePath
+    Source path to list all .csproj files, by default the current path is used.
+
+.PARAMETER excludePatterns
+    List of files to exclude so as not to update the contents of the .csproj file.
+
+.PARAMETER assemblyVersion
+    Assembly version.
+
+.PARAMETER packageVersion
+    Package version.
+
+.EXAMPLE
+    $> .\update-assembly-version -sourcePath "./" -excludePatterns "**/src/Templates/content/**/*.csproj", "**/tests/TemplateValidation/**/*.csproj" -assemblyVersion "1.2.3" -packageVersion "1.2.3"
+
+#>
+
+param (
+  [string]$sourcePath,
+  [string[]]$excludePatterns = @(),
+  [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$assemblyVersion,
+  [string]$packageVersion
+)
+
 
 # Define the patterns
-$includePattern = "**/*.csproj"
-$excludePatterns = @(
-  "**/src/Templates/content/**/*.csproj",
-  "**/tests/TemplateValidation/**/*.csproj")
-$assemblyVersion = "1.2.3"
-$packageVersion = "7.8.9"
+$includePattern = "$sourcePath/**/*.csproj"
+
+if ($assemblyVersion -eq $null) {
+  $assemblyVersion = "0.0.1"
+}
+
+if ($packageVersion -eq $null) {
+  $packageVersion = $assemblyVersion
+}
 
 Write-Host "Add or update assemblies versions '$assemblyVersion' and package versions '$packageVersion'."
+Write-Host " - include: $includePattern."
+Write-Host " - exclude: $excludePatterns."
 
 # ------------------------------------------
 # Get all files matching the include pattern
@@ -27,24 +62,26 @@ function Get-FilesFromDirectory {
 
   # Get all files matching the exclude pattern => $filteredFiles
   $excludedFiles = @() # Empty array
-  foreach ($pattern in $excludePatterns) {
-    # Create a RegEx: replacing "**" by ".*", "*" by "[^/]*"
-    $parts = $pattern -split '[\\/]'
-    foreach ($i in 0..($parts.Length - 1)) {
-      if ($parts[$i] -eq '**') {
-        $parts[$i] = '.*'
+  if ($excludePatterns.Length -gt 0) {
+    foreach ($pattern in $excludePatterns) {
+      # Create a RegEx: replacing "**" by ".*", "*" by "[^/]*"
+      $parts = $pattern -split '[\\/]'
+      foreach ($i in 0..($parts.Length - 1)) {
+        if ($parts[$i] -eq '**') {
+          $parts[$i] = '.*'
+        }
+        if ($parts[$i] -eq '*') {
+          $parts[$i] = '[^/]*'
+        }
       }
-      if ($parts[$i] -eq '*') {
-        $parts[$i] = '[^/]*'
-      }
-    }
-    $excludeRegEx = $parts -join "\/"
+      $excludeRegEx = $parts -join "\/"
 
-    # Check all files to detect the matching one
-    foreach ($file in $allFiles) {
-      $match1 = ($file.FullName -replace "\\", "/") -match $excludeRegEx
-      if ($match1) {
-        $excludedFiles += $file.FullName
+      # Check all files to detect the matching one
+      foreach ($file in $allFiles) {
+        $match1 = ($file.FullName -replace "\\", "/") -match $excludeRegEx
+        if ($match1) {
+          $excludedFiles += $file.FullName
+        }
       }
     }
 
