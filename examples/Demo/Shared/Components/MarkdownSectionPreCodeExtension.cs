11aa7016df04c5130ee96fc6a956bd0bbad5d5a5
Gary Chan <gary.chan@c0g1t8.com>
2024-03-27 10:43:11 -0700

    fixes edge case where OutputAttributesOnPre is true pass additional attributes as markdig GenericAttributes

--- examples/Demo/Shared/Components/MarkdownSectionPreCodeExtension.cs
+++ examples/Demo/Shared/Components/MarkdownSectionPreCodeExtension.cs
@@ -24,6 +24,6 @@ internal class MarkdownSectionPreCodeExtension : IMarkdownExtension
             htmlRenderer.ObjectRenderers.Remove(originalCodeBlockRenderer);
         }
 
-        htmlRenderer.ObjectRenderers.AddIfNotAlready(new MarkdownSectionPreCodeRenderer("snippet hljs-copy-wrapper"));
+        htmlRenderer.ObjectRenderers.AddIfNotAlready(new MarkdownSectionPreCodeRenderer("{.snippet .hljs-copy-wrapper}"));
     }
 }
--- examples/Demo/Shared/Components/MarkdownSectionPreCodeRenderer.cs
+++ examples/Demo/Shared/Components/MarkdownSectionPreCodeRenderer.cs
@@ -1,3 +1,5 @@
+using Markdig.Extensions.GenericAttributes;
+using Markdig.Helpers;
 using Markdig.Parsers;
 using Markdig.Renderers;
 using Markdig.Renderers.Html;
@@ -56,26 +58,13 @@ internal class MarkdownSectionPreCodeRenderer : HtmlObjectRenderer<CodeBlock>
         {
             if (renderer.EnableHtmlForBlock)
             {
-                if (_preTagClass is string)
-                {
-                    renderer.Write($"<pre class=\"{_preTagClass}\"");
-                }
-                else
-                {
-                    renderer.Write("<pre");
-                }
+                renderer.Write("<pre");
 
-                if (OutputAttributesOnPre)
-                {
-                    renderer.WriteAttributes(obj);
-                }
+                WritePreAttributes(renderer, obj, _preTagClass);
 
                 renderer.Write("><code");
 
-                if (!OutputAttributesOnPre)
-                {
-                    renderer.WriteAttributes(obj);
-                }
+                WriteCodeAttributes(renderer, obj, null);
 
                 renderer.Write('>');
             }
@@ -89,5 +78,60 @@ internal class MarkdownSectionPreCodeRenderer : HtmlObjectRenderer<CodeBlock>
         }
 
         renderer.EnsureLine();
+    }
+
+    private void WritePreAttributes(HtmlRenderer renderer, CodeBlock obj, string? preGenericAttributes)
+    {
+        HtmlAttributes? orig = null;
+
+        if (OutputAttributesOnPre)
+        {
+            orig = obj.TryGetAttributes();
+        }
+
+        WriteElementAttributes(renderer, orig, preGenericAttributes);
+    }
+
+    private void WriteCodeAttributes(HtmlRenderer renderer, CodeBlock obj, string? codeGenericAttributes)
+    {
+        HtmlAttributes? orig = null;
+
+        if (!OutputAttributesOnPre)
+        {
+            orig = obj.TryGetAttributes();
+        }
+
+        WriteElementAttributes(renderer, orig, codeGenericAttributes);
+    }
+    static private void WriteElementAttributes(HtmlRenderer renderer, HtmlAttributes? fromCodeBlock, string? genericAttributes)
+    {
+        // origin code block had no attributes
+        fromCodeBlock ??= new HtmlAttributes();
+
+        // append if any additional attributes provided
+        var ss = new StringSlice(genericAttributes);
+        if (!ss.IsEmpty && GenericAttributesParser.TryParse(ref ss, out var attributes))
+        {
+            if (fromCodeBlock != null)
+            {
+                if (attributes.Classes != null)
+                {
+                    foreach (var a in attributes.Classes)
+                    {
+                        fromCodeBlock.AddClass(a);
+                    }
+                }
+                if (attributes.Properties != null)
+                {
+                    foreach (var pr in attributes.Properties)
+                    {
+                        fromCodeBlock.AddProperty(pr.Key, pr.Value!);
+                    }
+                }
+            }
+        }
+
+        //
+        renderer.WriteAttributes(fromCodeBlock);
     }
 }
