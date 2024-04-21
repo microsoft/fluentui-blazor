d2f5df35dbbab8505b52d77733037ff22754ae28
Stefan Jansson <stefan@veum.se>
2024-04-21 20:56:04 +0200

    Added unittests

--- src/Core/Components/DateTime/FluentDatePicker.razor.cs
+++ src/Core/Components/DateTime/FluentDatePicker.razor.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
