0a40a6812a08c95e0bdc098a2047513bd1046b81
Denis Voituron <dvoituron@outlook.com>
2024-04-24 10:54:15 +0200

    [Demo site] New Video and PageTitle (#1925)
    * Add new Video
    
    * Add PageTitle (part 1)
    
    * Update PageTitle
    
    ---------
    
    Co-authored-by: Vincent Baaij <vnbaaij@outlook.com>

--- examples/Demo/Shared/App.razor.cs
+++ examples/Demo/Shared/App.razor.cs
@@ -1,6 +1,11 @@
 namespace FluentUI.Demo.Shared;
 public partial class App
 {
+    public static string PageTitle(string page)
+    {
+        return $"{page} - FluentUI Blazor Components";
+    }
+
     public const string MESSAGES_NOTIFICATION_CENTER = "NOTIFICATION_CENTER";
     public const string MESSAGES_TOP = "TOP";
     public const string MESSAGES_DIALOG = "DIALOG";
