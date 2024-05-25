03bab4b0f7195f4c272f2410931ce9a0aed2396f
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-05-25 14:08:55 +0700

    Update FluentLayout to support Orientation. Has Base class `layout` and two Modifiers layout--vertical layout--horizontal

--- src/Core/Components/Layout/FluentLayout.razor.cs
+++ src/Core/Components/Layout/FluentLayout.razor.cs
@@ -7,9 +7,17 @@ public partial class FluentLayout : FluentComponentBase
 {
     protected string? ClassValue => new CssBuilder(Class)
         .AddClass("layout")
+        .AddClass("layout--horizontal", () => Orientation == Orientation.Horizontal)
+        .AddClass("layout--vertical", () => Orientation == Orientation.Vertical)
         .Build();
 
     protected string? StyleValue => new StyleBuilder(Style).Build();
+
+    /// <summary>
+    /// Gets or sets the orientation of the stacked components.
+    /// </summary>
+    [Parameter]
+    public Orientation Orientation { get; set; } = Orientation.Vertical;
 
     /// <summary>
     /// Gets or sets the content to be rendered inside the component.
