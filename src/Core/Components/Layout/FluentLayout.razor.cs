f5c319a742db7ae6166f5ed308f846e72bac630a
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-05-26 00:45:44 +0700

    Add Orientation support for FluentLayout (#2096)
    * In Option<TType> change the type of `Text` to string?  If it is TType it only adds value for TType of string otherwise it makes no sense if Text is some Ojbect.
    
    * Update the demo apps to use Text as a string.
    
    * Use string literal instead of ToString.
    
    * Now we can remove redundant .ToString
    
    * Update FluentLayout to support Orientation.
    Has Base class `layout` and two Modifiers
    layout--vertical
    layout--horizontal
    
    * Use an orientation attribute instead of separate css classes.
    
    * Remove FluentLayoutTests.cs from TODO.
    
    * Add Layout tests
    
    ---------
    
    Co-authored-by: Vincent Baaij <vnbaaij@outlook.com>

--- src/Core/Components/Layout/FluentLayout.razor.cs
+++ src/Core/Components/Layout/FluentLayout.razor.cs
@@ -7,11 +7,19 @@ namespace Microsoft.Fast.Components.FluentUI;
 
 public partial class FluentLayout : FluentComponentBase
 {
+    private string? _orientation => Orientation == Orientation.Horizontal ? Orientation.ToAttributeValue() : null;
+
     protected string? ClassValue => new CssBuilder(Class)
         .AddClass("layout")
         .Build();
 
     protected string? StyleValue => new StyleBuilder(Style).Build();
+
+    /// <summary>
+    /// Gets or sets the orientation of the stacked components.
+    /// </summary>
+    [Parameter]
+    public Orientation Orientation { get; set; } = Orientation.Vertical;
 
     /// <summary>
     /// Gets or sets the content to be rendered inside the component.
