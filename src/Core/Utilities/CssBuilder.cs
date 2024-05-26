4eac9c06001ca57b6f99a463d0c0ac96e7c2b234
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2024-05-26 23:38:10 +0700

    Update CssBuilder to properly support adding multiple classes.

--- src/Core/Utilities/CssBuilder.cs
+++ src/Core/Utilities/CssBuilder.cs
@@ -11,7 +11,7 @@ public readonly struct CssBuilder
 
     public CssBuilder()
     {
-        _classes = [];
+        _classes = new HashSet<string>();
         _userClasses = null;
     }
 
@@ -32,7 +32,7 @@ public readonly struct CssBuilder
     /// </summary>
     /// <param name="value">CSS Class to add</param>
     /// <returns>CssBuilder</returns>
-    public CssBuilder AddClass(string? value) => AddRaw(value);
+    public CssBuilder AddClass(string? value) => AddClasses(value);
 
     /// <summary>
     /// Adds a conditional CSS Class to the builder with space separator.
@@ -40,7 +40,7 @@ public readonly struct CssBuilder
     /// <param name="value">CSS Class to conditionally add.</param>
     /// <param name="when">Condition in which the CSS Class is added.</param>
     /// <returns>CssBuilder</returns>
-    public CssBuilder AddClass(string? value, bool when = true) => when ? AddClass(value) : this;
+    public CssBuilder AddClass(string? value, bool when = true) => when ? AddClasses(value) : this;
 
     /// <summary>
     /// Adds a conditional CSS Class to the builder with space separator.
@@ -48,7 +48,38 @@ public readonly struct CssBuilder
     /// <param name="value">CSS Class to conditionally add.</param>
     /// <param name="when">Condition in which the CSS Class is added.</param>
     /// <returns>CssBuilder</returns>
-    public CssBuilder AddClass(string? value, Func<bool>? when = null) => AddClass(value, when != null && when());
+    public CssBuilder AddClass(string? value, Func<bool>? when = null) => AddClasses(value, when != null && when());
+
+    /// <summary>
+    /// Adds multiple CSS Classes to the builder with space separator.
+    /// </summary>
+    /// <param name="values">Space-separated CSS Classes to add</param>
+    /// <returns>CssBuilder</returns>
+    public CssBuilder AddClasses(string? values)
+    {
+        if (!string.IsNullOrWhiteSpace(values))
+        {
+            var classes = values.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            _classes.UnionWith(classes);
+        }
+        return this;
+    }
+
+    /// <summary>
+    /// Adds multiple CSS Classes to the builder with space separator, based on a condition.
+    /// </summary>
+    /// <param name="value">Space-separated CSS Classes to add</param>
+    /// <param name="when">Condition in which the CSS Classes are added.</param>
+    /// <returns>CssBuilder</returns>
+    public CssBuilder AddClasses(string? value, bool when) => when ? AddClasses(value) : this;
+
+    /// <summary>
+    /// Adds multiple CSS Classes to the builder with space separator, based on a condition.
+    /// </summary>
+    /// <param name="value">Space-separated CSS Classes to add</param>
+    /// <param name="when">Function that returns a condition in which the CSS Classes are added.</param>
+    /// <returns>CssBuilder</returns>
+    public CssBuilder AddClasses(string? value, Func<bool> when) => when() ? AddClasses(value) : this;
 
     /// <summary>
     /// Finalize the completed CSS Classes as a string.
@@ -73,19 +104,4 @@ public readonly struct CssBuilder
     /// </summary>
     /// <returns></returns>
     public override string? ToString() => Build();
-
-    /// <summary>
-    /// Adds a raw string to the builder that will be concatenated with the next classes or value added to the builder.
-    /// </summary>
-    /// <param name="value"></param>
-    /// <returns>StyleBuilder</returns>
-    private CssBuilder AddRaw(string? value)
-    {
-        if (!string.IsNullOrWhiteSpace(value))
-        {
-            _classes.Add(value.Trim());
-        }
-
-        return this;
-    }
 }
