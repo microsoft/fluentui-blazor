eafc730049965ed5ecc9bc0cba38721209af4861
Gary Chan <garychan@live.com>
2024-05-29 06:45:32 -0700

    [MarkdownSection] Optimize to minimize number of calls to OnContentConverted (#2092)
    * reduce number of renders
    
    * optimize to a single call to OnContentConverted
    
    * clean up code for optimization
    
    * fixes page to show table after a single call to OnFreshTableOfContents
    
    * rerender contents when markdown source is changed
    
    * reduce number of renders
    
    * optimize to a single call to OnContentConverted
    
    * clean up code for optimization
    
    * fixes page to show table after a single call to OnFreshTableOfContents
    
    * rerender contents when markdown source is changed
    
    * resolve pr comments
    
    ---------
    
    Co-authored-by: Vincent Baaij <vnbaaij@outlook.com>
    Co-authored-by: Denis Voituron <dvoituron@outlook.com>

--- examples/Demo/Shared/Components/MarkdownSection.razor.cs
+++ examples/Demo/Shared/Components/MarkdownSection.razor.cs
@@ -10,82 +10,121 @@ namespace FluentUI.Demo.Shared.Components;
 
 public partial class MarkdownSection : FluentComponentBase
 {
+    private IJSObjectReference _jsModule = default!;
+    private bool _markdownChanged = false;
     private string? _content;
-    private bool _raiseContentConverted;
+    private string? _fromAsset;
+
+    [Inject]
+    protected IJSRuntime JSRuntime { get; set; } = default!;
 
     [Inject]
     private IStaticAssetService StaticAssetService { get; set; } = default!;
-
 
     /// <summary>
     /// Gets or sets the Markdown content 
     /// </summary>
     [Parameter]
-    public string? Content { get; set; }
+    public string? Content
+    {
+        get => _content;
+        set
+        {
+            if (_content is not null && !_content.Equals(value))
+            {
+                _markdownChanged = true;
+            }
+            _content = value;
+        }
+    }
 
     /// <summary>
     /// Gets or sets asset to read the Markdown from
     /// </summary>
     [Parameter]
-    public string? FromAsset { get; set; }
+    public string? FromAsset
+    {
+        get => _fromAsset;
+        set
+        {
+            if (_fromAsset is not null && !_fromAsset.Equals(value))
+            {
+                _markdownChanged = true;
+            }
+            _fromAsset = value;
+        }
+    }
 
     [Parameter]
     public EventCallback OnContentConverted { get; set; }
 
-    public string? InternalContent
-    {
-        get => _content;
-        set
-        {
-            _content = value;
-            HtmlContent = ConvertToMarkupString(_content);
-
-
-            if (OnContentConverted.HasDelegate)
-            {
-                OnContentConverted.InvokeAsync();
-            }
-            _raiseContentConverted = true;
-            StateHasChanged();
-        }
-    }
-
     public MarkupString HtmlContent { get; private set; }
-
 
     protected override void OnInitialized()
     {
         if (Content is null && string.IsNullOrEmpty(FromAsset))
+        {
             throw new ArgumentException("You need to provide either Content or FromAsset parameter");
-
-        InternalContent = Content;
+        }
     }
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
-
-        if (firstRender && !string.IsNullOrEmpty(FromAsset))
+        if (firstRender)
         {
-            InternalContent = await StaticAssetService.GetAsync(FromAsset);
+            // import code for highlighting code blocks
+            _jsModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import",
+                "./_content/FluentUI.Demo.Shared/Components/MarkdownSection.razor.js");
         }
 
-        if (_raiseContentConverted)
+        if (firstRender || _markdownChanged)
         {
-            _raiseContentConverted = false;
+            _markdownChanged = false;
+
+            // create markup from markdown source
+            HtmlContent = await MarkdownToMarkupStringAsync();
+            StateHasChanged();
+
+            // notify that content converted from markdown 
             if (OnContentConverted.HasDelegate)
             {
                 await OnContentConverted.InvokeAsync();
             }
-
+            await _jsModule.InvokeVoidAsync("highlight");
+            await _jsModule.InvokeVoidAsync("addCopyButton");
         }
     }
 
+    /// <summary>
+    /// Converts markdown, provided in Content or from markdown file stored as a static asset, to MarkupString for rendering.
+    /// </summary>
+    /// <returns>MarkupString</returns>
+    private async Task<MarkupString> MarkdownToMarkupStringAsync()
+    {
+        string? markdown;
+        if (string.IsNullOrEmpty(FromAsset))
+        {
+            markdown = Content;
+        }
+        else
+        {
+            markdown = await StaticAssetService.GetAsync(FromAsset);
+        }
+
+        return ConvertToMarkupString(markdown);
+    }
     private static MarkupString ConvertToMarkupString(string? value)
     {
         if (!string.IsNullOrWhiteSpace(value))
         {
+            var builder = new MarkdownPipelineBuilder()
+                    .UseAdvancedExtensions()
+                    .Use<MarkdownSectionPreCodeExtension>();
+
+            var pipeline = builder.Build();
+
             // Convert markdown string to HTML
-            string? html = Markdown.ToHtml(value, new MarkdownPipelineBuilder().UseAdvancedExtensions().Build());
+            var html = Markdown.ToHtml(value, pipeline);
 
             // Return sanitized HTML as a MarkupString that Blazor can render
             return new MarkupString(html);
