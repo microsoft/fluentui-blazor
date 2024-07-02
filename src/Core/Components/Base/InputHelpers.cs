55c3100e25a78444b6b89eba15bd6012bf1932d2
daniele.corsini@corsinvest.it <daniele.corsini@corsinvest.it>
2024-07-02 08:48:21 +0200

    Add sbyte to number filed

--- src/Core/Components/Base/InputHelpers.cs
+++ src/Core/Components/Base/InputHelpers.cs
@@ -4,7 +4,6 @@ namespace Microsoft.FluentUI.AspNetCore.Components;
 
 internal static class InputHelpers<TValue>
 {
-
     public static string GetMaxValue()
     {
         Type? targetType = Nullable.GetUnderlyingType(typeof(TValue)) ?? typeof(TValue);
@@ -51,9 +50,20 @@ internal static class InputHelpers<TValue>
         return value;
     }
 
+
+    internal static void ValidateSByteInputs(string max, string min)
+    {
+        var maxValue = Convert.ToSByte(max, CultureInfo.InvariantCulture);
+        var minValue = Convert.ToSByte(min, CultureInfo.InvariantCulture);
+
+        if (maxValue < minValue)
+        {
+            throw new ArgumentException("Float Max value is smaller than Min value.");
+        }
+    }
+
     internal static void ValidateIntegerInputs(string? max, string? min)
     {
-
         var maxValue = Convert.ToInt32(max);
         var minValue = Convert.ToInt32(min);
 
@@ -61,7 +71,6 @@ internal static class InputHelpers<TValue>
         {
             throw new ArgumentException("Integer Max value is smaller then Min value.");
         }
-
     }
 
     internal static void ValidateLongInputs(string? max, string? min)
@@ -137,6 +146,11 @@ internal static class InputHelpers<TValue>
             return; //nothing to validate
         }
 
+        if (typeof(TValue) == typeof(sbyte))
+        {
+            ValidateSByteInputs(max, min);
+        }
+
         if (typeof(TValue) == typeof(int))
         {
             ValidateIntegerInputs(max, min);
@@ -149,7 +163,6 @@ internal static class InputHelpers<TValue>
 
         if (typeof(TValue) == typeof(short))
         {
-
             ValidateShortInputs(max, min);
         }
 
@@ -165,7 +178,6 @@ internal static class InputHelpers<TValue>
 
         if (typeof(TValue) == typeof(decimal))
         {
-
             ValidateDecimalInputs(max, min);
         }
     }
