c5cbf373d65353de2b5d1ebb78f78e27b20a41d6
Denis Voituron <dvoituron@outlook.com>
2024-07-02 12:19:30 +0200

    Add Unit Tests

--- src/Core/Components/Dialog/Services/DialogReference.cs
+++ src/Core/Components/Dialog/Services/DialogReference.cs
@@ -14,7 +14,7 @@ public class DialogReference : IDialogReference
 
     public DialogInstance Instance { get; set; } = default!;
 
-    internal string GetKey() => $"{Instance.Parameters.GetHashCode()}_{Instance.Content.GetHashCode()}";
+    internal string GetKey() => $"{Instance.Parameters?.GetHashCode() ?? 0}_{Instance.Content?.GetHashCode() ?? 0}";
 
     public string Id { get; }
 
