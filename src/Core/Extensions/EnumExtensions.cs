ef0a77b894ed3c13a5fd2b292140a3a09a8d3e64
daniele.corsini@corsinvest.it <daniele.corsini@corsinvest.it>
2024-07-04 22:57:44 +0200

    Add new methods

--- src/Core/Extensions/EnumExtensions.cs
+++ src/Core/Extensions/EnumExtensions.cs
@@ -6,20 +6,34 @@ namespace Microsoft.FluentUI.AspNetCore.Components.Extensions;
 
 public static class EnumExtensions
 {
+    [Obsolete("This method will be removed in a future version. Use GetDisplayName instead.")]
     public static string? ToAttributeValue<TEnum>(this TEnum? value, bool lowercase = true) where TEnum : struct, Enum
         => value == null ? null : ToAttributeValue(value.Value, lowercase);
 
+    [Obsolete("This method will be removed in a future version. Use GetDisplayName instead.")]
     public static string? ToAttributeValue<TEnum>(this TEnum value, bool lowercase = true) where TEnum : struct, Enum
-        => GetDisplayOrDescription(value, lowercase);
+        => GetDescription(value, lowercase);
 
-    public static string? GetDisplayOrDescription<TEnum>(this TEnum value, bool lowercase = true) where TEnum : struct, IConvertible
+    [Obsolete("This method will be removed in a future version. Use GetDisplayName instead.")]
+    public static string? GetDescription<TEnum>(this TEnum value, bool lowercase = true) where TEnum : struct, IConvertible
     {
         if (!typeof(TEnum).IsEnum)
         {
             return null;
         }
 
-        var description = GetDisplayOrDescription(value as Enum);
+        var description = value.ToString();
+
+        FieldInfo? fieldInfo = value.GetType().GetField(value.ToString() ?? "");
+        if (fieldInfo != null)
+        {
+            var attributes = fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), true);
+            if (attributes?.Length > 0)
+            {
+                description = ((DescriptionAttribute)attributes[0]).Description;
+            }
+        }
+
         if (lowercase)
         {
             return description?.ToLowerInvariant();
@@ -28,27 +42,23 @@ public static class EnumExtensions
         return description;
     }
 
-    public static string GetDisplayOrDescription(this Enum value) 
-    {
-        var description = value.ToString();
-        var fieldInfo = value.GetType().GetField(value.ToString() ?? "");
-        if (fieldInfo != null)
-        {
-            var displayAttribute = fieldInfo.GetCustomAttribute<DisplayAttribute>(true);
-            if (displayAttribute?.Name == null)
-            {
-                var descriptionAttribute = fieldInfo.GetCustomAttribute<DescriptionAttribute>(true);
-                if (descriptionAttribute?.Description != null)
-                {
-                    description = descriptionAttribute.Description;
-                }
-            }
-            else
-            {
-                description = displayAttribute.Name;
-            }
-        }
+    public static string? GetDisplayName<TEnum>(this TEnum? value, bool lowercase = true) where TEnum : struct, Enum
+        => value?.GetDisplayName(lowercase);
 
-        return description;
+    public static string? GetDisplayName<TEnum>(this TEnum value, bool lowercase = true) where TEnum : struct, Enum
+    {
+        var description = value.GetDisplayNameInt();
+        return lowercase
+                ? description?.ToLowerInvariant()
+                : description;
+    }
+
+    public static string GetDisplayName(this Enum value) => GetDisplayNameInt(value);
+
+    private static string GetDisplayNameInt(this Enum value)
+    {
+        var memberInfo = value.GetType().GetMember(value.ToString());
+        var displayAttribute = memberInfo[0].GetCustomAttribute<DisplayAttribute>();
+        return displayAttribute?.Name ?? value.ToString();
     }
 }
