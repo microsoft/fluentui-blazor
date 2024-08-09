fc8e5d7ae06828032db56fc1a69d735f94ede55f
Denis Voituron <dvoituron@outlook.com>
2024-08-09 17:58:51 +0200

    Ignore .map

--- src/Core/Components/Button/FluentButton.razor.ts
+++ src/Core/Components/Button/FluentButton.razor.ts
@@ -2,6 +2,7 @@ export namespace Microsoft.FluentUI.Blazor {
 
   /**
    * Sample function to test the library
+   * See V5-CHANGES.md for more information
    * @returns
    */
   export function MyFunction() {
