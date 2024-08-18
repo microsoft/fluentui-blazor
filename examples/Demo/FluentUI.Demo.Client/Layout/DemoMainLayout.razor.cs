95a331d7aadb5d04e1df7d936c41a31894415301
Denis Voituron <dvoituron@outlook.com>
2024-08-18 22:45:56 +0200

    Update styles

--- examples/Demo/FluentUI.Demo.Client/Layout/DemoMainLayout.razor.cs
+++ examples/Demo/FluentUI.Demo.Client/Layout/DemoMainLayout.razor.cs
@@ -3,11 +3,26 @@
 // ------------------------------------------------------------------------
 
 using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 namespace FluentUI.Demo.Client.Layout;
 
 public partial class DemoMainLayout
 {
+    private FluentLayout? Layout;
+    private bool MenuOpened;
+    private bool ConsoleLogOpened;
+
     [Parameter]
     public RenderFragment? Body { get; set; }
+
+    protected override void OnInitialized()
+    {
+        // Reset the menu when the location changes
+        Navigation.RegisterLocationChangingHandler((e) =>
+        {
+            MenuOpened = false;
+            return ValueTask.CompletedTask;
+        });
+    }
 }
