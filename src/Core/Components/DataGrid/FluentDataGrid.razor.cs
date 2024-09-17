dfb6b7ea5cb6c6ec2a757c3b00bbd07c92b247ca
Vincent Baaij <vnbaaij@outlook.com>
2024-09-17 20:41:35 +0200

    - Re-add separate header buttons - Fix some samples - Position resize handle better - Always put sort/filter indicators behind header text - Add comments to public methods

--- src/Core/Components/DataGrid/FluentDataGrid.razor.cs
+++ src/Core/Components/DataGrid/FluentDataGrid.razor.cs
@@ -258,6 +258,9 @@ public partial class FluentDataGrid<TGridItem> : FluentComponentBase, IHandleEve
     [Parameter]
     public bool MultiLine { get; set; } = false;
 
+    [Parameter]
+    public bool HeaderCellAsButtonWithMenu { get; set; } = true;
+
     /// <summary>
     /// Gets the first (optional) SelectColumn
     /// </summary>
@@ -808,6 +811,12 @@ public partial class FluentDataGrid<TGridItem> : FluentComponentBase, IHandleEve
         //return Task.CompletedTask;
     }
 
+    /// <summary>
+    /// Resizes the column width by a discrete amount.
+    /// </summary>
+    /// <param name="columnIndex">The column to be resized</param>
+    /// <param name="widthChange">The amount of pixels to change width with</param>
+    /// <returns></returns>
     public async Task SetColumnWidthDiscreteAsync(int? columnIndex, float widthChange)
     {
         if (_gridReference is not null && Module is not null)
@@ -816,6 +825,12 @@ public partial class FluentDataGrid<TGridItem> : FluentComponentBase, IHandleEve
         }
     }
 
+    /// <summary>
+    /// Resizes the column width to the exact width specified (in pixels).
+    /// </summary>
+    /// <param name="columnIndex">The column to be resized</param>
+    /// <param name="width">The new width in pixels</param>
+    /// <returns></returns>
     public async Task SetColumnWidthExactAsync(int columnIndex, int width)
     {
         if (_gridReference is not null && Module is not null)
@@ -824,6 +839,11 @@ public partial class FluentDataGrid<TGridItem> : FluentComponentBase, IHandleEve
         }
     }
 
+    /// <summary>
+    /// Resets the column widths to their initial values as specified with the <see cref="GridTemplateColumns"/> parameter.
+    /// If no value is specified, the default value is "1fr" for each column.
+    /// </summary>
+    /// <returns></returns>
     public async Task ResetColumnWidthsAsync()
     {
         if (_gridReference is not null && Module is not null)
