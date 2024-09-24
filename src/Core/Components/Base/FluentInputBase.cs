1af4ea938595b79e65f4f95b06b45738e438ae43
Denis Voituron <dvoituron@outlook.com>
2024-09-24 10:36:15 +0200

    Refactoring FluentInputBase (removing CurrentValue)

--- src/Core/Components/Base/FluentInputBase.cs
+++ src/Core/Components/Base/FluentInputBase.cs
@@ -75,6 +75,7 @@ public abstract partial class FluentInputBase<TValue> : FluentComponentBase, IDi
     /// </summary>
     [Parameter]
     public virtual bool Autofocus { get; set; }
+
     /// <summary>
     /// Gets or sets the text used on `aria-label` attribute.
     /// </summary>
@@ -151,6 +152,19 @@ public abstract partial class FluentInputBase<TValue> : FluentComponentBase, IDi
     public virtual TValue? Value { get; set; }
 
     /// <summary>
+    /// Gets or sets the value of the input, represented as a string.
+    /// </summary>
+    protected string? ValueAsString
+    {
+        // InputBase-derived components can hold invalid states (e.g., an InputNumber being blank even when bound
+        // to an int value). So, if parsing fails, we keep the rejected string in the UI even though it doesn't
+        // match what's on the .NET model. This avoids interfering with typing, but still notifies the EditContext
+        // about the validation error message.
+        get => _parsingFailed ? _incomingValueBeforeParsing : FormatValueAsString(Value);
+        set => _ = SetValueAsStringAsync(value);
+    }
+
+    /// <summary>
     /// Gets or sets a callback that updates the bound value.
     /// </summary>
     [Parameter]
@@ -183,7 +197,7 @@ public abstract partial class FluentInputBase<TValue> : FluentComponentBase, IDi
     /// </summary>
     /// <param name="value"></param>
     /// <returns></returns>
-    protected virtual async Task SetCurrentValueAsync(TValue? value)
+    protected virtual async Task SetValueAsync(TValue? value)
     {
         var hasChanged = !EqualityComparer<TValue>.Default.Equals(value, Value);
         if (!hasChanged)
@@ -218,33 +232,10 @@ public abstract partial class FluentInputBase<TValue> : FluentComponentBase, IDi
     }
 
     /// <summary>
-    /// Gets or sets the current value of the input.
-    /// </summary>
-    protected virtual TValue? CurrentValue
-    {
-        get => Value;
-        set => _ = SetCurrentValueAsync(value);
-    }
-
-    /// <summary>
-    /// Gets or sets the current value of the input, represented as a string.
-    /// </summary>
-    protected virtual string? CurrentValueAsString
-    {
-        // InputBase-derived components can hold invalid states (e.g., an InputNumber being blank even when bound
-        // to an int value). So, if parsing fails, we keep the rejected string in the UI even though it doesn't
-        // match what's on the .NET model. This avoids interfering with typing, but still notifies the EditContext
-        // about the validation error message.
-        get => _parsingFailed ? _incomingValueBeforeParsing : FormatValueAsString(CurrentValue);
-        set => _ = SetCurrentValueAsStringAsync(value);
-
-    }
-
-    /// <summary>
-    /// Attempts to set the current value of the input, represented as a string.
+    /// Attempts to set the value of the input, represented as a string.
     /// </summary>
     /// <param name="value"></param>
-    protected virtual async Task SetCurrentValueAsStringAsync(string? value)
+    protected virtual async Task SetValueAsStringAsync(string? value)
     {
         _incomingValueBeforeParsing = value;
         _parsingValidationMessages?.Clear();
@@ -255,13 +246,15 @@ public abstract partial class FluentInputBase<TValue> : FluentComponentBase, IDi
             // Then all subclasses get nullable support almost automatically (they just have to
             // not reject Nullable<T> based on the type itself).
             _parsingFailed = false;
-            CurrentValue = default!;
+            await SetValueAsync(default);
         }
+
         else if (TryParseValueFromString(value, out var parsedValue, out var validationErrorMessage))
         {
             _parsingFailed = false;
-            await SetCurrentValueAsync(parsedValue);
+            await SetValueAsync(parsedValue);
         }
+
         else
         {
             _parsingFailed = true;
@@ -272,7 +265,7 @@ public abstract partial class FluentInputBase<TValue> : FluentComponentBase, IDi
                 _parsingValidationMessages ??= new ValidationMessageStore(EditContext);
                 _parsingValidationMessages.Add(FieldIdentifier, validationErrorMessage);
 
-                // Since we're not writing to CurrentValue, we'll need to notify about modification from here
+                // Since we're not writing to Value, we'll need to notify about modification from here
                 EditContext.NotifyFieldChanged(FieldIdentifier);
             }
         }
@@ -287,7 +280,6 @@ public abstract partial class FluentInputBase<TValue> : FluentComponentBase, IDi
 
     /// <summary>
     /// Formats the value as a string.
-    /// Derived classes can override this to determine the formatting used for <see cref="CurrentValueAsString"/>.
     /// </summary>
     /// <param name="value">The value to format.</param>
     /// <returns>A string representation of the value.</returns>
@@ -298,7 +290,6 @@ public abstract partial class FluentInputBase<TValue> : FluentComponentBase, IDi
 
     /// <summary>
     /// Parses a string to create an instance of <typeparamref name="TValue"/>.
-    /// Derived classes can override this to change how <see cref="CurrentValueAsString"/> interprets incoming values.
     /// </summary>
     /// <param name="value">The string value to be parsed.</param>
     /// <param name="result">An instance of <typeparamref name="TValue"/>.</param>
@@ -318,7 +309,7 @@ public abstract partial class FluentInputBase<TValue> : FluentComponentBase, IDi
 
         if (isValid)
         {
-            await SetCurrentValueAsync(result ?? default);
+            await SetValueAsync(result ?? default);
             _notifyCalled = true;
         }
         else
@@ -372,6 +363,7 @@ public abstract partial class FluentInputBase<TValue> : FluentComponentBase, IDi
             _nullableUnderlyingType = Nullable.GetUnderlyingType(typeof(TValue));
             _hasInitializedParameters = true;
         }
+
         else if (CascadedEditContext != EditContext)
         {
             // Not the first run
@@ -400,6 +392,9 @@ public abstract partial class FluentInputBase<TValue> : FluentComponentBase, IDi
         _ = InvokeAsync(StateHasChanged);
     }
 
+    /// <summary>
+    /// Updates the additional validation attributes.
+    /// </summary>
     private void UpdateAdditionalValidationAttributes()
     {
         if (EditContext is null)
@@ -408,6 +403,7 @@ public abstract partial class FluentInputBase<TValue> : FluentComponentBase, IDi
         }
 
         var hasAriaInvalidAttribute = AdditionalAttributes != null && AdditionalAttributes.ContainsKey("aria-invalid");
+
         if (FieldBound && EditContext.GetValidationMessages(FieldIdentifier).Any())
         {
             if (hasAriaInvalidAttribute)
@@ -430,12 +426,12 @@ public abstract partial class FluentInputBase<TValue> : FluentComponentBase, IDi
         else if (hasAriaInvalidAttribute)
         {
             // No validation errors. Need to remove `aria-invalid` if it was rendered already
-
             if (AdditionalAttributes!.Count == 1)
             {
                 // Only aria-invalid argument is present which we don't need any more
                 AdditionalAttributes = null;
             }
+
             else
             {
                 if (ConvertToDictionary(AdditionalAttributes, out var additionalAttributes))
@@ -455,15 +451,18 @@ public abstract partial class FluentInputBase<TValue> : FluentComponentBase, IDi
     private static bool ConvertToDictionary(IReadOnlyDictionary<string, object>? source, out Dictionary<string, object> result)
     {
         var newDictionaryCreated = true;
+
         if (source == null)
         {
             result = [];
         }
+
         else if (source is Dictionary<string, object> currentDictionary)
         {
             result = currentDictionary;
             newDictionaryCreated = false;
         }
+
         else
         {
             result = [];
