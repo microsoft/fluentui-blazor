412213afc4892a2a21414b3575f862f56f365073
Vincent Baaij <vnbaaij@outlook.com>
2024-09-26 12:55:05 +0200

    Add a test

--- tests/Core/_ToDo/MenuButton/FluentMenuButtonTests.cs
+++ tests/Core/_ToDo/MenuButton/FluentMenuButtonTests.cs
@@ -31,6 +31,32 @@ public class FluentMenuButtonTests : TestBase
             .Add(p => p.MenuStyle, menuStyle)
             .Add(p => p.Items, items)
             .Add(p => p.OnMenuChanged, onMenuChanged)
+        );
+        //Act
+
+        //Assert
+        cut.Verify();
+    }
+
+    [Fact]
+    public void FluentMenuButton_IconStart()
+    {
+        //Arrange
+        FluentButton button = default!;
+        FluentMenu menu = default!;
+        string text = default!;
+        string buttonStyle = default!;
+        string menuStyle = default!;
+        Dictionary<string, string> items = default!;
+        Action<MenuChangeEventArgs> onMenuChanged = _ => { };
+        var cut = TestContext.RenderComponent<FluentMenuButton>(parameters => parameters
+            .Add(p => p.Button, button)
+            .Add(p => p.Menu, menu)
+            .Add(p => p.Text, text)
+            .Add(p => p.ButtonStyle, buttonStyle)
+            .Add(p => p.MenuStyle, menuStyle)
+            .Add(p => p.Items, items)
+            .Add(p => p.OnMenuChanged, onMenuChanged)
             .Add(p => p.IconStart, SampleIcons.Info)
         );
         //Act
