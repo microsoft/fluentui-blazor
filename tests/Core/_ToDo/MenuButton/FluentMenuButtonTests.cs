bd07ecb5681254fb06c5a5c20ff3757a2fcb829f
Vincent Baaij <vnbaaij@outlook.com>
2024-09-26 13:25:31 +0200

    [Calendar] Add ReadOnly in day click/keydown logic (#2720)
    * Add ReadOnly in day click/keydown logic
    
    * Update Test SDK
    
    * Add a test
    
    * Replace all static icon references

--- tests/Core/_ToDo/MenuButton/FluentMenuButtonTests.cs
+++ tests/Core/_ToDo/MenuButton/FluentMenuButtonTests.cs
@@ -31,7 +31,35 @@ public class FluentMenuButtonTests : TestBase
             .Add(p => p.MenuStyle, menuStyle)
             .Add(p => p.Items, items)
             .Add(p => p.OnMenuChanged, onMenuChanged)
-            .Add(p => p.IconStart, SampleIcons.Info)
+        );
+        //Act
+
+        //Assert
+        cut.Verify();
+    }
+
+    [Fact]
+    public void FluentMenuButton_IconStart()
+    {
+        //Arrange
+        FluentButton button = default!;
+        FluentMenu menu = default!;
+        string text = default!;
+        string buttonStyle = default!;
+        string menuStyle = default!;
+        Dictionary<string, string> items = default!;
+        var icon = new SampleIcons.Samples.Info();
+
+        Action<MenuChangeEventArgs> onMenuChanged = _ => { };
+        var cut = TestContext.RenderComponent<FluentMenuButton>(parameters => parameters
+            .Add(p => p.Button, button)
+            .Add(p => p.Menu, menu)
+            .Add(p => p.Text, text)
+            .Add(p => p.ButtonStyle, buttonStyle)
+            .Add(p => p.MenuStyle, menuStyle)
+            .Add(p => p.Items, items)
+            .Add(p => p.OnMenuChanged, onMenuChanged)
+            .Add(p => p.IconStart, icon)
         );
         //Act
 
