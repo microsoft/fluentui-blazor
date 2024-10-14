227e2b71c223f3720334c6a3f6a905c7cf564aac
Vincent Baaij <vnbaaij@outlook.com>
2024-10-14 20:47:15 +0200

    [MessageBox] Add settable primary action text for Show... methods (#2808)
    * Docu update
    
    * Fix #2805 by adding primary action parameter to message box shortcut methods

--- src/Core/Components/Dialog/Services/IDialogService-MessageBox.cs
+++ src/Core/Components/Dialog/Services/IDialogService-MessageBox.cs
@@ -2,25 +2,26 @@ namespace Microsoft.FluentUI.AspNetCore.Components;
 
 public partial interface IDialogService
 {
-    void ShowSuccess(string message, string? title = null);
 
-    void ShowWarning(string message, string? title = null);
+    void ShowSuccess(string message, string? title = null, string? primaryText = null);
 
-    void ShowError(string message, string? title = null);
+    void ShowWarning(string message, string? title = null, string? primaryText = null);
 
-    void ShowInfo(string message, string? title = null);
+    void ShowError(string message, string? title = null, string? primaryText = null);
+
+    void ShowInfo(string message, string? title = null, string? primaryText = null);
 
     void ShowConfirmation(object receiver, Func<DialogResult, Task> callback, string message, string primaryText = "Yes", string secondaryText = "No", string? title = null);
 
     void ShowMessageBox(DialogParameters<MessageBoxContent> parameters);
 
-    Task<IDialogReference> ShowSuccessAsync(string message, string? title = null);
+    Task<IDialogReference> ShowSuccessAsync(string message, string? title = null, string? primaryText = null);
 
-    Task<IDialogReference> ShowWarningAsync(string message, string? title = null);
+    Task<IDialogReference> ShowWarningAsync(string message, string? title = null, string? primaryText = null);
 
-    Task<IDialogReference> ShowErrorAsync(string message, string? title = null);
+    Task<IDialogReference> ShowErrorAsync(string message, string? title = null, string? primaryText = null);
 
-    Task<IDialogReference> ShowInfoAsync(string message, string? title = null);
+    Task<IDialogReference> ShowInfoAsync(string message, string? title = null, string? primaryText = null);
 
     Task<IDialogReference> ShowConfirmationAsync(object receiver, Func<DialogResult, Task> callback, string message, string primaryText = "Yes", string secondaryText = "No", string? title = null);
 
