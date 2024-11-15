7b0865848e79d6dbcd10bd5ce8d0279ed12e3fe1
Miguel Hasse <miguel.hasse@outlook.com>
2024-11-15 13:16:51 +0000

    Optimized  count handling in ODataAsyncQueryExecutor

--- src/Extensions/DataGrid.OData.Client/ODataAsyncQueryExecutor.cs
+++ src/Extensions/DataGrid.OData.Client/ODataAsyncQueryExecutor.cs
@@ -8,13 +8,13 @@ internal class ODataAsyncQueryExecutor : IAsyncQueryExecutor
     public bool IsSupported<T>(IQueryable<T> queryable) => queryable.Provider is DataServiceQueryProvider;
 
     public async Task<int> CountAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken)
-        =>  (int)(await ExecuteAsync(queryable, cancellationToken)).Count;
+        => (int)(await ExecuteAsync(((DataServiceQuery<T>)queryable.Take(0)).IncludeCount(), cancellationToken)).Count;
 
     public async Task<T[]> ToArrayAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken)
         => [.. await ExecuteAsync(queryable, cancellationToken)];
 
     private static async Task<QueryOperationResponse<T>> ExecuteAsync<T>(IQueryable<T> queryable, CancellationToken cancellationToken)
     {
-        return (QueryOperationResponse<T>)await ((DataServiceQuery<T>)queryable).IncludeCount().ExecuteAsync(cancellationToken);
+        return (QueryOperationResponse<T>)await ((DataServiceQuery<T>)queryable).ExecuteAsync(cancellationToken);
     }
 }
