0032eac989c1a636a368d88d3581305c628b8757
Vincent Baaij <vnbaaij@outlook.com>
2024-11-21 16:43:13 +0100

    - Add MAUI Hybrid + Web template

--- src/Templates/templates/maui-blazor-solution/MauiApp.1.Web.Client/Program.Main.cs
+++ src/Templates/templates/maui-blazor-solution/MauiApp.1.Web.Client/Program.Main.cs
@@ -2,8 +2,6 @@
 using MauiApp._1.Web.Client;
 using Microsoft.AspNetCore.Components.Authorization;
 #endif
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.FluentUI.AspNetCore.Components;
 #if (SampleContent)
 using MauiApp._1.Shared.Services;
 using MauiApp._1.Web.Client.Services;
@@ -16,18 +14,18 @@ class Program
     static async Task Main(string[] args)
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.Services.AddFluentUIComponents();
 
-        #if (SampleContent)
+#if (SampleContent)
         // Add device-specific services used by the MauiApp._1.Shared project
         builder.Services.AddSingleton<IFormFactor, FormFactor>();
-
-        #endif
-        #if (IndividualLocalAuth)
+#endif
+#if (IndividualLocalAuth)
         builder.Services.AddAuthorizationCore();
         builder.Services.AddCascadingAuthenticationState();
         builder.Services.AddAuthenticationStateDeserialization();
 
-        #endif
+#endif
         await builder.Build().RunAsync();
     }
 }
