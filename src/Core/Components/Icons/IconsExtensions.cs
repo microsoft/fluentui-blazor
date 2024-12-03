e7c303432a69b390f9db2d06a8ad987d26341ede
Vincent Baaij <vnbaaij@outlook.com>
2024-12-03 10:40:49 +0100

    [Icons] Update to Fluent UI System Icons 1.1.266 (#2990)
    * Update to Fluent UI System Icons 1.1.266
    Create project for each variant, change core Icon project ot reference new projects
    
    * Refactoring
    
    * Update sub-classes to namespaces
    
    * Update csproj emojis
    
    * Update sln
    
    * Fix existing emoji
    
    * Update emoji csproj to add dll in nuget package
    
    * Update ADO pipeline
    
    * - Move general project properties to `Directory.Build.Props`
    - Adjust `RootNamespace`s
    
    * ADO pipeline: remove old icons project and add OData adapter
    
    * Update templates to use packages version 4.11 or later and add required using
    
    * Doc updates
    
    * More docs
    
    ---------
    
    Co-authored-by: Denis Voituron <dvoituron@outlook.com>

---
+++ src/Core/Components/Icons/IconsExtensions.cs
@@ -0,0 +1,90 @@
+// ------------------------------------------------------------------------
+// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.FluentUI.AspNetCore.Components;
+
+/// <summary />
+public static partial class IconsExtensions
+{
+    private const string Namespace = "Microsoft.FluentUI.AspNetCore.Components";
+    private const string LibraryName = "Microsoft.FluentUI.AspNetCore.Components.Icons";
+
+    /// <summary>
+    /// Returns a new instance of the icon.
+    /// </summary>
+    /// <param name="icon">The <see cref="IconInfo"/> to instantiate.</param>
+    /// <remarks>
+    /// This method requires dynamic access to code. This code may be removed by the trimmer.
+    /// </remarks>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException">Raised when the <see cref="IconInfo.Name"/> is not found in predefined icons.</exception>
+    [RequiresUnreferencedCode("This method requires dynamic access to code. This code may be removed by the trimmer.")]
+    public static CustomIcon GetInstance(this IconInfo icon)
+    {
+        var assembly = AppDomain.CurrentDomain
+                                .GetAssemblies()
+                                .FirstOrDefault(i => i.ManifestModule.Name.StartsWith(LibraryName, StringComparison.Ordinal));
+
+        if (assembly != null)
+        {
+            var allIcons = assembly.GetTypes()
+                                   .Where(i => i.BaseType == typeof(Icon));
+
+            // Ex. Microsoft.FluentUI.AspNetCore.Components.Icons+Filled+Size10+PresenceAvailable
+            var iconFullName = $"{Namespace}.Icons.{icon.Variant}.Size{(int)icon.Size}+{icon.Name}";
+            var iconType = allIcons.FirstOrDefault(i => i.FullName == iconFullName);
+
+            if (iconType != null)
+            {
+                var newIcon = Activator.CreateInstance(iconType);
+                if (newIcon != null)
+                {
+                    return new CustomIcon((Icon)newIcon);
+                }
+            }
+        }
+
+        throw new ArgumentException($"Icon '{icon.Name}' not found.");
+    }
+
+    /// <summary>
+    /// Returns a new instance of the icon.
+    /// </summary>
+    /// <remarks>
+    /// This method requires dynamic access to code. This code may be removed by the trimmer.
+    /// </remarks>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException">Raised when the <see cref="IconInfo.Name"/> is not found in predefined icons.</exception>
+    [RequiresUnreferencedCode("This method requires dynamic access to code. This code may be removed by the trimmer.")]
+    public static IEnumerable<IconInfo> GetAllIcons()
+    {
+        var assembly = AppDomain.CurrentDomain
+                                .GetAssemblies()
+                                .FirstOrDefault(i => i.ManifestModule.Name.StartsWith(LibraryName, StringComparison.Ordinal));
+
+        if (assembly != null)
+        {
+            var allTypes = assembly.GetTypes()
+                                   .Where(i => i.BaseType == typeof(Icon)
+                                            && i.Name != nameof(CustomIcon));
+
+            var allIcons = allTypes.Select(type => Activator.CreateInstance(type) as IconInfo ?? new IconInfo());
+
+            return allIcons ?? Array.Empty<IconInfo>();
+        }
+
+        return Array.Empty<IconInfo>();
+    }
+
+    /// <summary />
+    public static IEnumerable<IconInfo> AllIcons
+    {
+        get
+        {
+            return GetAllIcons();
+        }
+    }
+}
