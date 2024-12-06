93cfaebbee4f82531191a579c310290d54ff2671
Denis Voituron <dvoituron@outlook.com>
2024-12-06 20:45:45 +0100

    Fix the Extension methods

--- src/Core/Components/Emojis/EmojiExtensions.cs
+++ src/Core/Components/Emojis/EmojiExtensions.cs
@@ -3,6 +3,7 @@
 // ------------------------------------------------------------------------
 
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 
 namespace Microsoft.FluentUI.AspNetCore.Components;
 
@@ -10,31 +11,32 @@ namespace Microsoft.FluentUI.AspNetCore.Components;
 public static partial class EmojiExtensions
 {
     private const string Namespace = "Microsoft.FluentUI.AspNetCore.Components";
-    private const string LibraryName = "Microsoft.FluentUI.AspNetCore.Components.Emojis";
+    private const string LibraryName = "Microsoft.FluentUI.AspNetCore.Components.Emojis.{0}";    // {0} must be replaced with the "Group": SmileysEmotion, PeopleBody, etc.
 
     /// <summary>
     /// Returns a new instance of the emoji.
     /// </summary>
     /// <remarks>
     /// This method requires dynamic access to code. This code may be removed by the trimmer.
+    /// If the assembly is not yet loaded, it will be loaded by the method `Assembly.Load`.
+    /// To avoid any issues, the assembly must be loaded before calling this method (e.g. adding an emoji in your code).
     /// </remarks>
     /// <returns></returns>
     /// <exception cref="ArgumentException">Raised when the <see cref="EmojiInfo.Name"/> is not found in predefined emojis.</exception>
     [RequiresUnreferencedCode("This method requires dynamic access to code. This code may be removed by the trimmer.")]
     public static CustomEmoji GetInstance(this EmojiInfo emoji)
     {
-        var assembly = AppDomain.CurrentDomain
-                                .GetAssemblies()
-                                .FirstOrDefault(i => i.ManifestModule.Name.StartsWith(LibraryName, StringComparison.Ordinal));
+        var group = emoji.Group.ToString().Replace("_", string.Empty);
+        var assemblyName = string.Format(LibraryName, group);
+        var assembly = GetAssembly(assemblyName);
 
         if (assembly != null)
         {
             var allEmojis = assembly.GetTypes()
                                     .Where(i => i.BaseType == typeof(Emoji));
 
-            // Ex. Microsoft.FluentUI.AspNetCore.Components.Emojis+Activities+Color+Default+Baseball
-            var group = emoji.Group.ToString().Replace("_", string.Empty);
-            var emojiFullName = $"{Namespace}.Emojis+{group}+{emoji.Style}+{emoji.Skintone}+{emoji.Name}";
+            // Ex. Microsoft.FluentUI.AspNetCore.Components.Emojis.Activities.Color.Default+Baseball
+            var emojiFullName = $"{Namespace}.Emojis.{group}.{emoji.Style}.{emoji.Skintone}+{emoji.Name}";
             var emojiType = allEmojis.FirstOrDefault(i => i.FullName == emojiFullName);
 
             if (emojiType != null)
@@ -47,7 +49,7 @@ public static partial class EmojiExtensions
             }
         }
 
-        throw new ArgumentException($"Emoji '{emoji.Name}' not found.");
+        throw new ArgumentException($"Emoji 'Emojis.{group}.{emoji.Style}.{emoji.Skintone}.{emoji.Name}' not found.");
     }
 
     /// <summary>
@@ -61,21 +63,24 @@ public static partial class EmojiExtensions
     [RequiresUnreferencedCode("This method requires dynamic access to code. This code may be removed by the trimmer.")]
     public static IEnumerable<EmojiInfo> GetAllEmojis()
     {
-        var assembly = AppDomain.CurrentDomain
-                                .GetAssemblies()
-                                .FirstOrDefault(i => i.ManifestModule.Name.StartsWith(LibraryName, StringComparison.Ordinal));
+        var allIcons = new List<EmojiInfo>();
 
-        if (assembly != null)
+        foreach (var group in Enum.GetValues(typeof(EmojiGroup)).Cast<EmojiGroup>())
         {
-            var allTypes = assembly.GetTypes()
-                                   .Where(i => i.BaseType == typeof(Emoji));
+            var assemblyName = string.Format(LibraryName, group.ToString().Replace("_", string.Empty));
+            var assembly = GetAssembly(assemblyName);
 
-            var allEmojis = allTypes.Select(type => Activator.CreateInstance(type) as EmojiInfo ?? new EmojiInfo());
+            if (assembly != null)
+            {
+                var allTypes = assembly.GetTypes()
+                                       .Where(i => i.BaseType == typeof(Emoji)
+                                                && i.Name != nameof(CustomEmoji));
 
-            return allEmojis ?? Array.Empty<EmojiInfo>();
+                allIcons.AddRange(allTypes.Select(type => Activator.CreateInstance(type) as EmojiInfo ?? new EmojiInfo()));
+            }
         }
 
-        return Array.Empty<EmojiInfo>();
+        return allIcons;
     }
 
     /// <summary />
@@ -84,6 +89,23 @@ public static partial class EmojiExtensions
         get
         {
             return GetAllEmojis();
+        }
+    }
+
+    /// <summary />
+    private static Assembly? GetAssembly(string assemblyName)
+    {
+        try
+        {
+            return AppDomain.CurrentDomain
+                            .GetAssemblies()
+                            .FirstOrDefault(i => i.ManifestModule.Name == assemblyName + ".dll")
+                ?? Assembly.Load(assemblyName);
+
+        }
+        catch (Exception)
+        {
+            return null;
         }
     }
 }
