c0bde70e9f06d8e0570d57c2f53e6f79443dee7f
Denis Voituron <dvoituron@outlook.com>
2024-12-17 18:16:56 +0100

    Refacroring

--- src/Core/Components/Base/IFluentServiceBase.cs
+++ src/Core/Components/Base/IFluentServiceBase.cs
@@ -13,18 +13,18 @@ namespace Microsoft.FluentUI.AspNetCore.Components;
 public interface IFluentServiceBase<TComponent> : IDisposable
 {
     /// <summary>
-    /// Gets or sets the Provider ID.
+    /// Gets the Provider ID.
     /// This value is set by the provider and will be empty if the provider is not initialized.
     /// </summary>
-    string? ProviderId { get; set; }
+    public string? ProviderId { get; internal set; }
 
     /// <summary>
     /// Gets the list of <typeparamref name="TComponent"/>.
     /// </summary>
-    ConcurrentDictionary<string, TComponent> Items { get; }
+    internal ConcurrentDictionary<string, TComponent> Items { get; }
 
     /// <summary>
     /// Action to be called when the <typeparamref name="TComponent"/> is updated.
     /// </summary>
-    Func<TComponent, Task> OnUpdatedAsync { get; set; }
+    internal Func<TComponent, Task> OnUpdatedAsync { get; set; }
 }
