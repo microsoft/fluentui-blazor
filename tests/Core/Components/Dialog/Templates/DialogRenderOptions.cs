b4931cecaf6b688e7c2b435b6f6623a47182a08d
Denis Voituron <dvoituron@outlook.com>
2024-12-27 19:53:53 +0100

    Add first Unit Tests

---
+++ tests/Core/Components/Dialog/Templates/DialogRenderOptions.cs
@@ -0,0 +1,16 @@
+// ------------------------------------------------------------------------
+// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------
+
+namespace Microsoft.FluentUI.AspNetCore.Components.Tests.Components.Dialog.Templates;
+
+public class DialogRenderOptions
+{
+    public bool AutoClose { get; set; }
+
+    public DialogResult AutoCloseResult { get; set; } = DialogResult.Ok(true);
+
+    public int OnInitializedCount { get; set; }
+
+    public int OnParametersSetCount { get; set; }
+}
