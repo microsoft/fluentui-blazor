d21904b28f31e24bbf95bacc0a8c50017673dd89
Adrien Clerbois <50712277+AClerbois@users.noreply.github.com>
2025-02-15 20:00:08 +0100

    [dev-v5] Add FluentDivider component (#3365)

---
+++ src/Core/Components/Divider/FluentDivider.razor.cs
@@ -0,0 +1,57 @@
+// ------------------------------------------------------------------------
+// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------
+using Microsoft.AspNetCore.Components;
+
+namespace Microsoft.FluentUI.AspNetCore.Components;
+
+/// <summary>
+/// FluentDivider component
+/// </summary>
+public partial class FluentDivider : FluentComponentBase
+{
+    /// <summary />
+    protected string? ClassValue => DefaultClassBuilder
+        .Build();
+
+    /// <summary />
+    protected string? StyleValue => DefaultStyleBuilder
+        .Build();
+
+    /// <summary>
+    /// Gets or sets the alignment of the content.
+    /// </summary>
+    [Parameter]
+    public DividerAlignContent? AlignContent { get; set; }
+
+    /// <summary>
+    /// Gets or sets the appearance of the content.
+    /// </summary>
+    [Parameter]
+    public DividerAppearance? Appearance { get; set; }
+
+    /// <summary>
+    /// Adds padding to the beginning and end of the divider.
+    /// </summary>
+    [Parameter]
+    public bool? Inset { get; set; }
+
+    /// <summary>
+    /// A divider can be horizontal (default) or vertical.
+    /// </summary>
+    [Parameter]
+    public bool? Vertical { get; set; }
+
+    /// <summary>
+    /// Gets or sets the content to be shown.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    private string? Orientation
+        => Vertical.HasValue
+            ? Vertical.Value
+                ? "vertical"
+                : "horizontal"
+            : null;
+}
