c1927dd9f76b85381bf093e089a57d0474d60454
Denis Voituron <dvoituron@outlook.com>
2025-02-18 15:16:35 +0100

    Add an obsolete FluentProgress component

---
+++ src/Core/Migration/FluentProgress.cs
@@ -0,0 +1,15 @@
+// ------------------------------------------------------------------------
+// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------
+
+namespace Microsoft.FluentUI.AspNetCore.Components;
+
+/// <summary>
+/// Visual representation of content being loaded or processed.
+/// This component is renamed to <see cref="FluentProgressBar"/> and will be removed in a future release.
+/// </summary>
+[Obsolete("This component is renamed to FluentProgressBar and will be removed in a future release.")]
+public class FluentProgress : FluentProgressBar
+{
+
+}
