83515c5eb8a74dd3ca08743d6b602e0d71403341
Antoine Griffard <agriffard@hotmail.com>
2025-02-23 12:36:58 +0100

    Switch component

---
+++ examples/Demo/FluentUI.Demo.Client/Documentation/Components/Switch/FluentSwitch.md
@@ -0,0 +1,25 @@
+---
+title: Switch
+route: /Switch
+---
+
+# Switch
+
+A **FluentSwitch** component represents a physical switch that allows someone to choose between two mutually exclusive options.  
+For example, "On/Off" and "Show/Hide". Choosing an option should produce an immediate result.
+
+{{ SwitchDefault }}
+
+## Disabled
+
+A Switch can be disabled.
+
+{{ SwitchDisabled }}
+
+## Label position
+
+{{ SwitchLabelPosition }}
+
+## API FluentSwitch
+
+{{ API Type=FluentSwitch }}
---
+++ src/Core/Components/Switch/FluentSwitch.razor.cs
@@ -0,0 +1,64 @@
+// ------------------------------------------------------------------------
+// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------
+
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Microsoft.FluentUI.AspNetCore.Components;
+
+/// <summary>
+/// The FluentSwitch component is used to render a Switch
+/// </summary>
+public partial class FluentSwitch : FluentInputBase<bool>, IFluentComponentElementBase
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public FluentSwitch()
+    {
+        LabelPosition = Components.LabelPosition.After;
+    }
+
+    /// <inheritdoc cref="IFluentComponentElementBase.Element" />
+    [Parameter]
+    public ElementReference Element { get; set; }
+
+    /// <summary>
+    /// Handler for the OnFocus event.
+    /// </summary>
+    /// <param name="e"></param>
+    /// <returns></returns>
+    protected virtual Task FocusOutHandlerAsync(FocusEventArgs e)
+    {
+        FocusLost = true;
+        return Task.CompletedTask;
+    }
+
+    private void OnSwitchChangedHandler(ChangeEventArgs e)
+    {
+        ArgumentNullException.ThrowIfNull(e);
+
+        CurrentValue = !CurrentValue;
+    }
+
+    /// <summary>
+    /// Parses a string to create the <see cref="Microsoft.AspNetCore.Components.Forms.InputBase{TValue}.Value"/>.
+    /// </summary>
+    /// <param name="value">The string value to be parsed.</param>
+    /// <param name="result">The result to inject into the Value.</param>
+    /// <param name="validationErrorMessage">If the value could not be parsed, provides a validation error message.</param>
+    /// <returns>True if the value could be parsed; otherwise false.</returns>
+    protected override bool TryParseValueFromString(string? value, [MaybeNullWhen(false)] out bool result, [NotNullWhen(false)] out string? validationErrorMessage)
+    {
+        // Overriding mandatory because the parent method is abstract and called via the OnChanged.
+        throw new NotSupportedException();
+    }
+
+    internal bool InternalTryParseValueFromString(string? value, [MaybeNullWhen(false)] out bool result, [NotNullWhen(false)] out string? validationErrorMessage)
+    {
+        return TryParseValueFromString(value, out result, out validationErrorMessage);
+    }
+}
