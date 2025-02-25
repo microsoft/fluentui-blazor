f0f1bf8c93ef23c6425b30bd4accf217a1256a20
Roland <195342210+rpodevns@users.noreply.github.com>
2025-02-25 08:55:23 +0000

    [Menu] Add CheckedChanged EventCallback (fix 3390) (#3414)
    * [Menu] Add CheckedChanged EventCallback (fix 3390)
    
    * [Menu] Add 2-way binding (fix 3390)
    
    * [Menu] Add CheckedChanged EventCallback (fix 3390)
    
    * [Menu] Add 2-way binding (fix 3390)
    
    * [Menu] fixed typos and changed properties to fields in sample
    
    ---------
    
    Co-authored-by: rpodevns <rpodevns@users.noreply.github.com>
    Co-authored-by: Vincent Baaij <vnbaaij@outlook.com>

--- src/Core/Components/Menu/FluentMenu.razor.js
+++ src/Core/Components/Menu/FluentMenu.razor.js
@@ -1,4 +1,4 @@
-﻿// Add Left click event to open the PowerMenu
+// Add Left click event to open the FluentMenu
 export function addEventLeftClick(id, dotNetHelper) {
     var item = document.getElementById(id);
     if (!!item) {
@@ -8,7 +8,7 @@ export function addEventLeftClick(id, dotNetHelper) {
     }
 }
 
-// Add Right click event to open the PowerMenu
+// Add Right click event to open the FluentMenu
 export function addEventRightClick(id, dotNetHelper) {
     var item = document.getElementById(id);
     if (!!item) {
@@ -18,4 +18,16 @@ export function addEventRightClick(id, dotNetHelper) {
             return false;
         }, false);
     }
+}
+
+// Must use an animation frame to ensure the DOM is fully updated before checking the element's
+// attributes to prevent stale or inconsistent reads.
+export function isChecked(menuItemId) {
+    return new Promise(resolve => {
+        requestAnimationFrame(() => {
+            const menuItemElement = document.getElementById(menuItemId);
+            if (!menuItemElement) return resolve(false);
+            resolve(menuItemElement.hasAttribute("checked") || menuItemElement.getAttribute("aria-checked") === "true");
+        });
+    });
 }
