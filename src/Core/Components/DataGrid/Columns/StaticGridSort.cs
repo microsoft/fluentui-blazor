3726bd8a932a02daa11d7b177ce716003a23c6bb
Steven Rasmussen <steven.rasmussen@gmail.com>
2025-02-28 21:16:56 -0700

    Introduced the IGridSort interface and refactored the codebase. Created a new 'StaticGridSort' class.

---
+++ src/Core/Components/DataGrid/Columns/StaticGridSort.cs
@@ -0,0 +1,42 @@
+// ------------------------------------------------------------------------
+// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------
+
+
+namespace Microsoft.FluentUI.AspNetCore.Components.Components.DataGrid.Columns;
+public class StaticGridSort<TGridItem> : IGridSort<TGridItem>
+{
+    private readonly string _propertyName;
+    private readonly Func<IQueryable<TGridItem>, bool, IOrderedQueryable<TGridItem>>? _sortFunction;
+
+    public StaticGridSort(
+        string propertyName,
+        Func<IQueryable<TGridItem>, bool, IOrderedQueryable<TGridItem>>? sortFunction)
+    {
+        _propertyName = propertyName;
+        _sortFunction = sortFunction;
+    }
+
+    public IOrderedQueryable<TGridItem> Apply(IQueryable<TGridItem> queryable, bool ascending)
+    {
+        if (_sortFunction != null)
+        {
+            return _sortFunction(queryable, ascending);
+        }
+
+        return queryable.OrderBy(x => 0);
+    }
+
+    public IReadOnlyCollection<SortedProperty> ToPropertyList(bool ascending)
+    {
+        return new List<SortedProperty> {
+            new SortedProperty
+            {
+                 PropertyName = _propertyName,
+                 Direction = ascending
+                    ? SortDirection.Ascending
+                    : SortDirection.Descending,
+            }
+        };
+    }
+}
