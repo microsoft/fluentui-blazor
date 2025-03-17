fe06f0bc62d7aa85daac7456c7cf3273a2377733
Denis Voituron <dvoituron@outlook.com>
2025-03-17 14:37:30 +0100

    Add a global FluentProviders component

---
+++ src/Core/Infrastructure/FluentProviders.razor.cs
@@ -0,0 +1,18 @@
+// ------------------------------------------------------------------------
+// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------
+
+namespace Microsoft.FluentUI.AspNetCore.Components;
+
+/// <summary />
+public partial class FluentProviders : FluentComponentBase
+{
+    /// <summary />
+    internal string? ClassValue => DefaultClassBuilder
+        .AddClass("fluent-providers")
+        .Build();
+
+    /// <summary />
+    internal string? StyleValue => DefaultStyleBuilder
+        .Build();
+}
