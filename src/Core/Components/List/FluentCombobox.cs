09ac2a9730f41a5dbd6b5d24e21ff7d46ab1cb73
Denis Voituron <dvoituron@outlook.com>
2025-03-26 11:31:55 +0100

    Add FluentCombobox (first part)

---
+++ src/Core/Components/List/FluentCombobox.cs
@@ -0,0 +1,32 @@
+// ------------------------------------------------------------------------
+// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace Microsoft.FluentUI.AspNetCore.Components;
+
+/// <summary>
+/// A FluentCombobox allows one option to be selected from multiple items.
+/// </summary>
+/// <typeparam name="TOption"></typeparam>
+public partial class FluentCombobox<TOption> : FluentSelect<TOption>
+{
+    /// <summary />
+    protected override string DropdownType => "combobox";
+
+    /// <summary>
+    /// Gets or sets whether the combobox allows free form entry.
+    /// Use the <see cref="FreeOptionOutput"/> component to display the user entry.
+    /// If the someone types a string that does not match any option in the list,
+    /// you can allow submission of their free form entry by using this parameter.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? FreeOption { get; set; }
+
+    /// <summary />
+    protected override RenderFragment? RenderFreeFormOption()
+    {
+        return FreeOption;
+    }
+}
---
+++ src/Core/Components/List/FreeOptionOutput.cs
@@ -0,0 +1,23 @@
+// ------------------------------------------------------------------------
+// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Microsoft.FluentUI.AspNetCore.Components;
+
+/// <summary>
+/// A FreeOptionOutput allows one option to be written in free form.
+/// </summary>
+public class FreeOptionOutput : FluentComponentBase
+{
+    /// <summary />
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        builder.OpenElement(0, "output");
+        builder.AddAttribute(1, "id", Id);
+        builder.AddAttribute(2, "class", DefaultClassBuilder.Build());
+        builder.AddAttribute(3, "style", DefaultStyleBuilder.Build());
+        builder.CloseElement();
+    }
+}
