ee9df8c7877402f7a5257a6e5bf99fc6677cb98e
Pascal Vorwerk <pascal.vorwerk@gmail.com>
2025-04-02 23:32:03 +0200

    Component, tests and examples

---
+++ examples/Demo/FluentUI.Demo.Client/Documentation/Components/Spacer/Spacer.md
@@ -0,0 +1,19 @@
+---
+title: Spacer
+route: /Spacer
+---
+
+# Spacer
+The purpose of this component is to generate space between other components. You can either create
+a space with a set width (in number of pixels) or create a space with a flexible width.
+
+## Default
+By default, the spacer will set a flex growth of '1' to fill any space it can compared to other elements in the flex container.
+
+{{ SpacerDefault }}
+
+## Width in pixels
+If specifying the width parameters in pixels, the spacer will be a <span> element with a inline width of that amount of pixels.
+
+{{ SpacerWidth }}
+
---
+++ src/Core/Components/Spacer/FluentSpacer.razor.cs
@@ -0,0 +1,20 @@
+﻿// ------------------------------------------------------------------------
+// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace Microsoft.FluentUI.AspNetCore.Components;
+
+/// <summary>
+/// Spacer component, used to create space between elements.
+/// </summary>
+public partial class FluentSpacer : ComponentBase
+{
+    /// <summary>
+    /// Gets or sets the width of the spacer (in pixels).
+    /// </summary>
+    [Parameter]
+    public int? Width { get; set; }
+}
+
