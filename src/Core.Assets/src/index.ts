bd5ec2acc9cc2f98f33f7bd7999d4f9dcc6213bb
MarvinKlein1508 <support@marvinkleinmusic.de>
2025-04-17 15:48:56 +0200

    Fix text spacing in FluentSearch

--- src/Core.Assets/src/index.ts
+++ src/Core.Assets/src/index.ts
@@ -67,7 +67,8 @@ fluent-text-field.invalid::part(root),
 
 fluent-button::part(control),
 fluent-text-area::part(control),
-fluent-text-field::part(control)
+fluent-text-field::part(control),
+fluent-search::part(control)
 {
     letter-spacing: inherit;
 }
