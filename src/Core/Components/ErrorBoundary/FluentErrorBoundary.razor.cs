94a285c02e451704560e67c7b7cae1761374a3ff
Denis Voituron <dvoituron@outlook.com>
2025-05-30 18:28:35 +0200

    Add FluentErrorBoundary

---
+++ src/Core/Components/ErrorBoundary/FluentErrorBoundary.razor.cs
@@ -0,0 +1,64 @@
+// ------------------------------------------------------------------------
+// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Microsoft.FluentUI.AspNetCore.Components;
+
+/// <summary>
+/// Provides a mechanism for handling errors in a fluent manner within a specific context.
+/// </summary>
+public partial class FluentErrorBoundary
+{
+    private ErrorBoundary? ErrorBoundary;
+
+    /// <summary>
+    /// Gets or sets the icon displayed in the title bar to indicate an error state.
+    /// </summary>
+    public static Icon TitleIcon { get; set; } = new CoreIcons.Filled.Size20.DismissCircle().WithColor(Color.Error);
+
+    /// <summary>
+    /// Gets or sets the content to be rendered inside the component.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether child content should be hidden when an error occurs.
+    /// </summary>
+    [Parameter]
+    public bool HideChildContentOnError { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets the content to be rendered as the dialog header for error messages.
+    ///  This content will be rendered in place of the default dialog header when an error occurs within the boundary.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ErrorHeader { get; set; }
+
+    /// <summary>
+    /// Gets or sets the content to be displayed when there is an error.
+    /// This content will be rendered in place of the default error message when an error occurs within the boundary.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ErrorContent { get; set; }
+
+    /// <summary>
+    /// Gets or sets the details to display when an error occurs within the error boundary.
+    /// </summary>
+    [Parameter]
+    public ErrorBoundaryDetails DisplayErrorDetails { get; set; } = ErrorBoundaryDetails.None;
+
+    /// <summary />
+    private Task OnToggleAsync(DialogToggleEventArgs e)
+    {
+        if (string.Equals(e.NewState, "closed", StringComparison.Ordinal))
+        {
+            ErrorBoundary?.Recover();
+        }
+
+        return Task.CompletedTask;
+    }
+}
