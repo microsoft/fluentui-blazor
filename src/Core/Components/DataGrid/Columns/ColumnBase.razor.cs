c1dc136c3076860be1d5b9a21135580553cb762a
Phillip Hoff <phillip@orst.edu>
2025-06-03 01:36:21 -0700

    Add `HeaderCellTitleTemplate` to `DataGrid` `ColumnBase` (#3860)
    * Sketch implementation of FluentDataGrid HeaderCellTitleTemplate.
    
    * Consolidate header title rendering.
    
    * Updated docs.
    
    ---------
    
    Co-authored-by: Vincent Baaij <vnbaaij@outlook.com>

--- src/Core/Components/DataGrid/Columns/ColumnBase.razor.cs
+++ src/Core/Components/DataGrid/Columns/ColumnBase.razor.cs
@@ -84,6 +84,13 @@ public abstract partial class ColumnBase<TGridItem>
     public RenderFragment<ColumnBase<TGridItem>>? HeaderCellItemTemplate { get; set; }
 
     /// <summary>
+    /// Gets or sets a template for the title content of this column's header cell.
+    /// If not specified, the default header template includes the <see cref="Title" />.
+    /// </summary>
+    [Parameter]
+    public RenderFragment<ColumnBase<TGridItem>>? HeaderCellTitleTemplate { get; set; }
+
+    /// <summary>
     /// If specified, indicates that this column has this associated options UI. A button to display this
     /// UI will be included in the header cell by default.
     ///
@@ -222,6 +229,14 @@ public abstract partial class ColumnBase<TGridItem>
     protected internal RenderFragment HeaderContent { get; protected set; }
 
     /// <summary>
+    /// Gets or sets a <see cref="RenderFragment" /> that will be rendered for this column's header title.
+    /// This allows derived components to change the header title output. However, derived components are then
+    /// responsible for using <see cref="HeaderCellTitleTemplate" /> within that new output if they want to continue
+    /// respecting that option.
+    /// </summary>
+    protected internal RenderFragment HeaderTitleContent { get; protected set; }
+
+    /// <summary>
     /// Gets a value indicating whether this column should act as sortable if no value was set for the
     /// <see cref="ColumnBase{TGridItem}.Sortable" /> parameter. The default behavior is not to be
     /// sortable unless <see cref="ColumnBase{TGridItem}.Sortable" /> is true.
@@ -247,6 +262,7 @@ public abstract partial class ColumnBase<TGridItem>
     public ColumnBase()
     {
         HeaderContent = RenderDefaultHeaderContent;
+        HeaderTitleContent = RenderDefaultHeaderTitle;
     }
 
     private async Task HandleColumnHeaderClickedAsync()
