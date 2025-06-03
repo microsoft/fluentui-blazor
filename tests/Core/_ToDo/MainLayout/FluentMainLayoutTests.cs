132bf3437046193e846d529548a98b1f72c44fbc
Tyme-Bleyaert <72735570+Tyme-Bleyaert@users.noreply.github.com>
2025-06-03 08:57:55 +0200

    [MainLayout] Remove double header reduction (#3841)

--- tests/Core/_ToDo/MainLayout/FluentMainLayoutTests.cs
+++ tests/Core/_ToDo/MainLayout/FluentMainLayoutTests.cs
@@ -36,5 +36,35 @@ public class FluentMainLayoutTests : TestBase
         //Assert
         cut.Verify();
     }
+
+    [Fact]
+    public void FluentMainLayout_MainContent_Should_HaveCorrectHeight()
+    {
+        //Arrange
+        int? headerHeight = 60;
+        var expectedHeightStyle = $"height: 100%;";
+        var header = "<b>render me</b>";
+        var subHeader = "<b>render me</b>";
+        var navMenuContent = "<b>render me</b>";
+        var body = "<b>render me</b>";
+        string navMenuTitle = default!;
+        var navMenuWidth = 320;
+        var cut = TestContext.RenderComponent<FluentMainLayout>(parameters => parameters
+            .Add(p => p.Header, header)
+            .Add(p => p.SubHeader, subHeader)
+            .Add(p => p.HeaderHeight, headerHeight)
+            .Add(p => p.NavMenuTitle, navMenuTitle)
+            .Add(p => p.NavMenuContent, navMenuContent)
+            .Add(p => p.NavMenuWidth, navMenuWidth)
+            .Add(p => p.Body, body)
+        );
+        //Act
+        var fullContentStack = cut.FindComponent<FluentStack>();
+        var mainContentStack = fullContentStack.FindComponent<FluentStack>();
+
+        //Assert
+        Assert.Equal(expectedHeightStyle, fullContentStack.Instance.Style, StringComparer.Ordinal);
+        Assert.Equal(expectedHeightStyle, mainContentStack.Instance.Style, StringComparer.Ordinal);
+    }
 }
 
