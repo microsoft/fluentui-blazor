96e40df713bf8976d3d7968073ec04bce85aa18e
Vincent Baaij <vnbaaij@outlook.com>
2025-06-12 10:10:56 +0200

    Add DataGridGeneratedHeaderType

---
+++ src/Core/Enums/DataGridGeneratedHeaderType.cs
@@ -0,0 +1,26 @@
+// ------------------------------------------------------------------------
+// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// ------------------------------------------------------------------------
+
+namespace Microsoft.FluentUI.AspNetCore.Components;
+
+/// <summary>
+/// The option for generating a header for the <see cref="FluentDataGrid{TGridItem}" />.
+/// </summary>
+public enum DataGridGeneratedHeaderType
+{
+    /// <summary>
+    /// No header row.
+    /// </summary>
+    None,
+
+    /// <summary>
+    /// Generate a header row.
+    /// </summary>
+    Default,
+
+    /// <summary>
+    /// Generate a sticky header row.
+    /// </summary>
+    Sticky
+}
