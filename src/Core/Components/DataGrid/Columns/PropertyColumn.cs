f4b691a913ad4b8a5748d0ebb668645f694e87fe
Vincent Baaij <vnbaaij@outlook.com>
2025-07-01 16:08:59 +0200

    Update file headers

--- src/Core/Components/DataGrid/Columns/PropertyColumn.cs
+++ src/Core/Components/DataGrid/Columns/PropertyColumn.cs
@@ -1,5 +1,5 @@
 // ------------------------------------------------------------------------
-// MIT License - Copyright (c) Microsoft Corporation. All rights reserved.
+// This file is licensed to you under the MIT License.
 // ------------------------------------------------------------------------
 
 using System.Linq.Expressions;
