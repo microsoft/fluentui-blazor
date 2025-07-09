a4a62483580afb5247fc7a4926ba9b11fdb2d6af
Vincent Baaij <vnbaaij@outlook.com>
2025-07-09 20:36:17 +0200

    Add DynamicDependency on Toast types

--- src/Core/Components/Toast/ContentComponents/CommunicationToast.razor.cs
+++ src/Core/Components/Toast/ContentComponents/CommunicationToast.razor.cs
@@ -2,10 +2,13 @@
 // This file is licensed to you under the MIT License.
 // ------------------------------------------------------------------------
 
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Components;
 
 namespace Microsoft.FluentUI.AspNetCore.Components;
-public partial class CommunicationToast : IToastContentComponent<CommunicationToastContent>
+
+[method: DynamicDependency(DynamicallyAccessedMemberTypes.PublicConstructors, typeof(CommunicationToast))]
+public partial class CommunicationToast() : IToastContentComponent<CommunicationToastContent>
 {
 
     [Parameter]
