bde7620b284f82143edf4b177d8b56f8bccc6f06
Denis Voituron <dvoituron@outlook.com>
2025-07-30 13:38:30 +0200

    Update to have a Standalone WASM project (part 1)

--- examples/Demo/FluentUI.Demo.Client/Program.cs
+++ examples/Demo/FluentUI.Demo.Client/Program.cs
@@ -3,10 +3,14 @@
 // ------------------------------------------------------------------------
 
 using FluentUI.Demo.Client;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<Routes>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 // Add FluentUI services
 builder.Services.AddFluentUIComponents();
