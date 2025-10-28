a1d5fd6a94bfe7c56f4b22ec9b5c19408dc916f8
Vincent Baaij <vnbaaij@outlook.com>
2025-10-28 13:22:38 +0100

    Store

--- src/Core.Assets/esbuild.config.mjs
+++ src/Core.Assets/esbuild.config.mjs
@@ -1,9 +1,11 @@
 import * as esbuild from 'esbuild'
+import { pluginCompress } from '@espcom/esbuild-plugin-compress';
 import pkg from './package.json' with { type: 'json' }
 
 await esbuild.build({
     entryPoints: [ pkg.source ],
     bundle: true,
+    write: false,
     minify: true,
     sourcemap: true,
     logLevel: 'info',
@@ -11,4 +13,13 @@ await esbuild.build({
     format: 'esm',
     outfile: pkg.main,
     legalComments: 'external',
+    plugins: [
+        pluginCompress({
+            gzip: true,  // Enable gzip compression
+            brotli: true,  // Enable brotli compression
+            zstd: true,  // Enable zstd compression
+            level: 'high',  // Compression level: low, high, max
+            extensions: ['.js']  // File extensions to compress
+        })
+    ],
 });
