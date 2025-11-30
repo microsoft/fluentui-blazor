cd763fd61ca515cd808a7d6d7315bda291c92a8b
Vincent Baaij <vnbaaij@outlook.com>
2025-11-30 19:32:36 +0100

    Start ith integration into demo site

---
+++ examples/Demo/FluentUI.Demo.Client/Layout/DemoNav.razor.cs
@@ -0,0 +1,91 @@
+// ------------------------------------------------------------------------
+// This file is licensed to you under the MIT License.
+// ------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace FluentUI.Demo.Client.Layout;
+
+public partial class DemoNav
+{
+    [Inject]
+    public required NavigationManager NavigationManager { get; set; }
+
+    [Parameter]
+    public FluentLayoutHamburger? Hamburger { get; set; }
+
+    protected override void OnInitialized()
+    {
+        var pages = DocViewerService.Pages.Where(i => !i.Hidden);
+
+        var navItems = pages
+                .GroupBy(p => p.Category.Title)
+                .Select(categoryGroup => 
+                {
+                    // Group items within each category by PageGroup
+                    var subGroups = categoryGroup
+                        .GroupBy(p => p.PageGroup ?? string.Empty)
+                        .Select(subGroup =>
+                        {
+                            // If PageGroup is empty, return items directly
+                            if (string.IsNullOrEmpty(subGroup.Key))
+                            {
+                                return subGroup.Select(p => new NavItem(
+                                    Title: p.Title,
+                                    Route: p.Route,
+                                    Icon: p.Icon,
+                                    Order: p.Order,
+                                    Items: []));
+                            }
+
+                            // If PageGroup has value, create a sub-category
+                            else
+                            {
+                                return [ new NavItem(
+                                    Title: subGroup.Key,
+                                    Route: subGroup.FirstOrDefault(p => p.IsDefaultPageGroup)?.Route ?? subGroup.OrderBy(p => p.Order).First().Route,
+                                    Icon: string.Empty,
+                                    Order: subGroup.First().Order,
+                                    Items: subGroup.Where(p => !p.IsDefaultPageGroup)
+                                                   .Select(p => new NavItem(
+                                                        Title: p.Title,
+                                                        Route: p.Route,
+                                                        Icon: p.Icon,
+                                                        Order: p.Order,
+                                                        Items: Enumerable.Empty<NavItem>()))
+                                    .OrderBy(i => i.Order)
+                                    .ThenBy(i => i.Title))
+                                ];
+                            }
+                        })
+                        .SelectMany(x => x)
+                        .OrderBy(i => i.Order)
+                        .ThenBy(i => i.Title);
+
+                    return new NavItem(
+                        Title: string.IsNullOrEmpty(categoryGroup.Key) ? "Components" : categoryGroup.Key,
+                        Route: string.Empty,
+                        Icon: string.Empty,
+                        Order: string.IsNullOrEmpty(categoryGroup.Key) ? "0099" : "0000",
+                        Items: subGroups);
+                })
+                .OrderBy(i => i.Order);
+
+        NavItems = navItems;
+    }
+
+    public IEnumerable<NavItem> NavItems { get; private set; } = Enumerable.Empty<NavItem>();
+
+    public record NavItem(string Title, string Route, string Icon, string Order, IEnumerable<NavItem> Items);
+
+    //private async Task ItemClickAsync(NavItem item)
+    //{
+    //    NavigationManager.NavigateTo(item.Route);
+
+    //    if (Hamburger is not null)
+    //    {
+    //        await Hamburger.HideAsync();
+    //    }
+    //}
+}
