0bdb697a4042b1c2151047230d9a4902310a99ad
Denis Voituron <dvoituron@outlook.com>
2025-12-04 18:25:07 +0100

    Add FluentOverlay Web Component

---
+++ src/Core.Scripts/src/Components/Overlay/FluentOverlay-Styles.ts
@@ -0,0 +1,60 @@
+export const fluentOverlayStyles: string = `
+  :host {
+    --overlayZIndex: 99999;
+    --overlayBackground: '';
+  }
+
+  :host dialog {
+    border: 1px solid blue; /* TODO: Remove me */
+    background: none;
+    z-index: var(--overlayZIndex);
+  }
+
+  :focus-visible {
+    outline: none;
+  }
+
+  :host[fullscreen] dialog {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    margin: 0;
+    transform: translate(-50%, -50%);
+    pointer-events: pointer;
+  }
+
+  :host[fullscreen] dialog::backdrop {
+    background-color: var(--overlayBackground);
+  }
+
+  :host:not([fullscreen]):has(dialog[open]) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: var(--overlayBackground);
+    /* pointer-events: none; */
+  }
+
+  :host:not([fullscreen]) dialog {
+    position: fixed;
+    margin: 0;
+    transform: translate(-50%, -50%);
+  }
+
+  :host:not([fullscreen]) dialog::backdrop {
+    background-color: transparent;
+  }
+
+  :host[interactive][fullscreen]:has(dialog[open]) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: calc(var(--overlayZIndex) - 1);
+    pointer-events: none;
+    background-color: var(--overlayBackground);
+  }
+`;
---
+++ src/Core.Scripts/src/Components/Overlay/FluentOverlay.ts
@@ -0,0 +1,196 @@
+import { StartedMode } from "../../d-ts/StartedMode";
+import { fluentOverlayStyles } from "./FluentOverlay-Styles";
+
+export namespace Microsoft.FluentUI.Blazor.Components.Overlay {
+
+  class FluentOverlay extends HTMLElement {
+
+    private container: HTMLElement | null = null;
+    private dialog: HTMLDialogElement;
+    private resizeObserver: ResizeObserver | null = null;
+
+    // Add backing field for opened property
+    private _opened: boolean = false;
+
+    /************************
+      Initialization
+     ************************/
+    constructor() {
+      super();
+    }
+
+    // Delay initialization to ensure child content is parsed
+    connectedCallback() {
+      setTimeout(() => {
+        this.initialize();
+      }, 0);
+    }
+
+    private initialize() {
+      this.container = this.parentElement;
+      const shadow = this.attachShadow({ mode: 'open' });
+
+      // Create the dialog element
+      this.dialog = document.createElement('dialog') as HTMLDialogElement;
+      this.dialog.setAttribute('fuib', '');
+      this.dialog.setAttribute('part', 'dialog');    // To allow styling using `fluent-overlay::part(dialog)`
+
+      // Prevent to use ESC key to close the dialog
+      // https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/closedBy#browser_compatibility
+      // this.dialog.setAttribute('closedBy', 'none');
+
+      // Set initial styles for the dialog
+      const sheet = new CSSStyleSheet();
+      sheet.replaceSync(fluentOverlayStyles);
+      this.shadowRoot!.adoptedStyleSheets = [
+        ...(this.shadowRoot!.adoptedStyleSheets || []),
+        sheet
+      ];
+
+      // Slot for user content
+      const contentSlot = document.createElement('slot');
+      while (this.firstChild) {
+        contentSlot.appendChild(this.firstChild);
+      }
+      this.dialog.appendChild(contentSlot);
+      shadow.appendChild(this.dialog);
+    }
+
+    /************************
+       Public Properties
+    ************************/
+
+    // Property getter/setter for fullscreen
+    public get fullscreen(): boolean {
+      return this.hasAttribute('fullscreen');
+    }
+
+    public set fullscreen(value: boolean) {
+      if (value) {
+        this.setAttribute('fullscreen', '');
+      } else {
+        this.removeAttribute('fullscreen');
+      }
+    }
+
+    // Property getter/setter for interactive
+    public get interactive(): boolean {
+      return this.hasAttribute('interactive');
+    }
+
+    public set interactive(value: boolean) {
+      if (value) {
+        this.setAttribute('interactive', '');
+      } else {
+        this.removeAttribute('interactive');
+      }
+    }
+
+    // Property getter/setter for background-color
+    public get background(): string {
+      return this.getAttribute('background') ?? 'color-mix(in srgb, var(--colorBackgroundOverlay) 40%, transparent)';
+    }
+
+    public set background(value: string) {
+      if (value) {
+        this.setAttribute('background', '');
+      } else {
+        this.removeAttribute('background');
+      }
+    }
+
+    /************************
+      Public methods
+    ************************/
+
+
+    // Public method to open the dialog
+    public show() {
+      if (this.dialog) {
+
+        if (this.fullscreen === false) {
+          this.ensureParentPositioning();
+          this.createResizeObserver();
+        }
+
+        if (this.background) {
+          this.style.setProperty('--overlayBackground', this.background);
+        }
+
+        if (this.interactive) {
+          this.dialog.show();
+        } else {
+          this.dialog.showModal();
+        }
+      }
+    }
+
+    // Public method to close the dialog
+    public close() {
+      if (this.dialog) {
+        this.dialog.close();
+      }
+    }
+
+    /************************
+      Private methods
+    ************************/
+
+    // Private method to ensure parent has proper positioning
+    private ensureParentPositioning(): void {
+      const parent = this.parentElement;
+      if (parent) {
+        const computedStyle = window.getComputedStyle(parent);
+        const position = computedStyle.position;
+
+        // Check if position is one of the required values
+        if (position !== 'relative' && position !== 'absolute' && position !== 'fixed' && position !== 'sticky') {
+          parent.style.position = 'relative';
+        }
+      }
+    }
+
+    // Private method to position the dialog in the container
+    private positionDialogInContainer(): void {
+      if (this.container && this.dialog && this.fullscreen === false) {
+        const containerRect = this.container.getBoundingClientRect();
+        this.dialog.style.top = `${containerRect.top + containerRect.height / 2}px`;
+        this.dialog.style.left = `${containerRect.left + containerRect.width / 2}px`;
+      }
+    }
+
+    // Subscribe to container size changes        
+    private createResizeObserver(): void {
+      if (!this.resizeObserver && this.container) {
+        this.resizeObserver = new ResizeObserver(() => {
+          this.positionDialogInContainer();
+        });
+        this.resizeObserver.observe(this.container);
+      }
+    }
+
+    // Private method to clean up the resize observer
+    private cleanResizeObserver(): void {
+      this.dialog.style.top = '';
+      this.dialog.style.left = '';
+      this.resizeObserver?.disconnect();
+      this.resizeObserver = null;
+    }
+
+    // Cleanup when element is removed from DOM
+    disconnectedCallback() {
+      this.cleanResizeObserver();
+    }
+  }
+
+  /**
+  * Register the FluentOverlay component
+  * @param blazor
+  * @param mode
+  */
+  export const registerComponent = (blazor: Blazor, mode: StartedMode): void => {
+    if (typeof blazor.addEventListener === 'function' && mode === StartedMode.Web) {
+      customElements.define('fluent-overlay', FluentOverlay);
+    }
+  };
+}
