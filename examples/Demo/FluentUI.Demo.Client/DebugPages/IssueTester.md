221ef9685da4bb601ef9849c3b16af49bdff2455
Vincent Baaij <vnbaaij@outlook.com>
2025-12-08 15:41:51 +0100

    Port #4383 to v5

---
+++ examples/Demo/FluentUI.Demo.Client/DebugPages/IssueTester.md
@@ -0,0 +1,10 @@
+---
+title: Issue Tester
+category: 90|Labs
+icon: Beaker
+route: /Test/IssueTester
+---
+
+Provides an easy entry point for testing out issue report related code in the demo environment.
+
+{{ IssueTester }}
--- src/Core/Components/DataGrid/FluentDataGridCell.razor.cs
+++ src/Core/Components/DataGrid/FluentDataGridCell.razor.cs
@@ -46,7 +46,7 @@ public partial class FluentDataGridCell<TGridItem> : FluentComponentBase
         .AddStyle("height", $"{Grid.ItemSize.ToString(CultureInfo.InvariantCulture):0}px", () => !Grid.EffectiveLoadingValue && Grid.Virtualize)
         .AddStyle("height", $"{((int)Grid.RowSize).ToString(CultureInfo.InvariantCulture)}px", () => !Grid.EffectiveLoadingValue && !Grid.Virtualize && !Grid.MultiLine && (Grid.Items is not null || Grid.ItemsProvider is not null) && InternalGridContext.TotalItemCount > 0)
         .AddStyle("height", "100%", Grid.MultiLine)
-        .AddStyle("min-height", "44px", Owner.RowType != DataGridRowType.Default)
+        .AddStyle("min-height", "40px", Owner.RowType != DataGridRowType.Default)
         .AddStyle("z-index", ZIndex.DataGridHeaderPopup.ToString(CultureInfo.InvariantCulture), CellType == DataGridCellType.ColumnHeader && Grid._columns.Count > 0 && Grid.UseMenuService)
         .AddStyle(Owner.Style)
         .Build();
