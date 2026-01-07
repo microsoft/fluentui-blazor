6af4847325693256f513dc6ef7318f4c16f66951
Vincent Baaij <vnbaaij@outlook.com>
2026-01-07 09:30:20 +0100

    Fix merging inconsistensies

--- src/Core/Components/Overflow/FluentOverflow.razor.cs
+++ src/Core/Components/Overflow/FluentOverflow.razor.cs
@@ -128,7 +128,6 @@ public partial class FluentOverflow : FluentComponentBase
     [JSInvokable]
     public async Task OverflowRaisedAsync(OverflowItem[] items)
     {
-
         if (items == null || items.Length == 0)
         {
             return;
