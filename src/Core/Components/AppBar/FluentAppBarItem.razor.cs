9b6334557f7ce56f13fecf3ced06d68272d6f1b6
Vincent Baaij <vnbaaij@outlook.com>
2026-02-13 12:35:44 +0100

    Only close the popover when in overflowed state

--- src/Core/Components/AppBar/FluentAppBarItem.razor.cs
+++ src/Core/Components/AppBar/FluentAppBarItem.razor.cs
@@ -105,7 +105,11 @@ public partial class FluentAppBarItem : FluentComponentBase, IAppBarItem, IDispo
     {
         if (OnClick.HasDelegate)
         {
-            await Owner.AppBar.TogglePopoverAsync();
+            if (Overflow is true)
+            {
+                await Owner.AppBar.TogglePopoverAsync();
+            }
+
             await OnClick.InvokeAsync(this);
         }
     }
