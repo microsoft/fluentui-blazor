2841f1e3fe673ce9ab30940765f79c5bd522db06
Vincent Baaij <vnbaaij@outlook.com>
2026-03-30 12:07:16 +0200

    [dev-v5] Add Theme API (and a Theme Designer to the demo) (#4602)
    * - Add ThemeService and corresponding TS code
    - Add color ramp calculation code (copied from React theme designer)
    - Add initial ThemeDesigner
    - Add Theme docs
    
    * Process review comments
    
    * - Add Palette component
    - Rework ThemeDesigner to use Fluent components
    - Refactor TS API
    - Add TS API doc
    
    * - Reorganize file structure
    
    * - Use ThemeService in Designer
    - Use C# Theme class in API calls
    - Update Theme readme
    
    * Small wording updates
    
    * More wording
    
    * Update docs
    
    * - Add Apply and Reset buttons to Designer
    - Tweak Designer behavior
    
    * Call correct TS func
    
    * - Add example on altering theme
    - Add option to apply theme to an element
    - Update docs
    
    * - Add unit tests
    
    * Process review comments
    
    * More review comments processed
    
    * Create custom theme throws when JS returns null
    
    * Adjust test to new thow behavior
    
    ---------
    
    Co-authored-by: Denis Voituron <dvoituron@outlook.com>

---
+++ examples/Demo/FluentUI.Demo.Client/Documentation/Components/Theme/Designer/CreateAndAlterTheme.razor.cs
@@ -0,0 +1,44 @@
+// ------------------------------------------------------------------------
+// This file is licensed to you under the MIT License.
+// ------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace FluentUI.Demo.Client.Documentation.Components.Theme.Designer;
+
+public partial class CreateAndAlterTheme
+{
+    ElementReference _exampleBlocks;
+
+    async Task ApplyCustomThemeAsync()
+    {
+        var isDark = await ThemeService.IsDarkModeAsync();
+        var theme = await ThemeService.CreateCustomThemeAsync(new ThemeSettings()
+        {
+            Color = "#0FFC0D",
+            Mode = isDark ? ThemeMode.Dark : ThemeMode.Light,
+        });
+
+        theme.Borders.Radius.Medium = "40px";
+        theme.Fonts.Family.Base = "Comic Sans MS, cursive, sans-serif";
+        theme.Fonts.Size.Base300 = "10px";
+        theme.Lines.Height.Base300 = "8px";
+
+        await ThemeService.SetThemeToElementAsync(_exampleBlocks, theme);
+    }
+
+    async Task ResetThemeAsync()
+    {
+        var isDark = await ThemeService.IsDarkModeAsync();
+        var color = await ThemeService.GetBrandColorAsync();
+
+        var theme = await ThemeService.CreateCustomThemeAsync(new ThemeSettings()
+        {
+            Color = color,
+            Mode = isDark ? ThemeMode.Dark : ThemeMode.Light,
+        });
+
+        await ThemeService.SetThemeToElementAsync(_exampleBlocks, theme);
+    }
+}
