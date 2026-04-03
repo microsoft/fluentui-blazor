affe63d481be24cc9ec3ca3c26132de570a6984b
Vincent Baaij <vnbaaij@outlook.com>
2026-04-03 20:49:16 +0200

    Delete AssemblyInfo.cs

--- src/Core/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// ------------------------------------------------------------------------
-// This file is licensed to you under the MIT License.
-// ------------------------------------------------------------------------
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Microsoft.FluentUI.AspNetCore.Components.Tests")]
