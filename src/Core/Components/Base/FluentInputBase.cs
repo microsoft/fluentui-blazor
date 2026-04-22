04cc16770a2256d0f1015c0c67db01325cbdc18e
Denis Voituron <dvoituron@outlook.com>
2026-04-22 22:00:41 +0200

    Fix DisposeAsync to handle InvalidOperationException during disposal (#4741)

--- src/Core/Components/Base/FluentInputBase.cs
+++ src/Core/Components/Base/FluentInputBase.cs
@@ -231,7 +231,8 @@ public abstract partial class FluentInputBase<TValue> : InputBase<TValue>, IFlue
                 await DisposeAsync(_jsModule.ObjectReference);
             }
             catch (Exception ex) when (ex is JSDisconnectedException ||
-                                       ex is OperationCanceledException)
+                                       ex is OperationCanceledException ||
+                                       ex is InvalidOperationException)
             {
                 // The JSRuntime side may routinely be gone already if the reason we're disposing is that
                 // the client disconnected. This is not an error.
