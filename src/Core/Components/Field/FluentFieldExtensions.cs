7cbc70827f3c1d31aa82f3b4fcc00134dcbf7e55
Denis VOITURON <dvoituron@outlook.com>
2026-05-06 20:23:35 +0200

    Refactor FluentCheckbox and FluentSwitch components to use ChildContent parameter

--- src/Core/Components/Field/FluentFieldExtensions.cs
+++ src/Core/Components/Field/FluentFieldExtensions.cs
@@ -2,6 +2,8 @@
 // This file is licensed to you under the MIT License.
 // ------------------------------------------------------------------------
 
+using Microsoft.AspNetCore.Components;
+
 namespace Microsoft.FluentUI.AspNetCore.Components;
 
 /// <summary>
@@ -18,5 +20,32 @@ public static class FluentFieldExtensions
     public static FluentFieldConditionItem When(this IFluentField fluentField, Func<bool> condition)
     {
         return new FluentFieldCondition(fluentField).When(condition);
+    }
+
+    /// <summary>
+    /// Combines two <see cref="RenderFragment"/> instances into one. If both fragments are null, returns null.
+    /// </summary>
+    /// <param name="item1"></param>
+    /// <param name="item2"></param>
+    /// <returns></returns>
+    public static RenderFragment? CombinedWith(this RenderFragment? item1, RenderFragment? item2)
+    {
+        if (item1 is null && item2 is null)
+        {
+            return null;
+        }
+
+        return builder =>
+        {
+            if (item1 is not null)
+            {
+                builder.AddContent(0, item1);
+            }
+
+            if (item2 is not null)
+            {
+                builder.AddContent(1, item2);
+            }
+        };
     }
 }
