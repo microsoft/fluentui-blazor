f359b01522b8345a000ae6214638d802515c4f05
Denis VOITURON <dvoituron@outlook.com>
2026-05-13 14:59:45 +0200

    Refactor FluentMenu example to enhance target positioning and context menu functionality

--- src/Core.Scripts/src/Components/Menu/FluentMenu.ts
+++ src/Core.Scripts/src/Components/Menu/FluentMenu.ts
@@ -1,6 +1,12 @@
 import { Menu, MenuList } from "@fluentui/web-components";
 
 export namespace Microsoft.FluentUI.Blazor.Components.Menu {
+
+  /**
+   * Initializes the menu by associating it with a trigger element and setting up the necessary styles for positioning.
+   * @param id The id of the fluent-menu element to initialize.
+   * @param triggerId The id of the trigger element that will open the menu when clicked.
+   */
   export function Initialize(id: string, triggerId: string) {
     const trigger = document.getElementById(triggerId) as HTMLElement;
 
@@ -15,6 +21,10 @@ export namespace Microsoft.FluentUI.Blazor.Components.Menu {
     }
   }
 
+  /**
+   * Closes the menu with the specified id.
+   * @param id 
+   */
   export function CloseMenu(id: string) {
     const menu = document.getElementById(id) as Menu;
     if (menu) {
@@ -22,6 +32,12 @@ export namespace Microsoft.FluentUI.Blazor.Components.Menu {
     }
   }
 
+  /**
+   * Opens the menu with the specified id, optionally positioning it relative to a target element with specified offsets.
+   * @param id The id of the fluent-menu element to open.
+   * @param targetId The id of the target element to position the menu relative to. If null, the menu will open in its default position.
+   * @param targetOffsetLeft The left offset from the target element to open the menu. Default is 0.
+   */
   export function OpenMenu(id: string, targetId: string | null = null, targetOffsetLeft: number = 0, targetOffsetTop: number = 0) {
     const menu = document.getElementById(id) as Menu;
 
