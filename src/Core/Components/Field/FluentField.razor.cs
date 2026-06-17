1f1ec6fea5472737fa09daa703ba0b14f98a066a
Denis VOITURON <dvoituron@outlook.com>
2026-06-17 12:04:19 +0200

    Refactor LabelWidth handling in WidthParameterPage and update FluentField styles for improved layout

--- src/Core/Components/Field/FluentField.razor.cs
+++ src/Core/Components/Field/FluentField.razor.cs
@@ -31,7 +31,6 @@ public partial class FluentField : FluentComponentBase, IFluentField
 
     private string? LabelStyle => new StyleBuilder()
         .AddStyle("width", Parameters.LabelWidth)
-        .AddStyle("min-width", Parameters.LabelWidth, when: Parameters.LabelPosition == Components.LabelPosition.Before)
         .Build();
 
     /// <summary>
