6fb3518200f922bf50279b8d3ca4705e598eb1e7
Denis VOITURON <dvoituron@outlook.com>
2026-06-27 11:39:37 +0200

    Add DemoNewsBar component and implement notification height adjustments

---
+++ examples/Demo/FluentUI.Demo.Client/Layout/DemoNewsBar.razor.cs
@@ -0,0 +1,10 @@
+// ------------------------------------------------------------------------
+// This file is licensed to you under the MIT License.
+// ------------------------------------------------------------------------
+
+namespace FluentUI.Demo.Client.Layout;
+
+public partial class DemoNewsBar
+{
+
+}
