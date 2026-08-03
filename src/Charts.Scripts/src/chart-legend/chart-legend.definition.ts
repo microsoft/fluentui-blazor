d122481db75198c7a3cbb0ccd32cefd339cc21af
Vincent Baaij <vnbaaij@outlook.com>
2026-08-03 16:45:06 +0200

    Fix merge issue

--- src/Charts.Scripts/src/chart-legend/chart-legend.definition.ts
+++ src/Charts.Scripts/src/chart-legend/chart-legend.definition.ts
@@ -1,9 +1,5 @@
 import { FluentDesignSystem } from '@fluentui/web-components';
-<<<<<<< HEAD
-import { ChartLegend } from './chart-legend.js';
-=======
 import { type PartialFASTElementDefinition } from '@microsoft/fast-element';
->>>>>>> users/vnbaaij/dev-v5/add-areachart
 import { styles } from './chart-legend.styles.js';
 import { template } from './chart-legend.template.js';
 
@@ -12,17 +8,10 @@ import { template } from './chart-legend.template.js';
  * @remarks
  * HTML Element: `<fluent-chart-legend>`
  */
-<<<<<<< HEAD
-export const definition = ChartLegend.compose({
-  name: `${FluentDesignSystem.prefix}-chart-legend`,
-  template,
-  styles,
-});
-=======
 export const definition: PartialFASTElementDefinition = {
   name: `${FluentDesignSystem.prefix}-chart-legend`,
   registry: FluentDesignSystem.registry,
   template,
   styles,
 };
->>>>>>> users/vnbaaij/dev-v5/add-areachart
+
