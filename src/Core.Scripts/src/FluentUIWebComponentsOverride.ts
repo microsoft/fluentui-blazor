504f75695f8eaf68e553275cdb5c7417adc437a8
Denis VOITURON <dvoituron@outlook.com>
2026-08-06 17:06:18 +0200

    Refactor handleNavigation in fluent-anchor-button to integrate Blazor navigation

--- src/Core.Scripts/src/FluentUIWebComponentsOverride.ts
+++ src/Core.Scripts/src/FluentUIWebComponentsOverride.ts
@@ -2,21 +2,25 @@
 /// <reference path="./d-ts/Blazor.d.ts" />
 
 export namespace Microsoft.FluentUI.Override {
-
     /**
      * Override the default behavior of the Fluent UI Web Components to use Blazor's features.
     */
     export function overrideComponents() {
 
-        /* Original code: https://github.com/microsoft/fluentui/blob/master/packages/web-components/src/anchor-button/anchor-button.base.ts
+        /**
+         * fluent-anchor-button
+         */
+        waitForCustomElements().then(async registry => {
+            const anchorButton = await registry.whenDefined('fluent-anchor-button');
+            const proto = anchorButton.prototype;
 
-           private handleNavigation(newTab: boolean): void {
-             newTab ? window.open(this.href, '_blank') : this.internalProxyAnchor.click();
-           }
-        */
+            /* 
+                Original code: https://github.com/microsoft/fluentui/blob/master/packages/web-components/src/anchor-button/anchor-button.base.ts
 
-        customElements.whenDefined('fluent-anchor-button').then(() => {
-            const proto = customElements.get('fluent-anchor-button').prototype;
+                private handleNavigation(newTab: boolean): void {
+                    newTab ? window.open(this.href, '_blank') : this.internalProxyAnchor.click();
+                }
+            */
             proto.handleNavigation = function (newTab: boolean): void {
                 if (newTab) {
                     window.open(this.href, '_blank');
@@ -25,13 +29,35 @@ export namespace Microsoft.FluentUI.Override {
 
                 const url = new URL(this.href, document.baseURI);
                 if (url.origin === location.origin) {
-                    Blazor.navigateTo(url.pathname + url.search + url.hash, false);
+                    const forceLoad = this.getAttribute('force-load') === 'true';
+                    console.log(`fluent-anchor-button handleNavigation: navigating to ${url.pathname + url.search + url.hash} using Blazor.navigateTo with forceLoad=${forceLoad}`);
+                    Blazor.navigateTo(url.pathname + url.search + url.hash, forceLoad);
                 }
-                else {
-                    this.internalProxyAnchor.click()
-                    //location.href = this.href;
+                else {                    
+                    console.log(`fluent-anchor-button handleNavigation: navigating to ${this.href} using internalProxyAnchor.click()`);
+                    this.internalProxyAnchor.click();
                 }
             };
+        });
+    }
+
+    /**
+     * Wait for the customElements registry to be available before overriding the components.
+     * This is necessary because the Fluent UI Web Components are defined asynchronously.
+     * @returns A promise that resolves with the customElements registry.
+     */
+    function waitForCustomElements(): Promise<CustomElementRegistry> {
+        return new Promise(resolve => {
+            const checkCustomElements = () => {
+                if (typeof customElements !== 'undefined') {
+                    resolve(customElements);
+                    return;
+                }
+
+                setTimeout(checkCustomElements, 10);
+            };
+
+            checkCustomElements();
         });
     }
 }
