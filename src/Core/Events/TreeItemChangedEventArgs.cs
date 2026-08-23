3400b42f9a0b23d46477b95533fe00decd1feb68
James Newton-King <james@newtonking.com>
2026-08-24 02:25:08 +0800

    fix: add AOT metadata for custom events (#5194)

--- src/Core/Events/TreeItemChangedEventArgs.cs
+++ src/Core/Events/TreeItemChangedEventArgs.cs
@@ -5,9 +5,11 @@
 namespace Microsoft.FluentUI.AspNetCore.Components;
 
 /// <summary>
-/// Event arguments for the FluentTabs ActiveId changed event.
+/// Event arguments for a FluentTreeItem selection change.
 /// </summary>
-internal class TreeItemChangedEventArgs : EventArgs
+// This type is public because it is included in the public FluentUIJsonSerializerContext.
+// It can be made internal again if the serializer context can be made internal in the future.
+public class TreeItemChangedEventArgs : EventArgs
 {
     /// <summary>
     /// Gets or sets the ID of the tree item.
