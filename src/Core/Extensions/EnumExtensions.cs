30ffc9853a2f4c6351f6b9099ba24e1d007337fc
James Newton-King <james@newtonking.com>
2026-09-10 14:19:47 +0800

    fix: correct enum description trimming justification

--- src/Core/Extensions/EnumExtensions.cs
+++ src/Core/Extensions/EnumExtensions.cs
@@ -65,7 +65,7 @@ public static class EnumExtensions
     /// <param name="value"></param>
     /// <returns></returns>
     [SuppressMessage("Trimming", "IL2075:'this' argument does not satisfy 'DynamicallyAccessedMembersAttribute' in call to target method. The return value of the source method does not have matching annotations.",
-                     Justification = "In the context of the Enum, the 'Display' attribute will not be trimmed.")]
+                     Justification = "In the context of the Enum, the 'Description' attribute will not be trimmed.")]
     public static string GetDescription(this Enum value)
     {
         var fieldInfo = value.GetType().GetField(value.ToString());
