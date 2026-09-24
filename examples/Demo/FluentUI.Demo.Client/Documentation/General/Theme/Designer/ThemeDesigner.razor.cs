9a0364996a975a8834a012657be04862c9753bcf
Denis VOITURON <dvoituron@outlook.com>
2026-09-24 17:47:26 +0200

    feat(ThemeDesigner): initialize dark mode setting based on system preference
    feat(DemoMainLayout): add DemoNewsBar to both home and content layouts

--- examples/Demo/FluentUI.Demo.Client/Documentation/General/Theme/Designer/ThemeDesigner.razor.cs
+++ examples/Demo/FluentUI.Demo.Client/Documentation/General/Theme/Designer/ThemeDesigner.razor.cs
@@ -21,6 +21,11 @@ public partial class ThemeDesigner
     [Inject]
     private IThemeService ThemeService { get; set; } = default!;
 
+    protected override async Task OnInitializedAsync()
+    {
+        _isDark = await ThemeService.IsSystemDarkAsync();
+    }
+
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         if (firstRender)
